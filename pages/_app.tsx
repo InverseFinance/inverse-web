1b442aff9384efe7fecda45d59a5ec39cf2a2ac9
Slokh <slokh@octane.gg>
2021-07-06 10:22:48 -0400

    Change refresh interval to 5 minutes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,7 +27,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <SWRConfig
           value={{
             fetcher,
-            refreshInterval: 30000,
+            refreshInterval: 120000,
           }}
         >
           <Component {...pageProps} />
