3041051baf761f3606ee75112fd34fcf8b5fa2f9
webmass <mextrasis@hotmail.com>
2021-11-10 17:45:50 +0100

    Tests: init landing page

---
+++ config/test-ids.ts
@@ -0,0 +1,8 @@
+export const TEST_IDS = {
+    announcement: 'announcement-bar',
+    connectBtn: 'connect-btn',
+    landing: {
+        enterBtn: 'landing-enter-btn',
+        learnMore: 'landing-learn-more-btn',
+    },
+}
---
+++ cypress/integration/landing.spec.ts
@@ -0,0 +1,28 @@
+/* eslint-disable */
+import { TEST_IDS } from '@inverse/config/test-ids';
+
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
+// TODO : more tests
+describe('Landing Page', () => {
+  it('should navigate to landing page by default', () => {
+    cy.visit('http://localhost:3000')
+    cy.shouldTestId(TEST_IDS.announcement, 'exist')
+    cy.shouldTestId(TEST_IDS.landing.enterBtn, 'exist')
+    cy.shouldTestId(TEST_IDS.landing.learnMore, 'exist')
+  })
+
+  it('should lead to /anchor on enter app', () => {
+    cy.getByFirstTestId(TEST_IDS.landing.enterBtn)
+      .find('a')
+      .should('have.attr', 'href', '/anchor')
+  })
+
+  it('should lead to https://docs.inverse.finance/ on learn more', () => {
+    cy.getByFirstTestId(TEST_IDS.landing.learnMore)
+      .find('a')
+      .should('have.attr', 'href', 'https://docs.inverse.finance/')
+  })
+})
