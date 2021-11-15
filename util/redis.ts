3ccfdf1f2020fca127a02baec20de309a6e9d229
webmass <mextrasis@hotmail.com>
2021-11-15 22:37:37 +0100

    Redis: add retry_strategy

--- util/redis.ts
+++ util/redis.ts
@@ -6,8 +6,21 @@ function initRedis() {
     console.log('Redis: connecting');
     const client = createNodeRedisClient({
         url: process.env.REDIS_URL,
-        max_attempts: 3,
-        retry_max_delay: 2000,
+        retry_strategy: function(options) {
+            console.log('retry strategy triggered');
+            if (options.error && options.error.code === "ECONNREFUSED") {
+              return new Error("The server refused the connection");
+            }
+            if (options.total_retry_time > 1000 * 60 * 60) {
+              return new Error("Retry time exhausted");
+            }
+            if (options.attempt > 10) {
+              // End reconnecting with built in error
+              return undefined;
+            }
+            // reconnect after
+            return Math.min(options.attempt * 100, 3000);
+          },
     });
 
     client.nodeRedis.on('error', (err) => {
