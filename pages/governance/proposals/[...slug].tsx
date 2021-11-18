27ac0aec2f278d28eb4332577198f4388da98dc8
webmass <mextrasis@hotmail.com>
2021-11-18 18:12:05 +0100

    Proposals: handle not found

--- pages/governance/proposals/[...slug].tsx
+++ pages/governance/proposals/[...slug].tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Text } from '@chakra-ui/react'
 import { Breadcrumbs } from '@inverse/components/common/Breadcrumbs'
 import {
   AgainstVotes,
@@ -19,13 +19,16 @@ import { useProposals } from '@inverse/hooks/useProposals';
 export const Governance = () => {
   const { asPath } = useRouter();
   const slug = asPath.replace('/governance/proposals/', '').split('/');
-  const { proposals } = useProposals();
+  const { proposals, isLoading } = useProposals();
 
   const proposal = proposals?.find((p: Proposal) => {
     return slug.length === 1 ? p.proposalNum.toString() === slug[0] : p.era === slug[0] && p.id.toString() === slug[1]
   }) || {} as Proposal;
 
   const { id = '', era = '' } = proposal;
+
+  const notFound = !isLoading && !id;
+  const proposalBreadLabel = !notFound ? `#${id.toString().padStart(3, '0')} of ${era.toUpperCase()} Era` : slug.join('/');
 
   return (
     <Layout>
@@ -35,32 +38,40 @@ export const Governance = () => {
         breadcrumbs={[
           { label: 'Governance', href: '/governance' },
           { label: 'Proposals', href: '/governance/proposals' },
-          { label: `Proposal ${id} of ${era} Era`, href: '#' },
+          { label: `Proposal ${proposalBreadLabel}`, href: '#' },
         ]}
       />
       <Flex w="full" justify="center" direction={{ base: 'column', xl: 'row' }}>
-        <Flex direction="column">
-          <Flex w={{ base: 'full', xl: '4xl' }} justify="center">
-            <ProposalDetails proposal={proposal} />
+        {
+          notFound ? <Flex w="full" justifyContent="center" pt="50">
+            <Text fontSize="xl">Proposal not found, please check the url</Text>
           </Flex>
-          <Flex w={{ base: 'full', xl: '4xl' }} justify="center">
-            <ProposalActions proposal={proposal} />
-          </Flex>
-        </Flex>
-        <Flex direction="column">
-          <Flex w={{ base: 'full', xl: 'sm' }} justify="center">
-            <VoteButton proposal={proposal} />
-          </Flex>
-          <Flex w={{ base: 'full', xl: 'sm' }} justify="center">
-            <VotingWallet />
-          </Flex>
-          <Flex w={{ base: 'full', xl: 'sm' }} justify="center">
-            <ForVotes proposal={proposal} />
-          </Flex>
-          <Flex w={{ base: 'full', xl: 'sm' }} justify="center">
-            <AgainstVotes proposal={proposal} />
-          </Flex>
-        </Flex>
+            :
+            <>
+              <Flex direction="column">
+                <Flex w={{ base: 'full', xl: '4xl' }} justify="center">
+                  <ProposalDetails proposal={proposal} />
+                </Flex>
+                <Flex w={{ base: 'full', xl: '4xl' }} justify="center">
+                  <ProposalActions proposal={proposal} />
+                </Flex>
+              </Flex>
+              <Flex direction="column">
+                <Flex w={{ base: 'full', xl: 'sm' }} justify="center">
+                  <VoteButton proposal={proposal} />
+                </Flex>
+                <Flex w={{ base: 'full', xl: 'sm' }} justify="center">
+                  <VotingWallet />
+                </Flex>
+                <Flex w={{ base: 'full', xl: 'sm' }} justify="center">
+                  <ForVotes proposal={proposal} />
+                </Flex>
+                <Flex w={{ base: 'full', xl: 'sm' }} justify="center">
+                  <AgainstVotes proposal={proposal} />
+                </Flex>
+              </Flex>
+            </>
+        }
       </Flex>
     </Layout>
   )
