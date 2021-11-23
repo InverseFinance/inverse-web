7cb21f61718fa0a05abf153b884317e8968d9f96
webmass <mextrasis@hotmail.com>
2021-11-23 15:50:54 +0100

    Debug: remove debug condition

--- components/Governance/Propose/ProposalFormContainer.tsx
+++ components/Governance/Propose/ProposalFormContainer.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@inverse/components/common/Container';
 import { InfoMessage } from '@inverse/components/common/Messages';
 import { ProposalForm } from './ProposalForm';
+import { Text } from '@chakra-ui/react';
 
 const REQUIRED_VOTING_POWER = 1000;
 
@@ -12,8 +13,14 @@ export const ProposalFormContainer = ({ votingPower }: { votingPower: number }) 
             href="https://docs.inverse.finance/governance"
         >
             {
-                votingPower > REQUIRED_VOTING_POWER ?
-                    <InfoMessage description="At least 1000 voting power is required to propose" />
+                votingPower <= REQUIRED_VOTING_POWER ?
+                    <InfoMessage
+                        alertProps={{ w: 'full' }}
+                        description={
+                            <>
+                                At least <b>1000 voting power</b> is required to propose
+                            </>
+                        } />
                     : <ProposalForm />
             }
         </Container>
