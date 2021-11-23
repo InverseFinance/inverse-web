9ccec6f4f8e5c357572f2abdd55843976a6fcc6a
webmass <mextrasis@hotmail.com>
2021-11-23 09:51:44 +0100

    Proposals: add propose abi

--- config/abis.ts
+++ config/abis.ts
@@ -91,6 +91,7 @@ export const GOVERNANCE_ABI = [
   "function proposals(uint256) public view returns (uint256 id, address proposer, uint256 eta, uint256 startBlock, uint256 endBlock, uint256 forVotes, uint256 againstVotes, bool canceled, bool executed)",
   "function quorumVotes() public view returns (uint256)",
   "function state(uint256) public view returns (uint256)",
+  "function propose(address[] targets, uint256[] values, string[] signatures, bytes[] calldata, string description) public returns (uint)",
   "event ProposalCreated (uint256 id, address proposer, address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, uint256 startBlock, uint256 endBlock, string description)",
   "event VoteCast (address voter, uint256 proposalId, bool support, uint256 votes)",
 ];
