412eb5cb94e6554ebf8a9e81a1b9b0fa752131db
webmass <mextrasis@hotmail.com>
2021-11-29 11:04:18 +0100

    Routing: add banking redirect to anchor

--- next.config.js
+++ next.config.js
@@ -1,15 +1,27 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
+const redirects = async () => {
+  return [
+    {
+      source: '/banking',
+      destination: '/anchor',
+      permanent: true,
+    },
+  ]
+}
+
 module.exports = (phase) => {
   switch (phase) {
     case PHASE_DEVELOPMENT_SERVER:
       return {
+        redirects,
         env: {
           COINGECKO_PRICE_API: 'https://api.coingecko.com/api/v3/simple/price',
         },
       }
     default:
       return {
+        redirects,
         typescript: {
           ignoreBuildErrors: true,
         },
