0b0a42a65d2d17d50fd1f40e2b6eb8ed90547ebb
webmass <mextrasis@hotmail.com>
2021-12-24 17:16:02 +0100

    Avatar: alchemy only, handle invalid addresses

--- components/common/Avatar/index.tsx
+++ components/common/Avatar/index.tsx
@@ -1,11 +1,12 @@
 import { Box, BoxProps } from '@chakra-ui/react'
 import Davatar, { DavatarProps } from '@davatar/react';
-import { getProvider } from '@inverse/util/providers';
 import { NetworkIds } from '@inverse/types';
+import { AlchemyProvider } from '@ethersproject/providers';
+import { isAddress } from 'ethers/lib/utils';
 
 export const Avatar = ({
   address,
-  defaultAvatar = 'blockies',
+  defaultAvatar = 'jazzicon',
   sizePx = 20,
   provider,
   ...boxProps
@@ -16,11 +17,11 @@ export const Avatar = ({
   defaultAvatar?: 'blockies' | 'jazzicon',
 } & Partial<BoxProps>) => {
   // specific key for this usage only
-  const defaultProvider = getProvider(NetworkIds.mainnet, 'Im06YX8gmXb3PDPLG7nv3ExIYGppGDvf', true);
-  
+  const alchemyProvider = new AlchemyProvider(Number(NetworkIds.mainnet), 'Im06YX8gmXb3PDPLG7nv3ExIYGppGDvf');
+  const avatarAddress = !address || !isAddress(address) ? '0x0000000000000000000000000000000000000000' : address
   return (
     <Box boxSize={`${sizePx}px`} {...boxProps}>
-      <Davatar provider={provider || defaultProvider} size={sizePx} address={address} generatedAvatarType={defaultAvatar} />
+      <Davatar provider={provider || alchemyProvider} size={sizePx} address={avatarAddress} generatedAvatarType={defaultAvatar} />
     </Box>
   )
 }
