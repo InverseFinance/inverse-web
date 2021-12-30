4acce524e1d00b0af4934d930f07a972a709dd4d
webmass <mextrasis@hotmail.com>
2021-12-30 18:24:17 +0100

    Delegates: show ens name in more sections

--- hooks/useNamedAddress.tsx
+++ hooks/useNamedAddress.tsx
@@ -1,23 +1,29 @@
 import { useState, useEffect } from 'react';
-import { getEnsName, namedAddress } from '@inverse/util';
+import { getEnsName, namedAddress, shortenAddress } from '@inverse/util';
 
-export const useNamedAddress = (address? : string | undefined | null, chainId?: string | number, ensName?: string) => {
+export const useNamedAddress = (address? : string | undefined | null, chainId?: string | number, ensName?: string, priorityToEns = false) => {
   const initial = namedAddress(address||'', chainId, ensName);
   const [name, setName] = useState(initial);
 
   useEffect(() => {
+    let isMounted = true
     const init = async () => {
       if(!address) { return }
       const initial = namedAddress(address||'', chainId, ensName);
+      if(!isMounted){ return }
       setName(initial);
-      if (!ensName) {
+      if (!ensName && ((initial !== shortenAddress(address) && priorityToEns) || (initial === shortenAddress(address)))) {
         const fetchedEnsName = await getEnsName(address);
         if (fetchedEnsName) {
+          if(!isMounted){ return }
           setName(fetchedEnsName)
         }
       }
     }
     init();
+    return () => {
+      isMounted = false
+    }
   }, [address, chainId, ensName])
 
   return { addressName: name, address };
