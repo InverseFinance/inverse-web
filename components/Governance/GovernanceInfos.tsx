59cd2fd8a921dcac12ebf76a85f0e58ea317eab5
webmass <mextrasis@hotmail.com>
2022-01-13 11:32:56 +0100

    Governance: remove diagram link

--- components/Governance/GovernanceInfos.tsx
+++ components/Governance/GovernanceInfos.tsx
@@ -1,15 +1,10 @@
 import { InfoMessage } from '@inverse/components/common/Messages'
-import Link from '@inverse/components/common/Link'
 
 export const GovernanceInfos = () => {
     return (
         <InfoMessage
             alertProps={{ fontSize: '12px', w: 'full' }}
             title="Governance data is updated every 15 min"
-            description={
-                <Link href="/diagrams/overview">
-                    See the Governance Overview
-                </Link>
-            } />
+        />
     )
 }
