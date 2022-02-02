b56e924c5f68bffc26a567521f0709c5c8aeba10
webmass <mextrasis@hotmail.com>
2022-02-02 11:35:08 +0100

    Transparency: add fed-policy-msg api

---
+++ pages/api/transparency/fed-policy-msg.ts
@@ -0,0 +1,41 @@
+
+import { getRedisClient } from '@app/util/redis';
+import { verifyMessage } from 'ethers/lib/utils';
+import { DRAFT_SIGN_MSG } from '@app/config/constants';
+
+const client = getRedisClient();
+const redisKey = 'fed-policy-msg'
+
+export default async function handler(req, res) {
+    const {
+        method,
+    } = req
+
+    switch (method) {
+        case 'GET':
+            const msg = await client.get(redisKey) || '{"msg": "No guidance at the moment","lastUpdate": '+Date.now()+'}';
+            res.status(200).json({ status: 'success', ...JSON.parse(msg) })
+            break
+        case 'POST':
+            try {
+                const { sig, msg } = req.body
+                const whitelisted = (process?.env?.DRAFT_ADDRESS_WHITELIST || '')?.replace(/\s/g, '').toLowerCase().split(',');
+                const sigAddress = verifyMessage(DRAFT_SIGN_MSG, sig).toLowerCase();
+
+                if (!whitelisted.includes(sigAddress)) {
+                    res.status(401).json({ status: 'warning', message: 'Unauthorized' })
+                    return
+                };
+                
+                await client.set(redisKey, JSON.stringify({ msg, lastUpdate: Date.now() }));
+
+                res.status(200).json({ status: 'success' })
+            } catch (e) {
+                res.status(200).json({ status: 'error', message: 'An error occured' })
+            }
+            break
+        default:
+            res.setHeader('Allow', ['GET', 'POST'])
+            res.status(405).end(`Method ${method} Not Allowed`)
+    }
+}
