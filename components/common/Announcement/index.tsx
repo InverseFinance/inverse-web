24efa094130211ab6d6723d277eebc0051da8f02
webmass <mextrasis@hotmail.com>
2022-02-04 14:34:55 +0100

    Inv+: replace countdown

--- components/common/Announcement/index.tsx
+++ components/common/Announcement/index.tsx
@@ -1,15 +1,15 @@
 import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { Text, useInterval } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
 import { Flex } from '@chakra-ui/layout'
 import Link from '@app/components/common/Link'
 import { TEST_IDS } from '@app/config/test-ids';
 import { useSupplyBalances } from '@app/hooks/useBalances';
 import { OLD_XINV } from '@app/config/constants';
 import { utils } from 'ethers'
-import { Countdown } from '../Countdown';
-import { useState } from 'react';
-import { PlusAnimIcon } from '../Animation/PlusAnim';
+
+import { PlusAnimIcon } from '@app/components/common/Animation/PlusAnim';
 import { useRouter } from 'next/router';
+import { LaunchAnim } from '@app/components/common/Animation';
 
 const XinvMigrationMessage = () => {
   const symbol = process.env.NEXT_PUBLIC_REWARD_TOKEN_SYMBOL
@@ -24,7 +24,7 @@ const MessageWithLink = ({ href, msg }: { href: string, msg: string }) => {
   return <Link
     pl={1}
     color="#fff"
-    isExternal={ href.startsWith('http') ? true : false }
+    isExternal={href.startsWith('http') ? true : false}
     href={href}
     _hover={{ color: 'purple.100' }}
   >
@@ -71,7 +71,11 @@ export const Announcement = ({ isLanding = false }: { isLanding: boolean }) => {
           <Text>{process.env.NEXT_PUBLIC_ANNOUNCEMENT_MSG}</Text>
       } */}
       <PlusAnimIcon boxProps={{ mr: '4' }} width={30} height={30} loop={true} />
-      <Countdown />
+      {/* <Countdown /> */}
+      <Box mx="20">
+        <LaunchAnim width={30} height={30} loop={true} />
+        <Text fontSize="12px">INV+ is Launched!</Text>
+      </Box>
       <PlusAnimIcon boxProps={{ ml: '4' }} width={30} height={30} loop={true} />
     </Flex>
   )
