696a938be25e7fb9715d78c74ef719fdd66e9c3d
webmass <mextrasis@hotmail.com>
2022-02-07 17:36:20 +0100

    Transparency: add interest-rate section

---
+++ components/Transparency/InterestModelChart.tsx
@@ -0,0 +1,92 @@
+import { shortenNumber } from '@app/util/markets';
+import { VictoryChart, VictoryLabel, VictoryAxis, VictoryArea, VictoryTheme, VictoryClipContainer, VictoryTooltip, VictoryVoronoiContainer, VictoryAreaProps, VictoryAxisProps } from 'victory';
+import { Box, useMediaQuery } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+
+type Props = { x: number, y: number }[]
+
+const defaultAxisStyle: VictoryAxisProps["style"] = {
+    tickLabels: { fill: '#fff' },
+    grid: {
+        stroke: '#666666aa',
+        strokeDasharray: '4 4',
+    }
+}
+
+export const InterestModelChart = ({
+    data,
+    title,
+    width = 900,
+    height = 300,
+    showLabels = false,
+    showTooltips = false,
+    interpolation = 'basis',
+    axisStyle = defaultAxisStyle,
+}: {
+    data: Props,
+    title?: string,
+    width?: number,
+    height?: number,
+    showLabels?: boolean,
+    showTooltips?: boolean,
+    interpolation?: VictoryAreaProps["interpolation"],
+    axisStyle?: VictoryAxisProps["style"],
+}) => {
+    const [isLargerThan] = useMediaQuery('(min-width: 900px)');
+    const [rightPadding, setRightPadding] = useState(50);
+    const maxY = data.length > 0 ? Math.max(...data.map(d => d.y)) : 95000000;
+
+    useEffect(() => {
+        setRightPadding(isLargerThan ? 50 : 20)
+    }, [isLargerThan]);
+
+    return (
+        <Box
+            width={width}
+            height={height}
+            position="relative"
+        >
+            <VictoryChart
+                width={width}
+                height={height}
+                theme={VictoryTheme.grayscale}
+                animate={{ duration: 500 }}
+                padding={{ top: 50, bottom: 50, left: 50, right: rightPadding }}
+                containerComponent={!showTooltips ? undefined :
+                    <VictoryVoronoiContainer
+                        mouseFollowTooltips={true}
+                        voronoiDimension="x"
+                        labelComponent={<VictoryTooltip centerOffset={{ x: -50 }} cornerRadius={0} flyoutStyle={{ fill: '#8881c9' }} />}
+                        // style={{ labels: { fill: 'white' } }}
+                        labels={({ datum }) => {
+                            return (
+                                `For ${datum.x}% Utilisation Rate\n=> ${shortenNumber(datum.y, 2)}% Intest Rate`
+                            )
+                        }}
+                    />
+                }
+            >
+                {
+                    !!title && <VictoryLabel text={title} style={{ fill: 'white' }} x={Math.floor(width / 2)} y={30} textAnchor="middle" />
+                }
+                <VictoryAxis style={axisStyle} dependentAxis tickFormat={(t) => shortenNumber(t, 1)} />
+                <VictoryAxis style={axisStyle} />
+                <VictoryArea
+                    groupComponent={<VictoryClipContainer clipId="area-chart" />}
+                    data={data}
+                    labels={
+                        ({ data, index }) => {
+                            const isMax = (maxY === data[index].y && index > 0 && maxY !== data[index - 1].y);
+                            return showLabels || isMax ? `${isMax && 'Max: '}${shortenNumber(data[index].y, 1)}` : ''
+                        }
+                    }
+                    style={{
+                        data: { fillOpacity: 0.9, fill: 'url(#primary-gradient)', stroke: '#8881c9', strokeWidth: 1 },
+                        labels: { fill: 'white' }
+                    }}
+                    interpolation={interpolation}
+                />
+            </VictoryChart>
+        </Box >
+    )
+}
---
+++ pages/transparency/interest-model.tsx
@@ -0,0 +1,95 @@
+import { Box, Flex, Text, useMediaQuery } from '@chakra-ui/react'
+
+import Layout from '@app/components/common/Layout'
+import { AppNav } from '@app/components/common/Navbar'
+import Head from 'next/head'
+import { TransparencyTabs } from '@app/components/Transparency/TransparencyTabs'
+import { useEffect, useState } from 'react'
+import { InterestModelChart } from '@app/components/Transparency/InterestModelChart'
+import { ShrinkableInfoMessage } from '@app/components/common/Messages'
+import Link from '@app/components/common/Link'
+
+const KINK = 75;
+const multiplierPerBlock = 6;
+const jumpMultiplierPerYear = 168.75;
+const utilisationRates = [...Array(100).keys()];
+
+export const InterestModelPage = () => {
+    const [chartWidth, setChartWidth] = useState<number>(900);
+    const [isLargerThan] = useMediaQuery('(min-width: 900px)')
+
+    useEffect(() => {
+        setChartWidth(isLargerThan ? 900 : (screen.availWidth || screen.width) - 40)
+    }, [isLargerThan]);
+
+    const borrowChartData = utilisationRates.map((rate) => {
+        const normalRate = rate / 100 * multiplierPerBlock;
+        const jumpedRate = (rate / 100 - KINK / 100) * jumpMultiplierPerYear + normalRate;
+        return { x: rate, y: rate <= KINK ? normalRate : jumpedRate }
+    })
+
+    const lendingChartData = borrowChartData.map(data => {
+        return { x: data.x, y: data.y * 0.8 }
+    })
+
+    return (
+        <Layout>
+            <Head>
+                <title>{process.env.NEXT_PUBLIC_TITLE} - Interest Rate Model</title>
+            </Head>
+            <AppNav active="Transparency" />
+            <TransparencyTabs active="interest-model" />
+            <Flex w="full" justify="center" direction={{ base: 'column', xl: 'row' }}>
+                <Flex direction="column">
+                    <InterestModelChart
+                        title={`Borrowing Interest Rate Model`}
+                        showTooltips={true}
+                        height={300}
+                        width={chartWidth}
+                        data={borrowChartData}
+                        interpolation={'basis'}
+                    />
+                    <InterestModelChart
+                        title={`Lending Interest Rate Model`}
+                        showTooltips={true}
+                        height={300}
+                        width={chartWidth}
+                        data={lendingChartData}
+                        interpolation={'basis'}
+                    />
+                </Flex>
+                <Flex direction="column" p={{ base: '4', xl: '0' }} ml="2">
+                    <ShrinkableInfoMessage
+                        title="📈 Interest Model Parameters"
+                        description={
+                            <>
+                                <Flex direction="row" w='full' justify="space-between">
+                                    <Text fontWeight="bold">- Kink:</Text>
+                                    <Text>{KINK}</Text>
+                                </Flex>
+                                <Flex direction="row" w='full' justify="space-between">
+                                    <Text fontWeight="bold">- Multiplier Per Block:</Text>
+                                    <Text>{multiplierPerBlock}</Text>
+                                </Flex>
+                                <Flex direction="row" w='full' justify="space-between">
+                                    <Text fontWeight="bold">- Jump Multiplier Per Year:</Text>
+                                    <Text>{jumpMultiplierPerYear}</Text>
+                                </Flex>
+                                <Flex direction="row" w='full' justify="space-between">
+                                    <Text fontWeight="bold">- Lending IR = Borrowing IR * 0.8</Text>
+                                </Flex>
+                                <Box mt="2">
+                                    <Link isExternal href="https://docs.inverse.finance/inverse-finance/technical/interest-rate-model">
+                                        Learn more about how the Interest Model works
+                                    </Link>
+                                </Box>
+                            </>
+                        }
+                    />
+                </Flex>
+            </Flex>
+        </Layout>
+    )
+}
+
+export default InterestModelPage
