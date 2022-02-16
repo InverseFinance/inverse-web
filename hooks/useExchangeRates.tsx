0d8d1328cf5dd00fb65886ccdfc92250a1d605a2
webmass <mextrasis@hotmail.com>
2022-02-16 14:50:33 +0100

    Anchor: fix rtoken disabled case in hooks

--- hooks/useExchangeRates.tsx
+++ hooks/useExchangeRates.tsx
@@ -4,6 +4,7 @@ import { BigNumber } from 'ethers'
 import { useWeb3React } from '@web3-react/core';
 import { Web3Provider } from '@ethersproject/providers';
 import { getNetworkConfigConstants } from '@app/util/networks';
+import { HAS_REWARD_TOKEN } from '@app/config/constants';
 
 type ExchangeRates = {
   exchangeRates: { [key: string]: BigNumber }
@@ -13,7 +14,7 @@ export const useExchangeRates = (): SWR & ExchangeRates => {
   const { chainId } = useWeb3React<Web3Provider>()
   const { ANCHOR_TOKENS, XINV, XINV_V1 } = getNetworkConfigConstants(chainId)
 
-  const tokens = ANCHOR_TOKENS.concat(XINV ? [XINV] : []).concat(XINV_V1 ? [XINV_V1] : [])
+  const tokens = ANCHOR_TOKENS.concat(HAS_REWARD_TOKEN && XINV ? [XINV] : []).concat(HAS_REWARD_TOKEN && XINV_V1 ? [XINV_V1] : [])
 
   const { data, error } = useEtherSWR(tokens.map((address: string) => [address, 'exchangeRateStored']))
 
