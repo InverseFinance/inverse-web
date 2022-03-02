a0241438c0fbc98c741d26e5fc06e2449b499548
webmass <mextrasis@hotmail.com>
2022-03-02 17:59:43 +0100

    Util: timestamps

---
+++ util/timestamps.ts
@@ -0,0 +1,29 @@
+
+import { BLOCK_TIMESTAMPS } from '@app/config/blocknumber-timestamps-archived';
+import { getRedisClient } from '@app/util/redis';
+import { mergeDeep } from './misc';
+import { getProvider } from './providers';
+
+const client = getRedisClient();
+
+export const addBlockTimestamps = async (blockNumbers: number[], chainId: string) => {
+    const blockTimestamps = await getCachedBlockTimestamps();
+    const cachedBns = Object.keys(blockTimestamps[chainId]);
+
+    const uniqueBns = [...new Set(blockNumbers)].filter(v => !cachedBns.includes(v.toString()));
+
+    const provider = getProvider(chainId);
+    const results = await Promise.all(
+        uniqueBns.map(blockNumber => provider.getBlock(blockNumber))
+    );
+    results.forEach(r => {
+        // in secs
+        blockTimestamps[chainId][r.number] = r.timestamp;
+    });
+    await client.set('block-timestamps', JSON.stringify(blockTimestamps));
+}
+
+export const getCachedBlockTimestamps = async () => {
+    const cachedBlockTimestamps: { [key: string]: { [key: string]: number } } = JSON.parse(await client.get('block-timestamps') || '{}');
+    return mergeDeep(BLOCK_TIMESTAMPS, cachedBlockTimestamps);
+}
