3382b2f9215a87652edbf35c30072990d26ac276
webmass <mextrasis@hotmail.com>
2022-03-15 13:56:39 +0100

    Proposals: more HumanReadable actions

--- components/Governance/ProposalActionPreview.tsx
+++ components/Governance/ProposalActionPreview.tsx
@@ -3,13 +3,47 @@ import { AbiCoder, commify, FunctionFragment, isAddress } from 'ethers/lib/utils
 import { Stack, Flex, Text, StackProps } from '@chakra-ui/react';
 import Link from '@app/components/common/Link'
 import { namedAddress } from '@app/util';
-import { TOKENS } from '@app/variables/tokens';
+import { TOKENS, UNDERLYING } from '@app/variables/tokens';
 import { capitalize } from '@app/util/misc';
 import { formatUnits } from 'ethers/lib/utils';
 import { getNetworkConfigConstants } from '@app/util/networks';
 import ScannerLink from '../common/ScannerLink';
 
 const { DOLA_PAYROLL, DOLA } = getNetworkConfigConstants();
+
+const Amount = ({ value, decimals }: { value: string, decimals: number }) => {
+    return <Text display="inline-block" fontWeight="bold" color="secondary">
+        {commify(formatUnits(value, decimals)).replace(/\.0$/, '')}
+    </Text>;
+}
+
+const AnchorHumanReadableActionLabel = ({
+    target,
+    signature,
+    callDatas,
+}: {
+    target: string,
+    signature: string,
+    callDatas: string[],
+}) => {
+    const contractKnownToken = UNDERLYING[target];
+
+    const funName = signature.split('(')[0];
+    let text;
+    
+    if (['_reduceReserves', '_addReserves'].includes(funName)) {
+        const amount = <Amount value={callDatas[0]} decimals={contractKnownToken.decimals} />;
+        text = <Flex display="inline-block">
+            <b>{funName === '_addReserves' ? 'Add' : 'Reduce'}</b> <b>{contractKnownToken.symbol}</b> Anchor Market's <b>Reserves</b> by {amount}
+        </Flex>
+    }
+
+    return (
+        <Flex display="inline-block" mb="2" fontStyle="italic">
+            &laquo; {text} &raquo;
+        </Flex>
+    )
+}
 
 const HumanReadableActionLabel = ({
     target,
@@ -20,29 +54,32 @@ const HumanReadableActionLabel = ({
     signature: string,
     callDatas: string[],
 }) => {
+    if (UNDERLYING[target]) {
+        return <AnchorHumanReadableActionLabel target={target} signature={signature} callDatas={callDatas} />;
+    }
     const contractKnownToken = target === DOLA_PAYROLL ? TOKENS[DOLA] : TOKENS[target];
 
-    const destinator = <ScannerLink value={callDatas[0]} label={namedAddress(callDatas[0])} />;
+    const destinator = <ScannerLink color="info" value={callDatas[0]} label={namedAddress(callDatas[0])} />;
     const funName = signature.split('(')[0];
-    const symbol = contractKnownToken.symbol;
-    
-    const amount = `${commify(formatUnits(callDatas[1], contractKnownToken.decimals)).replace(/\.0$/, '')}`;
+    const symbol = <Text fontWeight="bold" display="inline-block">{contractKnownToken.symbol}</Text>;
+
+    const amount = <Amount value={callDatas[1]} decimals={contractKnownToken.decimals} />
 
     let text;
 
-    if(target === DOLA_PAYROLL) {
-        text = <Flex>Add ${destinator} to the PayRolls with a yearly salary of ${amount} ${symbol}</Flex>
-    } else if(funName === 'approve') {
-        text = `Set ${destinator}'s ${symbol} Allowance to ${amount}`;
+    if (target === DOLA_PAYROLL) {
+        text = <Flex display="inline-block">Add {destinator} to the <b>PayRolls</b> with a yearly salary of {amount} {symbol}</Flex>
+    } else if (funName === 'approve') {
+        text = <Flex display="inline-block">Set {destinator}'s {symbol} <b>Allowance</b> to {amount}</Flex>;
     } else {
-        text = `${capitalize(funName)} ${amount} ${symbol} to ${destinator}`;
+        text = <Flex display="inline-block"><b>{capitalize(funName)}</b> {amount} {symbol} to {destinator}</Flex>;
     }
 
     return (
-        <Flex mb="2" fontStyle="italic">
-            &laquo; {text} &raquo; 
+        <Flex display="inline-block" mb="2" fontStyle="italic">
+            &laquo; {text} &raquo;
         </Flex>
-    ) 
+    )
 }
 
 
@@ -59,8 +96,8 @@ export const ProposalActionPreview = (({
         .split(',');
 
     const funName = signature.split('(')[0];
-    const isHumanRedeableCaseHandled = ['approve', 'transfer', 'mint', 'addRecipient'].includes(funName);
-    const contractKnownToken = target === DOLA_PAYROLL ? TOKENS[DOLA] : TOKENS[target];
+    const isHumanRedeableCaseHandled = ['approve', 'transfer', 'mint', 'addRecipient', '_reduceReserves', '_addReserves'].includes(funName);
+    const contractKnownToken = target === DOLA_PAYROLL ? TOKENS[DOLA] : TOKENS[target] || UNDERLYING[target];
 
     return (
         <Stack w="full" spacing={1} {...props} textAlign="left">
