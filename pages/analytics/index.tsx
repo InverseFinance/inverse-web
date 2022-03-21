935c42bc5cd65fd1bfd07e2e2095a368387c428d
webmass <mextrasis@hotmail.com>
2022-03-21 15:30:57 +0100

    Analytics: minor changes

--- pages/analytics/index.tsx
+++ pages/analytics/index.tsx
@@ -5,13 +5,13 @@ import Head from 'next/head';
 
 export const Analytics = () => {
   return (
-    <Layout>
+    <Layout bg="#100e21" bgColor="#100e21">
       <Head>
         <title>{process.env.NEXT_PUBLIC_TITLE} - Analytics</title>
       </Head>
       <AppNav active="INV" />
-      <Flex direction="column" w={{ base: 'full' }} p="5">
-        <iframe width="100%" height="900px" src="https://datastudio.google.com/embed/reporting/cb58a483-78a0-4f08-9625-25ea42a2bd12/page/Iz7jC"></iframe>
+      <Flex direction="column" w={{ base: 'full' }} p="0" m="0">
+        <iframe width="100%" height="700px" src="https://datastudio.google.com/embed/reporting/cb58a483-78a0-4f08-9625-25ea42a2bd12/page/Iz7jC"></iframe>
       </Flex>
     </Layout>
   )
