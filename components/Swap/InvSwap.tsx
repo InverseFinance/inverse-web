f1711e9887ec602a83291b90587bd6ef51ec7e11
webmass <mextrasis@hotmail.com>
2022-03-22 12:39:41 +0100

    InvSwap: handle not connected ui

--- components/Swap/InvSwap.tsx
+++ components/Swap/InvSwap.tsx
@@ -2,7 +2,7 @@ import { Flex } from '@chakra-ui/react'
 import Container from '@app/components/common/Container'
 import { useWeb3React } from '@web3-react/core';
 import { Web3Provider } from '@ethersproject/providers';
-import { ShrinkableInfoMessage } from '@app/components/common/Messages';
+import { InfoMessage } from '@app/components/common/Messages';
 
 const url = 'https://app.sushi.com/swap?inputCurrency=0x865377367054516e17014CcdED1e7d814EDC9ce4&outputCurrency=0x41D5D79431A913C4aE7d69a668ecdfE5fF9DFB68';
 
@@ -38,36 +38,57 @@ export const InvSwap = ({
         >
             <Flex
                 overflow="hidden"
-                minH={!!account ? '495px' : 'auto'}
+                minH={'495px'}
                 borderRadius='10px'
                 width="full"
-                minWidth= '300px'
+                minWidth='300px'
+                position="relative"
                 w='full'>
                 {
-                    !account ?
-                        <ShrinkableInfoMessage
-                            description="Please Connect Your Wlallet"
+                    !account && <Flex 
+                    alignItems="center"
+                    position= 'absolute'
+                    justify="center"
+                    zIndex= '9'
+                    minWidth= '300px'
+                    minH= '300px'
+                    margin= 'auto'
+                    top= '0'
+                    bottom= '0'
+                    left= '0'
+                    right= '0'
+                    >
+                        <InfoMessage alertProps={{
+                            fontSize: '16px',
+                            position: 'absolute',
+                            p: '5',
+                            top: '30%',
+                            boxShadow: '0 0 4px 1px #aaa',
+                        }}
+                            title="Please Connect Your Wallet"
                         />
-                        :
-                        <iframe
-                            src={url}
-                            height={minH}
-                            width="100%"
-                            scrolling="no"
-                            id="swapIframe"
-                            style={{
-                                transform: 'translateY(-43px)',
-                                border: '0',
-                                margin: '0 auto',
-                                display: 'block',
-                                borderRadius: '10px',
-                                height: minH,
-                                width: 'full',
-                                minWidth: '300px',
-                                overflow: 'hidden',
-                            }}
-                        />
+                    </Flex>
                 }
+                <iframe
+                    src={url}
+                    height={minH}
+                    width="100%"
+                    scrolling="no"
+                    id="swapIframe"
+                    style={{
+                        opacity: !account ? '0.1' : '1',
+                        transform: 'translateY(-43px)',
+                        border: '0',
+                        margin: '0 auto',
+                        display: 'block',
+                        borderRadius: '10px',
+                        height: minH,
+                        width: 'full',
+                        minWidth: '300px',
+                        overflow: 'hidden',
+                    }}
+                />
+
             </Flex>
         </Container>
     )
