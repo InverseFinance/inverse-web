d9f67bdee667b91b3dc73b49547c4c0b8846c8ef
webmass <mextrasis@hotmail.com>
2022-03-29 10:42:17 +0200

    Apis: add dola supply and circulating supply

---
+++ pages/api/dola/circulating-supply.ts
@@ -0,0 +1,63 @@
+import { Contract } from 'ethers'
+import 'source-map-support'
+import { INV_ABI } from '@app/config/abis'
+import { getNetworkConfig } from '@app/util/networks'
+import { getProvider } from '@app/util/providers';
+import { getCacheFromRedis, redisSetWithTimestamp } from '@app/util/redis'
+import { getNetworkConfigConstants } from '@app/util/networks';
+import { getBnToNumber } from '@app/util/markets'
+
+const {
+  TREASURY,
+  OP_BOND_MANAGER,
+} = getNetworkConfigConstants();
+
+const excluded = [
+  TREASURY,
+  OP_BOND_MANAGER,
+];
+
+export default async function handler(req, res) {
+  const networkConfig = getNetworkConfig(process.env.NEXT_PUBLIC_CHAIN_ID!, true)!;
+  const cacheKey = `${networkConfig.chainId}-dola-circ-supply-v1.0.0`;
+
+  try {
+    const validCache = await getCacheFromRedis(cacheKey, true, 30);
+
+    if (validCache) {
+      res.status(200).send(validCache);
+      return
+    }
+
+    const provider = getProvider(networkConfig.chainId);
+    const contract = new Contract(process.env.NEXT_PUBLIC_DOLA!, INV_ABI, provider);
+
+    const [totalSupply, ...excludedBalances] = await Promise.all([
+      contract.totalSupply(),
+      ...excluded.map(excludedAd => contract.balanceOf(excludedAd)),
+    ]);
+
+    const totalInvExcluded = excludedBalances
+      .map(bn => getBnToNumber(bn))
+      .reduce((prev, curr) => prev + curr, 0);
+
+    const circulatingSupply = getBnToNumber(totalSupply) - totalInvExcluded;
+
+    await redisSetWithTimestamp(cacheKey, circulatingSupply);
+
+    res.status(200).send(circulatingSupply);
+  } catch (err) {
+    console.error(err);
+    // if an error occured, try to return last cached results
+    try {
+      const cache = await getCacheFromRedis(cacheKey, false);
+      if (cache) {
+        console.log('Api call failed, returning last cache found');
+        res.status(200).send(cache);
+      }
+    } catch (e) {
+      console.error(e);
+      res.status(500);
+    }
+  }
+}
---
+++ pages/api/dola/supply.ts
@@ -0,0 +1,43 @@
+import { Contract } from 'ethers'
+import { formatEther } from 'ethers/lib/utils'
+import 'source-map-support'
+import { INV_ABI } from '@app/config/abis'
+import { getNetworkConfig } from '@app/util/networks'
+import { getProvider } from '@app/util/providers';
+import { getCacheFromRedis, redisSetWithTimestamp } from '@app/util/redis'
+
+export default async function handler(req, res) {
+  const networkConfig = getNetworkConfig(process.env.NEXT_PUBLIC_CHAIN_ID!, true)!;
+  const cacheKey = `${networkConfig.chainId}-dola-supply-v1.0.0`;
+
+  try {
+    const validCache = await getCacheFromRedis(cacheKey, true, 5);
+    if(validCache) {
+      res.status(200).send(validCache);
+      return
+    }
+
+    const provider = getProvider(networkConfig.chainId);
+    const contract = new Contract(process.env.NEXT_PUBLIC_DOLA!, INV_ABI, provider);
+
+    const result = await contract.totalSupply()
+
+    const totalSupply = formatEther(result);
+
+    await redisSetWithTimestamp(cacheKey, totalSupply);
+
+    res.status(200).send(totalSupply);
+  } catch (err) {
+    console.error(err);
+    // if an error occured, try to return last cached results
+    try {
+      const cache = await getCacheFromRedis(cacheKey, false);
+      if(cache) {
+        console.log('Api call failed, returning last cache found');
+        res.status(200).send(cache);
+      }
+    } catch(e) {
+      console.error(e);
+    }
+  }
+}
