961e3815964877dc4738fafba998cce24bfb4376
webmass <mextrasis@hotmail.com>
2022-04-08 11:07:19 +0200

    Blog init with template and posts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import '../blog/styles/index.css'
 import './polyfill.css'
 import '@fontsource/inter/100.css'
 import '@fontsource/inter/200.css'
---
+++ pages/blog/index.js
@@ -0,0 +1,43 @@
+import Container from '../../blog/components/container'
+import MoreStories from '../../blog/components/more-stories'
+import HeroPost from '../../blog/components/hero-post'
+import Intro from '../../blog/components/intro'
+import Layout from '../../blog/components/layout'
+import { getAllPostsForHome } from '../../blog/lib/api'
+import Head from 'next/head'
+import { CMS_NAME } from '../../blog/lib/constants'
+
+export default function Index({ preview, allPosts }) {
+  const heroPost = allPosts[0]
+  const morePosts = allPosts.slice(1)
+  return (
+    <>
+      <Layout preview={preview}>
+        <Head>
+          <title>Next.js Blog Example with {CMS_NAME}</title>
+        </Head>
+        <Container>
+          <Intro />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export async function getStaticProps({ preview = false }) {
+  const allPosts = (await getAllPostsForHome(preview)) ?? []
+  return {
+    props: { preview, allPosts },
+  }
+}
