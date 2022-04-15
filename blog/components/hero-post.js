e19f1266c702abf1dcaac5f29a318ace8d4c1fc6
webmass <mextrasis@hotmail.com>
2022-04-15 10:33:47 +0200

    Blog: reorganise hero-post

--- blog/components/hero-post.js
+++ blog/components/hero-post.js
@@ -4,9 +4,8 @@ import CoverImage from '../components/cover-image'
 import { useContext } from 'react'
 import { BlogContext } from '../../pages/blog/[...slug]'
 import TagsBar from './tags-bar'
-import { Stack, VStack} from '@chakra-ui/react'
+import { Stack, VStack } from '@chakra-ui/react'
 import Excerpt from './excerpt'
-import BlogText from './common/text'
 import BlogLink from './common/blog-link'
 
 export default function HeroPost({
@@ -23,24 +22,24 @@ export default function HeroPost({
   const { locale } = useContext(BlogContext)
   const url = `/blog/posts/${locale}/${slug}`;
   return (
-    <Stack as="section" direction={{ base: 'column', lg: 'column' }} w='full' maxWidth="900px" spacing="4">
-      <VStack justifyContent="space-between" alignItems="flex-start">
-        <BlogLink href={url} w='full' as="h3" fontSize="4xl" fontWeight="extrabold">
-          {title}
-        </BlogLink>
-        {author && <Avatar name={author.name} picture={author.picture} title={author.title} />}
-        <Stack w='full' direction={{ base: 'column', lg: 'row' }} justifyContent="space-between">
-          <DateComponent dateString={date} readtime={readtime} />
-          <TagsBar tagsCollection={tagsCollection} />
-        </Stack>
-      </VStack>
+    <VStack as="section" w='full' maxWidth="900px" spacing="4">
+
+      <BlogLink href={url} w='full' as="h3" fontSize="4xl" fontWeight="extrabold">
+        {title}
+      </BlogLink>
+
       <VStack alignItems="flex-start" spacing="5">
         {
           !!coverImage?.url &&
           <CoverImage title={title} slug={slug} url={coverImage.url} maxHeight={'300px'} />
         }
         <Excerpt excerpt={excerpt} content={content} url={url} />
+        <Stack w='full' direction={{ base: 'column', lg: 'row' }} justifyContent="space-between">
+          <DateComponent dateString={date} readtime={readtime} />
+          <TagsBar tagsCollection={tagsCollection} />
+        </Stack>
+        {author && <Avatar name={author.name} picture={author.picture} title={author.title} />}
       </VStack>
-    </Stack>
+    </VStack>
   )
 }
