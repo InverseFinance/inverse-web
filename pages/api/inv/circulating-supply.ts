883097bfe42b750a2fd02dd349e3c91388781172
webmass <mextrasis@hotmail.com>
2022-04-21 11:45:31 +0200

    Inv: circulating supply easier scale with vesters

--- pages/api/inv/circulating-supply.ts
+++ pages/api/inv/circulating-supply.ts
@@ -1,6 +1,6 @@
 import { Contract } from 'ethers'
 import 'source-map-support'
-import { INV_ABI, XINV_ABI } from '@app/config/abis'
+import { INV_ABI, VESTER_FACTORY_ABI, XINV_ABI } from '@app/config/abis'
 import { getNetworkConfig } from '@app/util/networks'
 import { getProvider } from '@app/util/providers';
 import { getCacheFromRedis, redisSetWithTimestamp } from '@app/util/redis'
@@ -10,7 +10,7 @@ import { getBnToNumber } from '@app/util/markets'
 const {
   TREASURY,
   OP_BOND_MANAGER,
-  VESTERS,
+  XINV_VESTOR_FACTORY,
 } = getNetworkConfigConstants();
 
 const excluded = [
@@ -22,7 +22,9 @@ const excluded = [
   '0x5c1245F9dB3f8f7Fe1208cB82325eA88fC11Fe89',
 ];
 
-const xinvExcluded = VESTERS;
+// increase number when a new proposal adds new vesters, can be more but not less than the nb of vesters deployed
+// contract factory does not have data of the numbers of vesters
+const vestersToCheck = [...Array(14).keys()];
 
 export default async function handler(req, res) {
   const networkConfig = getNetworkConfig(process.env.NEXT_PUBLIC_CHAIN_ID!, true)!;
@@ -47,8 +49,15 @@ export default async function handler(req, res) {
       ...excluded.map(excludedAd => contract.balanceOf(excludedAd)),
     ]);
 
+    const vesterFactory = new Contract(XINV_VESTOR_FACTORY, VESTER_FACTORY_ABI, provider);
+    const vestersResults = await Promise.allSettled([
+      ...vestersToCheck.map((v, i) => vesterFactory.vesters(i))
+    ])
+  
+    const vesters = vestersResults ? vestersResults.filter(r => r.status === 'fulfilled').map(r => r.value) : [];
+
     const xinvExcludedBalances = await Promise.all([
-      ...xinvExcluded.map(excludedAd => xinvContract.balanceOf(excludedAd)),
+      ...vesters.map(excludedAd => xinvContract.balanceOf(excludedAd)),
     ]);
 
     const totalInvExcluded = xinvExcludedBalances
