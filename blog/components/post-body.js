de936abae80b593de1860e9f0f535b1442a66633
webmass <mextrasis@hotmail.com>
2022-04-26 11:29:37 +0200

    Blog: handle twitter and youtube "watch" link type

--- blog/components/post-body.js
+++ blog/components/post-body.js
@@ -1,6 +1,7 @@
 import { Box } from '@chakra-ui/react'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { BLOCKS, INLINES } from '@contentful/rich-text-types'
+import { useEffect } from 'react'
 import markdownStyles from './markdown-styles.module.css'
 import RichTextAsset from './rich-text-asset'
 
@@ -25,6 +26,15 @@ const customMarkdownOptions = (content) => ({
         return <IframeContainer><iframe src={node.data.uri} frameBorder="0" allowFullScreen></iframe></IframeContainer>
       } else if ((node.data.uri).includes("youtube.com/embed")) {
         return <IframeContainer><iframe src={node.data.uri} allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" frameBorder="0" allowFullScreen></iframe></IframeContainer>
+      } else if ((node.data.uri).includes("youtube.com/watch") && !!node.data.uri.match(/v=([A-Za-z0-9]+)/)) {
+        const videoId = node.data.uri.match(/v=([A-Za-z0-9]+)/)[1]
+        const embedUri = `https://youtube.com/embed/${videoId}`
+        return <IframeContainer><iframe src={embedUri} allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" frameBorder="0" allowFullScreen></iframe></IframeContainer>
+      } else if (node.data.uri.includes('https://twitter.com') && node.data.uri.includes('/status/')) {
+        const xLink = node.data.uri.replace(/(https:\/\/twitter\.com\/)([a-zA-Z0-9]+)/i, '$1x');
+        return <blockquote className="twitter-tweet" style={{ width: '100%', border: '1px solid red' }}>
+          <a href={`${xLink}`}>{xLink}</a>
+        </blockquote>
       } else {
         return <a href={node.data.uri} target="_blank">{node.content[0].value}</a>
       }
@@ -33,6 +43,20 @@ const customMarkdownOptions = (content) => ({
 })
 
 export default function PostBody({ content }) {
+
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = "https://platform.twitter.com/widgets.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
   return (
     <div className="max-w-4xl mx-auto blog-post-body">
       <div className={markdownStyles['markdown']}>
