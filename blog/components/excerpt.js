df9671fd6dde91f6c81ace754196dde87b6ea803
webmass <mextrasis@hotmail.com>
2022-05-05 12:11:36 +0200

    Blog: small ui changes

--- blog/components/excerpt.js
+++ blog/components/excerpt.js
@@ -2,10 +2,11 @@ import { BLOG_THEME } from '../lib/constants';
 import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer';
 import BlogLink from './common/blog-link';
 
-export default function Excerpt({ excerpt, content, url, charLimit = 200 }) {
+export default function Excerpt({ excerpt, content, url, charLimit = 256 }) {
+    const text = (excerpt || documentToPlainTextString(content.json))
     return (
         <BlogLink href={url} color={BLOG_THEME.colors.secondaryTextColor} cursor="pointer" fontWeight="normal" _hover={{}}>
-            {excerpt || (documentToPlainTextString(content.json).substring(0, charLimit) + '...')}
+            {text.substring(0, charLimit)}{text?.length > charLimit ? '...' : ''}
         </BlogLink>
     )
 }
