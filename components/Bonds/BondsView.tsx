786e7810773acb3ee5fb1ff3f69df6f95fa94ec6
webmass <mextrasis@hotmail.com>
2022-05-13 16:44:07 +0200

    Bonds: show paused msg when all maxPayout=0

--- components/Bonds/BondsView.tsx
+++ components/Bonds/BondsView.tsx
@@ -24,14 +24,12 @@ const LocalTooltip = ({ children }) => <AnimatedInfoTooltip
 
 export const BondsView = () => {
     const { account } = useWeb3React<Web3Provider>();
-    const { prices: oraclePrices } = useAnchorPricesUsd();
     const { prices: cgPrices } = usePrices();
     const { bonds } = useBonds();
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [selectedBondIndex, setSelectedBondIndex] = useState<number | null>(null);
     const [isNotConnected, setIsNotConnected] = useState(false);
 
-    const invOraclePrice = oraclePrices && oraclePrices[XINV];
     const invCgPrice = cgPrices && cgPrices[RTOKEN_CG_ID]?.usd;
 
     const handleDetails = (bondIndex: number) => {
@@ -43,6 +41,8 @@ export const BondsView = () => {
         setIsNotConnected(!account);
     }, [account], !account, 1000, 0);
 
+    const bondsPaused = bonds?.length > 0 && bonds?.reduce((prev, curr) => prev + curr.maxPayout, 0) === 0;
+
     return (
         <Stack w='full' color="mainTextColor">
             {
@@ -51,20 +51,20 @@ export const BondsView = () => {
                 </Container>
             }
             {selectedBondIndex !== null && <BondSlide handleDetails={handleDetails} isOpen={isOpen} onClose={onClose} bonds={bonds} bondIndex={selectedBondIndex} />}
-            <Container contentProps={{ p: { base: '2', sm: '8' } }} noPadding label="INV Market Price" contentBgColor="gradient3">
+            <Container contentProps={{ p: { base: '2', sm: '8' } }} noPadding label="Get INV at a discount thanks to Bonds!" contentBgColor="gradient3">
                 <VStack fontSize={{ base: '12px', sm: '14px' }} w="full" justify="space-between">
-                    <Text fontWeight="bold">
-                        The Return On Investment is calculated with the Coingecko Market Price, the INV Oracle Price which is updated less frequently is shown for convenience only.
+                    <Text>
+                        Bonds allow users to get <b>INV at a discount</b> in exchange for another asset with a <b>linear unlocking of 7 days</b>. It's a win-win situation as this lets Inverse Finance increase its <b>Protocol Owned Liquidity</b> instead of renting out its liquidity which is very expensive.
                     </Text>
                     <Flex w='full' pt="2" justify="space-between">
-                        <Flex direction={{ base: 'column', sm: 'row' }} alignItems="flex-end">
-                            <Text mr="1">Coingecko Market Price:</Text>
+                        <Flex direction={{ base: 'column', sm: 'row' }} alignItems="flex-start">
+                            <Text mr="1">INV Market Price:</Text>
                             <Text fontWeight="extrabold">{invCgPrice ? shortenNumber(invCgPrice, 2, true) : '-'}</Text>
                         </Flex>
-                        <Flex direction={{ base: 'column', sm: 'row' }}>
+                        {/* <Flex direction={{ base: 'column', sm: 'row' }}>
                             <Text mr="1">Oracle Market Price:</Text>
                             <Text fontWeight="extrabold">{invOraclePrice ? shortenNumber(invOraclePrice, 2, true) : '-'}</Text>
-                        </Flex>
+                        </Flex> */}
                     </Flex>
                 </VStack>
             </Container>
@@ -72,46 +72,51 @@ export const BondsView = () => {
             <Container
                 noPadding
                 contentProps={{ p: { base: '2', sm: '8' } }}
-                label="Bonds"
+                label="Available Bonds"
                 contentBgColor="gradient3"
                 description="Get INV at a discount via Olympus Pro  - Learn More about bonds here"
                 href="https://docs.inverse.finance/inverse-finance/providing-liquidity/olympus-pro-bonds"
             >
-                <VStack w='full' fontSize={{ base: '12px', sm: '20px' }}>
-                    <Stack display={{ base: 'none', sm: 'inline-flex' }} direction="row" w='full' justify="space-between" fontWeight="bold">
-                        <Flex w="240px" alignItems="center">
-                            <Text>
-                                Asset to Bond With
-                            </Text>
-                            <LocalTooltip>
-                                This is the asset you give to get INV in exchange
-                            </LocalTooltip>
-                        </Flex>
-                        <Flex w="80px" alignItems="center" textAlign="left">
-                            Price
-                            <LocalTooltip>
-                                If the Bond Price is lower than INV's market price then there is a positive ROI allowing you to get INV at a discount
-                            </LocalTooltip>
-                        </Flex>
-                        <Flex w="80px" justify="flex-end" alignItems="center">
-                            ROI
-                            <LocalTooltip>
-                                A <b>positive Return On Investment</b> means you get INV at a
-                                <Text display="inline-block" mx="1" fontWeight="bold" color="secondary">discount</Text>
-                                compared to INV market price !
-                                <Text mt="2">
-                                    ROI can turn negative when bond demand is too high
-                                </Text>
-                            </LocalTooltip>
-                        </Flex>
-                        <Flex w='80px'></Flex>
-                    </Stack>
-                    {
-                        bonds.map((bond, i) => {
-                            return <BondListItem key={bond.bondContract} bond={bond} bondIndex={i} handleDetails={handleDetails} />
-                        })
-                    }
-                </VStack>
+                {
+                    bondsPaused ?
+                        <InfoMessage description="Bonds are paused at the moment" />
+                        :
+                        <VStack w='full' fontSize={{ base: '12px', sm: '20px' }}>
+                            <Stack display={{ base: 'none', sm: 'inline-flex' }} direction="row" w='full' justify="space-between" fontWeight="bold">
+                                <Flex w="240px" alignItems="center">
+                                    <Text>
+                                        Asset to Bond With
+                                    </Text>
+                                    <LocalTooltip>
+                                        This is the asset you give to get INV in exchange
+                                    </LocalTooltip>
+                                </Flex>
+                                <Flex w="80px" alignItems="center" textAlign="left">
+                                    Price
+                                    <LocalTooltip>
+                                        If the Bond Price is lower than INV's market price then there is a positive ROI allowing you to get INV at a discount
+                                    </LocalTooltip>
+                                </Flex>
+                                <Flex w="80px" justify="flex-end" alignItems="center">
+                                    ROI
+                                    <LocalTooltip>
+                                        A <b>positive Return On Investment</b> means you get INV at a
+                                        <Text display="inline-block" mx="1" fontWeight="bold" color="secondary">discount</Text>
+                                        compared to INV market price !
+                                        <Text mt="2">
+                                            ROI can turn negative when bond demand is too high
+                                        </Text>
+                                    </LocalTooltip>
+                                </Flex>
+                                <Flex w='80px'></Flex>
+                            </Stack>
+                            {
+                                bonds.map((bond, i) => {
+                                    return <BondListItem key={bond.bondContract} bond={bond} bondIndex={i} handleDetails={handleDetails} />
+                                })
+                            }
+                        </VStack>
+                }
             </Container>
 
             <Container
