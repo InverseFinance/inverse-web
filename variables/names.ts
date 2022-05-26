ed9dbefe6726a5413514310ec38fb35e3848ef65
webmass <mextrasis@hotmail.com>
2022-05-26 21:20:07 +0200

    Names: add yellowtail dev

--- variables/names.ts
+++ variables/names.ts
@@ -57,6 +57,7 @@ const namedAddresses: { [key: string]: string } = {
     '0x27b8D1347bE36Ff697b48d1D74323E6a9f28eA6D': 'Ishita',
     '0x9F3614afb3Df9f899caDBFfaA05c6C908059F726': '0xMT',
     '0x1f7e8b2C4289Ff033A1Db980c9FDb40CCF29294f': 'yellowtail',
+    '0xC253Af7B80F3DD3d469bf4b94AA9e3df59e71e43': 'yellowtail2',
 }
 
 Object.entries(UNDERLYING).forEach(([key, value]) => {
