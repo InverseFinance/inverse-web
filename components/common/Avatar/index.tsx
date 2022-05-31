1c5d378d51d1cf9722b9146ae30c383561a48172
webmass <mextrasis@hotmail.com>
2022-05-31 12:27:30 +0200

    Avatar: replace inefficient Davatar component

--- components/common/Avatar/index.tsx
+++ components/common/Avatar/index.tsx
@@ -1,27 +1,31 @@
 import { Box, BoxProps } from '@chakra-ui/react'
-import Davatar, { DavatarProps } from '@davatar/react';
-import { NetworkIds } from '@app/types';
-import { AlchemyProvider } from '@ethersproject/providers';
+
 import { isAddress } from 'ethers/lib/utils';
+import { useEnsProfile } from '@app/hooks/useEnsProfile';
 
 export const Avatar = ({
   address,
   defaultAvatar = 'jazzicon',
   sizePx = 20,
-  provider,
   ...boxProps
 }: {
   address: string,
   sizePx?: number,
-  provider?: DavatarProps["provider"],
   defaultAvatar?: 'blockies' | 'jazzicon',
 } & Partial<BoxProps>) => {
-  // specific key for this usage only
-  const alchemyProvider = new AlchemyProvider(Number(NetworkIds.mainnet), process.env.NEXT_PUBLIC_ENS_ALCHEMY_API);
   const avatarAddress = !address || !isAddress(address) ? '0x0000000000000000000000000000000000000000' : address
+  const { ensProfile } = useEnsProfile(avatarAddress, true);
+
   return (
-    <Box boxSize={`${sizePx}px`} {...boxProps}>
-      <Davatar provider={provider || alchemyProvider} size={sizePx} address={avatarAddress} generatedAvatarType={defaultAvatar} />
+    <Box
+      boxSize={`${sizePx}px`}
+      backgroundImage={ensProfile?.avatar ? `url('${ensProfile?.avatar}')` : undefined}
+      backgroundSize="cover"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      borderRadius="50px"
+      {...boxProps}>
+        
     </Box>
   )
 }
