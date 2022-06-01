c45536cb6be98cad646b6c6c6b31cec69d775b2b
webmass <mextrasis@hotmail.com>
2022-06-01 16:53:28 +0200

    Frontier: xSushi yield on 7 days avg

--- util/markets.ts
+++ util/markets.ts
@@ -178,23 +178,46 @@ export const getStethData = async () => {
     return [];
 }
 
-export const getXSushiData = async () => {
+export const getXSushiData = async (nbDays = 7) => {
     let apy = 0;
-    let apr = 0;
+    const period = 365;
     try {
-        const [xsushi, sushiPriceUSD, daysData] = await Promise.all([
-            sushiData.bar.info(),
-            sushiData.sushi.priceUSD(),
+        const days = [...Array(nbDays).keys()];
+
+        const [daysData] = await Promise.all([
             sushiData.exchange.dayData(),
         ]);
+        const infos = await Promise.all([
+            ...days.map(v => {
+                const d = new Date();
+                const utc = Date.UTC(d.getFullYear(), d.getMonth(), d.getDate() -v, 0, 0, 0);
+                return sushiData.bar.info({ timestamp: utc });
+            }),
+        ]);
+        // const prices = await Promise.all([
+        //     ...days.map(v => {
+        //         const d = new Date();
+        //         const utc = Date.UTC(d.getFullYear(), d.getMonth(), d.getDate() -v, 0, 0, 0);
+        //         console.log(new Date(utc));
+        //         return sushiData.sushi.priceUSD({ timestamp: utc });
+        //     }),
+        // ]);
+        const prices = (await Promise.all([
+            ...days.map(v => {
+                const d = new Date();
+                const utc = Date.UTC(d.getFullYear(), d.getMonth(), d.getDate() -v, 0, 0, 0);
+                console.log(new Date(utc));
+                return sushiData.exchange.token({ timestamp: utc, token_address: '0x8798249c2e607446efb7ad49ec89dd1865ff4272' });
+            }),
+        ])).map(d => d.derivedETH);
 
-        const daysBeforeToday = daysData.slice(1, 3);
-        const avgVolume = daysBeforeToday.reduce((prev, curr) => prev + curr.volumeUSD, 0) / daysBeforeToday.length;
+        const apys = days.map((d, i) => {
+            const apr = (((daysData[i].volumeETH * 0.05 * 0.01) / infos[i].totalSupply) * period) / (infos[i].ratio * prices[i])
+            const apy = (Math.pow((1 + (apr / period)), period)) - 1;
+            return apy;
+        });
 
-        const period = 365;
-        // 0.05 * 0.01 = swap fees
-        apr = (((avgVolume * 0.05 * 0.01) / xsushi.totalSupply) * period) / (xsushi.ratio * sushiPriceUSD)
-        apy = (Math.pow((1 + (apr / period)), period)) - 1;
+        apy = apys.reduce((prev, curr) => prev + curr, 0)/apys.length;
     } catch (e) { console.log(e) }
-    return { apy: apy * 100, apr: apr * 100 };
+    return { apy: apy * 100 };
 }
