f974263e17a9214da5bc382c1b9334d1a6d392fd
webmass <mextrasis@hotmail.com>
2022-07-05 16:37:35 +0200

    Frontier: big button changes

--- components/Anchor/AnchorBigButton.tsx
+++ components/Anchor/AnchorBigButton.tsx
@@ -5,19 +5,22 @@ export const AnchorBigButton = (props: StackProps) => {
     return <VStack
         fontWeight="bold"
         cursor="pointer"
-        _hover={{ bg: "gradient2" }}
+        transitionProperty="transform"
+        transitionDuration="500ms"
+        _hover={{ transform: 'scale(1.05)' }}
         borderRadius="5px"
         alignItems="flex-start"
+        justify="center"
         color="mainTextColor"
+        textShadow={`2px 2px ${theme.colors.darkPrimary}`}
         bg="gradient1"
         textTransform="none"
         p="6"
         px="8"
         spacing="0"
         w="50%"
-        h="100px"
-        boxShadow={`0px 0px 5px 1px ${theme.colors.primaryPlus}`}
-        fontSize="20px"
+        h="150px"
+        fontSize="30px"
         {...props}
     />
 }
