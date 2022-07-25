2b6dd38345c0d33dac5e164fbba9f2827062fd03
webmass <mextrasis@hotmail.com>
2022-07-25 17:30:02 +0200

    Bonds stats: loader & minor adjustments

--- components/Bonds/BondsStatsView.tsx
+++ components/Bonds/BondsStatsView.tsx
@@ -4,6 +4,7 @@ import { useBondsDeposits } from '@app/hooks/useBonds';
 import { BondsAreaChart } from './BondsAreaChart';
 import { BondsBarChart } from './BondsBarChart';
 import theme from '@app/variables/theme';
+import { SkeletonBlob } from '@app/components/common/Skeleton';
 
 export const BondsStatsView = () => {
     const { deposits, acc } = useBondsDeposits();
@@ -60,36 +61,52 @@ export const BondsStatsView = () => {
     return (
         <Stack pt="8" spacing="10" w='full' color="mainTextColor" direction={{ base: 'column', lg: 'row' }}>
             <VStack w={{ base: 'full', lg: '50%' }} spacing="10">
-                <BondsAreaChart
-                    title={`Total INV exchanged for bonds over time (${shortenNumber(acc?.output)})`}
-                    chartData={invExchanged}
-                    showMaxY={false}
-                    mainColor="info"
-                />
                 {
-                    inputReceived.map(d => {
-                        return <BondsAreaChart
-                            title={`Total ${d.name} bonded over time (${shortenNumber(acc[d.name])})`}
-                            chartData={d.data}
-                            showMaxY={false}
-                            mainColor="secondary"
-                        />
-                    })
+                    !deposits?.length ?
+                        <SkeletonBlob />
+                        :
+                        <>
+                            <BondsAreaChart
+                                title={`Total INV exchanged for bonds over time (${shortenNumber(acc?.output)})`}
+                                chartData={invExchanged}
+                                showMaxY={false}
+                                mainColor="info"
+                            />
+                            {
+                                inputReceived.map(d => {
+                                    return <BondsAreaChart
+                                        key={d.name}
+                                        title={`Total ${d.name} bonded over time (${shortenNumber(acc[d.name])})`}
+                                        chartData={d.data}
+                                        showMaxY={false}
+                                        mainColor="secondary"
+                                    />
+                                })
+                            }
+                        </>
                 }
             </VStack>
             <VStack w={{ base: 'full', lg: '50%' }} spacing="10">
-                <BondsBarChart
-                    title={`Monthly INV exchanged for bonds`}
-                    chartData={invExchanged}
-                    colorScale={[theme.colors.info]}               
-                />
                 {
-                    inputReceived.map(d => {
-                        return <BondsBarChart
-                            title={`Monthly ${d.name} bonded`}
-                            chartData={d.data}
-                        />
-                    })
+                    !deposits?.length ?
+                        <SkeletonBlob />
+                        :
+                        <>
+                            <BondsBarChart
+                                title={`Monthly INV exchanged for bonds`}
+                                chartData={invExchanged}
+                                colorScale={[theme.colors.info]}
+                            />
+                            {
+                                inputReceived.map(d => {
+                                    return <BondsBarChart
+                                        key={d.name}
+                                        title={`Monthly ${d.name} bonded`}
+                                        chartData={d.data}
+                                    />
+                                })
+                            }
+                        </>
                 }
             </VStack>
         </Stack>
