b3f0bbac4ac23bc82e17491e69d88009c81868cd
webmass <mextrasis@hotmail.com>
2022-07-26 12:07:51 +0200

    Oppys: api, hook & type

--- hooks/useMarkets.tsx
+++ hooks/useMarkets.tsx
@@ -1,6 +1,6 @@
 import { getNetworkConfigConstants } from '@app/util/networks'
 import useEtherSWR from '@app/hooks/useEtherSWR'
-import { Market, SWR } from '@app/types'
+import { Market, SWR, YieldOppy } from '@app/types'
 import { fetcher } from '@app/util/web3'
 import { useWeb3React } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers';
@@ -35,6 +35,18 @@ export const useAccountMarkets = (address?: string): SWR & Markets => {
   return {
     markets:
       data && markets?.length ? data.map((address: string) => markets.find(({ token }) => token === address)).filter(v => !!v) : [],
+    isLoading: !error && !data,
+    isError: error,
+  }
+}
+
+export const useOppys = (): SWR & {
+  oppys: YieldOppy[]
+} => {
+  const { data, error } = useCustomSWR(`/api/oppys`, fetcher)
+
+  return {
+    oppys: data?.pools.sort((a,b) => b.tvlUsd - a.tvlUsd) || [],
     isLoading: !error && !data,
     isError: error,
   }
---
+++ pages/api/oppys.ts
@@ -0,0 +1,45 @@
+import 'source-map-support'
+import { getCacheFromRedis, redisSetWithTimestamp } from '@app/util/redis'
+import { getYieldOppys } from '@app/util/markets'
+
+export default async function handler(req, res) {
+  const cacheKey = `oppys-v1.0.0`;
+
+  try {
+    const validCache = await getCacheFromRedis(cacheKey, true, 60);
+    if (validCache) {
+      res.status(200).json(validCache);
+      return
+    }
+
+    const pools = await getYieldOppys();
+
+    const resultData = {
+      pools: pools.map(p => {
+        return {
+          ...p,
+          symbol: pools.symbol
+          .replace('-3CRV', '-3POOL')
+          .replace('DOLA-DAI+USDC', 'DOLA-2POOL')
+          ,
+        }
+      })
+    }
+
+    await redisSetWithTimestamp(cacheKey, resultData);
+
+    res.status(200).json(resultData)
+  } catch (err) {
+    console.error(err);
+    // if an error occured, try to return last cached results
+    try {
+      const cache = await getCacheFromRedis(cacheKey, false);
+      if (cache) {
+        console.log('Api call failed, returning last cache found');
+        res.status(200).json(cache);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
