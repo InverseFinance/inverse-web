1f031b5280a9d0de6e9f71c007b3a21292d176f4
webmass <mextrasis@hotmail.com>
2022-08-02 10:24:39 +0200

    Governance: add link to hero image

--- components/Governance/GovernanceInfos.tsx
+++ components/Governance/GovernanceInfos.tsx
@@ -1,16 +1,18 @@
 import { InfoMessage } from '@app/components/common/Messages'
-import { useDAO } from '@app/hooks/useDAO'
+import { Link } from '@app/components/common/Link'
 import { VStack, Image } from '@chakra-ui/react'
 import { GovernanceRules } from './GovernanceRules'
 
 export const GovernanceInfos = () => {
-    const { } = useDAO()
     return (
         <VStack w='full'>
-            <Image
-                borderRadius="5px"
-                src="https://images.ctfassets.net/kfs9y9ojngfc/6yAG6AVICeMaq6CPntNZqZ/d25e6524959cbba190f4af4b42dbfb83/cover-governance.png?w=3840&q=75"
-            />
+            <Link href="https://docs.inverse.finance/inverse-finance/governance" isExternal={true} target="_blank">
+                <Image
+                    cursor="pointer"
+                    borderRadius="5px"
+                    src="https://images.ctfassets.net/kfs9y9ojngfc/6yAG6AVICeMaq6CPntNZqZ/d25e6524959cbba190f4af4b42dbfb83/cover-governance.png?w=3840&q=75"
+                />
+            </Link>
             <InfoMessage
                 alertProps={{ fontSize: '12px', w: 'full' }}
                 title="Governance data is updated every 15 min"
