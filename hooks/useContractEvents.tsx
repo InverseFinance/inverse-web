c09ac909f8790a8b51946d7a76162c7df046ca86
webmass <mextrasis@hotmail.com>
2022-08-09 12:27:52 +0200

    useContractEvents: add error

--- hooks/useContractEvents.tsx
+++ hooks/useContractEvents.tsx
@@ -3,12 +3,12 @@ import useSWR from 'swr';
 import { useWeb3React } from '@web3-react/core';
 import { Web3Provider } from '@ethersproject/providers';
 
-export const useContractEvents = (address: string, abi: string[], method: string, args: any[] = [], ignoreIfArgsUndefined = false, swrKey = ''): { events: Event[], isLoading: boolean } => {
+export const useContractEvents = (address: string, abi: string[], method: string, args: any[] = [], ignoreIfArgsUndefined = false, swrKey = ''): { events: Event[], isLoading: boolean, error: any } => {
     const { account, library } = useWeb3React<Web3Provider>();
 
     const _swrKey = swrKey || `contract-event-${address}-${method}-${account}`;
 
-    const { data } = useSWR(_swrKey, async () => {
+    const { data, error } = useSWR(_swrKey, async () => {
         if(ignoreIfArgsUndefined && args?.length && args.filter(arg => arg !== undefined).length === 0) {
             return [];
         }
@@ -19,5 +19,6 @@ export const useContractEvents = (address: string, abi: string[], method: string
     return {
         events: data || [],
         isLoading: !data,
+        error,
     }
 }
