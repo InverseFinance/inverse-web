a429f61bcbc3bb8549b2724e3f3a74d2b42b7235
webmass <mextrasis@hotmail.com>
2022-08-26 17:22:10 +0200

    Theme: lightWarning

--- variables/theme.tsx
+++ variables/theme.tsx
@@ -53,6 +53,7 @@ const theme = extendTheme({
     success: '#25C9A1',
     successAlpha: '#25C9A122',
     successLight: '#25C9A199',
+    lightWarning: '#edc536',
     error: '#F44061',
     errorAlpha: '#F4406166',
     warning: '#ed8936',
