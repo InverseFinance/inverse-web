f18460214b12a6ed4a7e88b79c8f57b9ec22fdc7
webmass <mextrasis@hotmail.com>
2022-09-07 11:09:23 +0200

    F2:  only show dbr bar when there is debt

--- pages/f2/index.tsx
+++ pages/f2/index.tsx
@@ -8,9 +8,11 @@ import { DbrHealth } from '@app/components/F2/bars/DbrHealth'
 import { F2Markets } from '@app/components/F2/F2Markets'
 import { useAccount } from '@app/hooks/misc'
 import { F2DbrInfosModal } from '@app/components/F2/Modals/F2DbrInfosModal'
+import { useAccountDBR } from '@app/hooks/useDBR'
 
 export const F2PAGE = () => {
     const account = useAccount();
+    const { debt } = useAccountDBR(account);
     const { isOpen: isDbrOpen, onOpen: onDbrOpen, onClose: onDbrClose } = useDisclosure();
     return (
         <Layout>
@@ -25,11 +27,13 @@ export const F2PAGE = () => {
             <ErrorBoundary>
                 <VStack w='full' maxW="84rem">
                     <ErrorBoundary description="Failed to load header"><F2Header /></ErrorBoundary>
-                    <ErrorBoundary description="Failed to load Dbr Health">
-                        <VStack px='6' w='full'>
-                            <DbrHealth account={account} onModalOpen={onDbrOpen} />
-                        </VStack>
-                    </ErrorBoundary>
+                    {
+                        !!account && debt > 0 && <ErrorBoundary description="Failed to load Dbr Health">
+                            <VStack px='6' w='full'>
+                                <DbrHealth account={account} onModalOpen={onDbrOpen} />
+                            </VStack>
+                        </ErrorBoundary>
+                    }
                     <ErrorBoundary description="Failed to Markets">
                         <F2Markets />
                     </ErrorBoundary>
