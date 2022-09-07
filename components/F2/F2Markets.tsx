cd8f56fb55c60d450f018694940d903f76583d6c
webmass <mextrasis@hotmail.com>
2022-09-07 11:10:21 +0200

    F2: rework master page, more infos

--- components/F2/F2Markets.tsx
+++ components/F2/F2Markets.tsx
@@ -1,92 +1,135 @@
 import { Flex, Stack, Text } from "@chakra-ui/react"
 import Table from "@app/components/common/Table";
-import { UnderlyingItemBlock } from "@app/components/common/Assets/UnderlyingItemBlock";
 import { shortenNumber } from "@app/util/markets";
 import Container from "@app/components/common/Container";
 import { useAccountF2Markets, useDBRMarkets } from '@app/hooks/useDBR';
-import { commify } from 'ethers/lib/utils';
 import { useRouter } from 'next/router';
 import { useAccount } from '@app/hooks/misc';
 import { getRiskColor } from "@app/util/f2";
 import { BigImageButton } from "../common/Button/BigImageButton";
 
 const ColHeader = ({ ...props }) => {
-    return <Flex justify="flex-start" minWidth={'150px'} fontSize="12px" fontWeight="extrabold" {...props} />
+    return <Flex  justify="flex-start" minWidth={'150px'} fontSize="14px" fontWeight="extrabold" {...props} />
 }
 
 const Cell = ({ ...props }) => {
-    return <Stack direction="row" fontSize="14px" fontWeight="normal" justify="flex-start" minWidth="150px" {...props} />
+    return <Stack  direction="row" fontSize="14px" fontWeight="normal" justify="flex-start" minWidth="150px" {...props} />
+}
+
+const CellText = ({ ...props }) => {
+    return <Text fontSize="16px" {...props}/>
 }
 
 const columns = [
     {
         field: 'name',
         label: 'Market',
-        header: ({ ...props }) => <ColHeader minWidth="100px" justify="center"  {...props} />,
-        value: ({ address, underlying }) => {
-            return <Cell minWidth="100px" justify="center" >
-                <UnderlyingItemBlock symbol={underlying?.symbol} imgSize={20} />
+        header: ({ ...props }) => <ColHeader minWidth="130px" justify="flex-start"  {...props} />,
+        tooltip: 'Market type, each market have an underlying token and strategy',
+        value: ({ name }) => {
+            return <Cell minWidth="130px" justify="flex-start" alignItems="center" >
+                <BigImageButton bg={`url('/assets/f2/markets/${name}.png')`} h="37px" w="60px" />
+                <CellText>{name}</CellText>
             </Cell>
         },
     },
-    {
-        field: 'supplyApy',
-        label: 'SUPPLY APY',
-        header: ({ ...props }) => <ColHeader minWidth="100px" justify="center"  {...props} />,
-        value: ({ supplyApy }) => {
-            return <Cell minWidth="100px" justify="center" >
-                <Text>{supplyApy}%</Text>
-            </Cell>
-        },
-    },
-    {
-        field: 'price',
-        label: 'price',
-        header: ({ ...props }) => <ColHeader minWidth="100px" justify="center"  {...props} />,
-        value: ({ price }) => {
-            return <Cell minWidth="100px" justify="center" >
-                <Text>${commify((price||0)?.toFixed(2))}</Text>
-            </Cell>
-        },
-    },
+    // {
+    //     field: 'supplyApy',
+    //     label: 'SUPPLY APY',
+    //     header: ({ ...props }) => <ColHeader minWidth="100px" justify="center"  {...props} />,
+    //     value: ({ supplyApy }) => {
+    //         return <Cell minWidth="100px" justify="center" >
+    //             <Text>{supplyApy}%</Text>
+    //         </Cell>
+    //     },
+    // },
+    // {
+    //     field: 'price',
+    //     label: 'price',
+    //     header: ({ ...props }) => <ColHeader minWidth="100px" justify="center"  {...props} />,
+    //     value: ({ price }) => {
+    //         return <Cell minWidth="100px" justify="center" >
+    //             <CellText>${commify((price||0)?.toFixed(2))}</CellText>
+    //         </Cell>
+    //     },
+    // },
     {
         field: 'collateralFactor',
         label: 'C.F',
-        header: ({ ...props }) => <ColHeader minWidth="100px" justify="center"  {...props} />,
+        header: ({ ...props }) => <ColHeader minWidth="90px" justify="center"  {...props} />,
+        tooltip: 'Collateral Factor: percentage of the collateral worth transformed into borrowing power',
         value: ({ collateralFactor }) => {
+            return <Cell minWidth="90px" justify="center" >
+                <CellText>{shortenNumber(collateralFactor, 2)}%</CellText>
+            </Cell>
+        },
+    },
+    {
+        field: 'dola',
+        label: 'Liquidity',
+        header: ({ ...props }) => <ColHeader minWidth="100px" justify="center"  {...props} />,
+        tooltip: 'Remaining borrowable DOLA liquidity',
+        value: ({ dola }) => {
             return <Cell minWidth="100px" justify="center" >
-                <Text>{shortenNumber(collateralFactor, 2)}%</Text>
+                <CellText>{shortenNumber(dola, 2, true)}</CellText>
             </Cell>
         },
     },
     {
         field: 'totalDebt',
-        label: 'Total Borrows',
-        header: ({ ...props }) => <ColHeader minWidth="100px" justify="center"  {...props} />,
+        label: 'Borrows',
+        header: ({ ...props }) => <ColHeader minWidth="150px" justify="center"  {...props} />,
+        tooltip: 'Total DOLA borrowed in the Market',
         value: ({ totalDebt }) => {
-            return <Cell minWidth="100px" justify="center" >
-                <Text>{shortenNumber(totalDebt, 2)} DOLA</Text>
+            return <Cell minWidth="150px" justify="center" >
+                <CellText>{shortenNumber(totalDebt, 2, true)}</CellText>
+            </Cell>
+        },
+    },
+    {
+        field: 'collateral',
+        label: 'Wallet',
+        header: ({ ...props }) => <ColHeader minWidth="100px" justify="center"  {...props} />,
+        tooltip: 'Collateral balance in your wallet',
+        value: ({ collateral, price }) => {
+            return <Cell minWidth="100px" justify="center" alignItems="center" direction="column" spacing="0">
+                <CellText>{shortenNumber(collateral, 2)}</CellText>
+                <CellText>({shortenNumber(collateral * price, 2, true)})</CellText>
             </Cell>
         },
     },
     {
         field: 'debt',
-        label: 'You Borrowed',
-        header: ({ ...props }) => <ColHeader minWidth="100px" justify="center"  {...props} />,
+        label: 'Your Deposits',
+        header: ({ ...props }) => <ColHeader minWidth="150px" justify="center"  {...props} />,
+        tooltip: 'Amount of Collateral you deposited in the Market',
+        value: ({ deposits, price }) => {
+            return <Cell minWidth="150px" justify="center" alignItems="center" direction="column" spacing="0">
+            <CellText>{shortenNumber(deposits, 2)}</CellText>
+            <CellText>({shortenNumber(deposits * price, 2, true)})</CellText>
+        </Cell>
+        },
+    },
+    {
+        field: 'debt',
+        label: 'Your Debt',
+        header: ({ ...props }) => <ColHeader minWidth="120px" justify="center"  {...props} />,
+        tooltip: 'Amount of DOLA you borrowed from the Market',
         value: ({ debt }) => {
-            return <Cell minWidth="100px" justify="center" >
-                <Text>{shortenNumber(debt, 2)} DOLA</Text>
+            return <Cell minWidth="120px" justify="center" >
+                <CellText>{shortenNumber(debt, 2, true)}</CellText>
             </Cell>
         },
     },
     {
         field: 'perc',
-        label: 'Health',
-        header: ({ ...props }) => <ColHeader minWidth="100px" justify="center"  {...props} />,
+        label: 'Loan Health',
+        header: ({ ...props }) => <ColHeader minWidth="150px" justify="flex-end"  {...props} />,
+        tooltip: 'Remaining Collateral Health',
         value: ({ perc, hasDebt }) => {
             const color = getRiskColor(perc);
-            return <Cell minWidth="100px" justify="center" >
-                <Text color={perc && hasDebt ? color : undefined}>{hasDebt ? `${shortenNumber(perc, 2)}%` : '-'}</Text>
+            return <Cell minWidth="150px" justify="flex-end" >
+                <CellText color={perc && hasDebt ? color : undefined}>{hasDebt ? `${shortenNumber(perc, 2)}%` : '-'}</CellText>
             </Cell>
         },
     },
