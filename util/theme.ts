f32babd59e95f53c216d98f93b272f0de13bb9ea
webmass <mextrasis@hotmail.com>
2022-09-28 15:25:25 +0200

    Theming: first switch fix

--- util/theme.ts
+++ util/theme.ts
@@ -1,7 +1,7 @@
 
 export const switchTheme = () => {
     const currentTheme = localStorage.getItem('theme');
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    const newTheme = currentTheme === 'light' || !currentTheme ? 'dark' : 'light';
     localStorage.setItem('theme', newTheme);
     const customEvent = new CustomEvent('change-theme', { detail: { newTheme } });
     document.dispatchEvent(customEvent);    
