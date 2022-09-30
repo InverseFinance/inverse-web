dbdd959f01dc69fc8736dd7a32e93a5fe7a567a2
webmass <mextrasis@hotmail.com>
2022-09-30 14:19:37 +0200

    AmountInfos: show delta

--- components/common/Messages/AmountInfos.tsx
+++ components/common/Messages/AmountInfos.tsx
@@ -21,13 +21,15 @@ export const AmountInfos = ({
 } & Partial<StackProps>) => {
     const _textProps = { fontSize: '12px', color: 'secondaryTextColor', ...textProps }
     const formatFun = format ? shortenNumber : (v) => v;
+    const deltaAmount = value && newValue ? newValue - value : 0;
+    const deltaSign = deltaAmount > 0 ? '+' : '-';
     return <HStack spacing="1" justify="space-between" {...props}>
         <Text {..._textProps}>
             {label}: {!!value || !value && !newValue ? formatFun(value, 2, false, true) : ''} {price && value ? `(${formatFun(value * price, 2, true)})` : ''}
         </Text>
         {
             !!newValue && value !== newValue &&
-            <Text {..._textProps}> {!!value && !!newValue ? '=> ' : ''}{formatFun(newValue, 2, false, true)} {price ? `(${formatFun(newValue * price, 2, true)})` : ''}{dbrCover ? ` + DBR Debt = ${formatFun(dbrCover + newValue, 2)}` : ''}</Text>
+            <Text {..._textProps}> {!!value && !!newValue ? `${deltaSign} ${formatFun(deltaAmount, 2, false, true)}${price ? ` (${formatFun(deltaAmount * price, 2, true)})` : ''} => ` : ''}{formatFun(newValue, 2, false, true)} {price ? `(${formatFun(newValue * price, 2, true)})` : ''}{dbrCover ? ` + DBR Debt = ${formatFun(dbrCover + newValue, 2)}` : ''}</Text>
         }
     </HStack>
 }
