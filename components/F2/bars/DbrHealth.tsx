874afb95480e37a38a6959883f32cebad35d959c
webmass <mextrasis@hotmail.com>
2022-10-04 14:55:20 +0200

    F2: rework Dbr bar

--- components/F2/bars/DbrHealth.tsx
+++ components/F2/bars/DbrHealth.tsx
@@ -32,10 +32,10 @@ export const DbrHealth = ({
           nextValue={isPreviewing ? previewExpiryDate : undefined}
           type={'date'}
           placeholder={hasDebt && dbrNbDaysExpiry <= 0 ? 'Exhausted!' : 'No on-going Loans'}
-          prefix={(hasDebt && !isPreviewing) || (isPreviewing && !dbrExpiryDate) ? 'Fixed Rate until ' : undefined}
+          prefix={(hasDebt && !isPreviewing) || (isPreviewing && !dbrExpiryDate) ? 'Enough DBR until ' : undefined}
           nullPlaceholder="no more loan"
-          tooltipTitle={hasDebt || isPreviewing ? "Fixed Rate until" : ''}
-          color={riskColor}
+          tooltipTitle={hasDebt || isPreviewing ? "Enough DBR until" : ''}
+          // color={riskColor}
           tooltip={
             hasDebt || isPreviewing ? "Date where you will be in deficit of DBR tokens (Exhaustion state), someone can then do a forced top-up of your DBR, which will cause your debt to increase and damage the Collateral Health."
               : ''
@@ -46,14 +46,14 @@ export const DbrHealth = ({
           nextValue={isPreviewing ? -newDailyRate : undefined}
           placeholder={balance ? `Can borrow ${preciseCommify(balance, 2)} DOLA / Year` : ''}
           suffix=" DBR a day"
-          prefix="Fixed Rate: "
+          prefix="Burn Rate: "
           type="number"
-          color={riskColor}
+          // color={riskColor}
           tooltip="If you have loans your DBR balance will decrease over time by an amount that depends on your debt."
         />
       </HStack>
       <QuantityBar
-        title={needsRechargeSoon ? 'Please top-up DBR' : 'Fixed Rate Duration'}
+        title={needsRechargeSoon ? 'Please top-up DBR' : 'DBR Balance Health'}
         perc={dbrDepletionPerc}
         previewPerc={previewPerc}
         badgeColorScheme={'success'}
@@ -82,9 +82,9 @@ export const DbrHealth = ({
           nextValue={isPreviewing ? previewExpiryDate : undefined}
           type={'remainingTime'}
           placeholder={hasDebt && dbrNbDaysExpiry <= 0 ? 'Exhausted!' : 'Borrow to start'}
-          suffix={(isPreviewing && previewExpiryDate !== null) || (!isPreviewing && hasDebt) ? ' before Fixed Rate Exhaustion' : undefined}
+          suffix={(isPreviewing && previewExpiryDate !== null) || (!isPreviewing && hasDebt) ? ' before DBR Depletion' : undefined}
           nullPlaceholder="no more loan"
-          tooltipTitle="Time before Exhaustion"
+          tooltipTitle="Time before Depletion"
           tooltip={
             hasDebt ? "Remaining time before you become in deficit of DBR tokens (Exhaustion state), a state  where someone can do a forced top-up of your DBR, which will cause your debt to increase and damage the Collateral Health."
               : ''
@@ -93,7 +93,7 @@ export const DbrHealth = ({
         <F2StateInfo
           currentValue={signedBalance}
           placeholder={'No DBR tokens'}
-          prefix="DBR tokens: "
+          prefix="DBR balance: "
           tooltip="DBR stands for DOLA Borrowing Rights, it allows borrowers to lock-in a fixed borrow rate. If you have loans your DBR balance will decrease over time by an amount that depends on your debt."
         />
         {/* <Text color="secondaryTextColor">
