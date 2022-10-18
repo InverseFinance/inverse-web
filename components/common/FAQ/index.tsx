cc2817db0744da71a4968fde88a04c05840f9c02
webmass <mextrasis@hotmail.com>
2022-10-18 17:37:47 +0200

    Update FAQ comp

--- components/common/FAQ/index.tsx
+++ components/common/FAQ/index.tsx
@@ -2,35 +2,46 @@ import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPane
 import { Box } from "@chakra-ui/react"
 import Container from "../Container"
 
-export const FAQ = ({
-    label = 'FAQ',
-    items,
-}: {
+export type FAQType = {
     label: string
+    defaultCollapse?: boolean
+    collapsable?: boolean
     items: {
         title: string
         body: string
     }[]
-}) => {
-    return <Container label={label} noPadding p="0">
+}
+
+export const FAQ = ({
+    label = 'FAQ',
+    items,
+    collapsable = false,
+    defaultCollapse = false,
+}: FAQType) => {
+    return <Container
+        label={label}
+        noPadding p="0"
+        collapsable={collapsable}
+        defaultCollapse={defaultCollapse}
+    >
         <Accordion w='full' allowMultiple>
-        {
-            items.map((item, i) => {
-                return <AccordionItem border="none" borderBottom={ i < (items.length-1) ? '1px solid #eee' : 'none' } key={item.title}>
-                    <h2>
-                        <AccordionButton >
-                            <Box flex='1' textAlign='left' color="mainTextColor" fontWeight="bold" fontSize="lg">
-                                {item.title}
-                            </Box>
-                            <AccordionIcon />
-                        </AccordionButton>
-                    </h2>
-                    <AccordionPanel pb={4} color="secondaryTextColor">
-                        {item.body}
-                    </AccordionPanel>
-                </AccordionItem>
-            })
-        }
-    </Accordion>
+            {
+                items.map((item, i) => {
+                    return <AccordionItem border="none" borderBottom={i < (items.length - 1) ? '1px solid #eee' : 'none'} key={item.title}>
+                        <h2>
+                            <AccordionButton >
+                                <Box flex='1' textAlign='left' color="mainTextColor" fontWeight="bold" fontSize="lg">
+                                    {item.title}
+                                </Box>
+                                <AccordionIcon />
+                            </AccordionButton>
+                        </h2>
+                        <AccordionPanel pb={4} color="secondaryTextColor">
+                            {item.body}
+                        </AccordionPanel>
+                    </AccordionItem>
+                })
+            }
+        </Accordion>
     </Container>
 }
