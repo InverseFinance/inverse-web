4ab97005c33821f963b900294f8c86753e96c5c0
webmass <mextrasis@hotmail.com>
2022-10-20 16:23:35 +0200

    F2: handle dbr balance cases & deficit, buy link

--- components/F2/Infos/MarketBar.tsx
+++ components/F2/Infos/MarketBar.tsx
@@ -1,25 +1,29 @@
 import { MarketImage } from "@app/components/common/Assets/MarketImage"
+import Link from "@app/components/common/Link"
 import { useDBRPrice } from "@app/hooks/useDBR"
 import { useDualSpeedEffect } from "@app/hooks/useDualSpeedEffect"
-import { F2Market } from "@app/types"
+import { getDBRBuyLink } from "@app/util/f2"
 import { shortenNumber } from "@app/util/markets"
 import { preciseCommify } from "@app/util/misc"
 import { HStack, VStack, Text, FormControl, FormLabel, Switch, useMediaQuery, StackProps } from "@chakra-ui/react"
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
+import { F2MarketContext } from "../F2Contex"
 
 export const MarketBar = ({
-    market,
-    isWalkthrough = false,
-    setIsWalkthrough,
     ...props
 }: {
-    setIsWalkthrough: (v: boolean) => void
-    market: F2Market
-    isWalkthrough?: boolean
 } & Partial<StackProps>) => {
     const { price: dbrPrice } = useDBRPrice();
     const [isLargerThan] = useMediaQuery('(min-width: 600px)');
     const [effectEnded, setEffectEnded] = useState(true);
+
+    const {
+        market,
+        isWalkthrough,
+        setIsWalkthrough,
+        dbrBalance,
+        debt,
+    } = useContext(F2MarketContext);
 
     useEffect(() => {
         setEffectEnded(false);
@@ -29,25 +33,27 @@ export const MarketBar = ({
         setEffectEnded(true);
     }, [isWalkthrough], !isWalkthrough, 200, 50);
 
+    const needTopUp = dbrBalance < 0 || (dbrBalance === 0 && debt > 0);
+
     return <HStack w='full' justify="space-between" {...props}>
         <HStack spacing={{ base: '2', md: '8' }}>
             <HStack spacing={{ base: '1', md: '2' }}>
                 <MarketImage pr="2" image={market.icon || market.underlying.image} size={isLargerThan ? 40 : 30} />
                 <VStack spacing="1" alignItems="flex-start">
-                    <Text as="h1" fontWeight="extrabold" fontSize={{ base: '14px', md: '20px' }}>
+                    <Text as='h2' fontWeight="extrabold" fontSize={{ base: '14px', md: '20px' }}>
                         {market.name} Market
                     </Text>
-                    <Text as="h1" color="secondaryTextColor" fontSize={{ base: '14px', md: '16px' }}>
+                    <Text color="secondaryTextColor" fontSize={{ base: '14px', md: '16px' }}>
                         {shortenNumber(market.dolaLiquidity, 2)} DOLA liquidity
                     </Text>
                 </VStack>
             </HStack>
             {
                 isLargerThan && <VStack spacing="1" alignItems="flex-start">
-                    <Text as="h1" fontWeight="extrabold" fontSize="20px">
+                    <Text fontWeight="extrabold" fontSize="20px">
                         Oracle Price
                     </Text>
-                    <Text as="h1" color="secondaryTextColor">
+                    <Text color="secondaryTextColor">
                         {preciseCommify(market.price, 2, true)}
                     </Text>
                 </VStack>
@@ -55,20 +61,43 @@ export const MarketBar = ({
             {
                 !isWalkthrough && effectEnded && isLargerThan && <>
                     <VStack spacing="1" alignItems="flex-start">
-                        <Text as="h1" fontWeight="extrabold" fontSize="20px">
+                        <Text fontWeight="extrabold" fontSize="20px">
                             Collateral Factor
                         </Text>
-                        <Text as="h1" color="secondaryTextColor">
+                        <Text color="secondaryTextColor">
                             {preciseCommify(market.collateralFactor * 100, 2)}%
                         </Text>
                     </VStack>
                     <VStack spacing="1" alignItems="flex-start">
-                        <Text as="h1" fontWeight="extrabold" fontSize="20px">
+                        <Text fontWeight="extrabold" fontSize="20px">
                             DBR Price
                         </Text>
-                        <Text as="h1" color="secondaryTextColor">
+                        <Text color="secondaryTextColor">
                             {preciseCommify(dbrPrice, 4, true)}
                         </Text>
+                    </VStack>
+                    <VStack spacing="1" alignItems="flex-start">
+                        <Text fontWeight="extrabold" fontSize="20px">
+                            DBR Balance
+                        </Text>
+
+                        <Link color={ needTopUp ? 'error' : 'secondaryTextColor' } href={getDBRBuyLink()} isExternal target='_blank'>
+                            {
+                                dbrBalance > 0 && <Text color="inherit">
+                                    {shortenNumber(dbrBalance, 2)}{!!dbrBalance && ` (${shortenNumber(dbrBalance * dbrPrice, 2, true)})`}
+                                </Text>
+                            }
+                            {
+                                dbrBalance === 0 && !debt && <Text color="inherit">
+                                    Buy now
+                                </Text>
+                            }
+                            {
+                                needTopUp && <Text color="inherit">
+                                    {shortenNumber(dbrBalance, 2)} Top-up now
+                                </Text>
+                            }
+                        </Link>
                     </VStack>
                 </>
             }
@@ -88,11 +117,10 @@ export const MarketBar = ({
                     htmlFor='walkthrough-mode'
                     textAlign="right"
                     mb='0'
-                    
                 >
                     <VStack color="secondaryTextColor" spacing="0" alignItems="flex-end">
-                    <Text color="inherit">Deposit & Borrow</Text>
-                        <Text color="inherit">Walkthrough mode</Text>                        
+                        <Text color="inherit">Deposit & Borrow</Text>
+                        <Text color="inherit">Walkthrough mode</Text>
                     </VStack>
                 </FormLabel>
                 <Switch colorScheme="purple" isChecked={isWalkthrough} onChange={() => setIsWalkthrough(!isWalkthrough)} id='walkthrough-mode' mr="1" />
