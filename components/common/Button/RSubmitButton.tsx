c443ed0e8511e77c56bfed0e1b74f6b937ce07e8
webmass <webmass@inverse.finance>
2022-11-15 15:44:55 +0100

    Landing: responsive landing btn

--- components/common/Button/RSubmitButton.tsx
+++ components/common/Button/RSubmitButton.tsx
@@ -7,8 +7,8 @@ import { lightTheme } from '@app/variables/theme'
 type Props = ButtonProps & { href?: string, target?: string }
 
 export const RSubmitButton = (props: Props) => {
-    const _props = { borderLeftRadius: '50vmax', borderRightRadius: '50vmax' ,...props }
-    if(_props?.href) {
+    const _props = { borderLeftRadius: '50vmax', borderRightRadius: '50vmax', ...props }
+    if (_props?.href) {
         const { target, w, width, ...btnProps } = _props;
         return <NextLink href={_props.href} passHref>
             <Link w={w} width={width} target={target} textDecoration="none" _hover={{ textDecoration: 'none' }}>
@@ -22,15 +22,15 @@ export const RSubmitButton = (props: Props) => {
 export const LandingSubmitButton = (props: Props) => {
     const px = !props?.px ? { base: '2', sm: '40px', '2xl': '3vw' } : props.px;
     const py = !props?.py ? { base: '2', sm: '30px', '2xl': '2.5vh' } : props.py;
-    return <RSubmitButton 
-    boxShadow="none"
-     textTransform="inherit"
-      w={{ base: 'full', sm: 'auto' }}
-       px={px}
-       py={py}
+    return <RSubmitButton
+        boxShadow="none"
+        textTransform="inherit"
+        w={{ base: 'full', sm: 'auto' }}
+        px={px}
+        py={py}
         h='50px'
         fontSize={{ base: '18px', '2xl': '0.9vw' }}
-       {...props} />
+        {...props} />
 }
 
 export const LandingOutlineButton = (props: Props) => {
