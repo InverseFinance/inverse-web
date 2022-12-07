ada6a616afec87ce80b088be55519337885af029
webmass <webmass@inverse.finance>
2022-12-07 09:26:49 +0100

    F2: recap => ui for success state

--- components/F2/walkthrough/Recap.tsx
+++ components/F2/walkthrough/Recap.tsx
@@ -1,17 +1,24 @@
-import { SubmitButton } from "@app/components/common/Button"
 import { SimpleAmountForm } from "@app/components/common/SimpleAmountForm"
 import { ChevronLeftIcon } from "@chakra-ui/icons"
-import { HStack } from "@chakra-ui/react"
-import { useContext } from "react"
+import { HStack, Stack } from "@chakra-ui/react"
+import { useContext, useState } from "react"
 import { F2MarketContext } from "@app/components/F2/F2Contex"
 import { RecapInfos } from "../Infos/RecapInfos"
 import { StepNavBtn } from "./StepNavBtn"
+import { f2depositAndBorrow } from "@app/util/f2"
+import { getNumberToBn } from "@app/util/markets"
+import { SuccessMessage } from "@app/components/common/Messages"
+import { RSubmitButton } from "@app/components/common/Button/RSubmitButton"
+import { useRouter } from "next/router"
 
 export const F2WalkthroughRecap = ({
     onStepChange,
 }: {
     onStepChange: (step: number) => void
 }) => {
+    const [isDone, setIsDone] = useState(false);
+    const router = useRouter();
+
     const {
         step,
         market,
@@ -33,6 +40,9 @@ export const F2WalkthroughRecap = ({
         newPerc,
         dbrCoverDebt,
         newDBRExpiryDate,
+        handleDebtChange,
+        handleCollateralChange,
+        setIsWalkthrough,
     } = useContext(F2MarketContext);
 
     const recapData = {
@@ -51,6 +61,37 @@ export const F2WalkthroughRecap = ({
         newDBRExpiryDate,
     }
 
+    const handleAction = () => {
+        if(market.helper) {
+            alert('Not implemented yet');
+        } else {
+            return f2depositAndBorrow(signer, market.address, getNumberToBn(collateralAmount, market.underlying.decimals), getNumberToBn(debtAmount));
+        }        
+    }
+
+    const handleSuccess = () => {
+        setIsDone(true);
+        handleDebtChange('');
+        handleCollateralChange(''); 
+    }
+
+    if(isDone) {
+        return <SuccessMessage
+            title="Borrowing complete!"
+            alertProps={{ w: 'full' }}
+            description={
+                <Stack direction={{ base: 'column', sm: 'row' }} mt="4">
+                    <RSubmitButton onClick={() => router.replace('/firm')}>
+                        Go Back to Markets
+                    </RSubmitButton>
+                    <RSubmitButton onClick={() => setIsWalkthrough(false)}>
+                        Go to Loan
+                    </RSubmitButton>
+                </Stack>
+            }
+        />
+    }
+
     return <>
         <RecapInfos {...recapData} />
         <HStack w='full' justify="space-between">
@@ -65,7 +106,7 @@ export const F2WalkthroughRecap = ({
                 signer={signer}
                 decimals={colDecimals}
                 maxAmountFrom={isDeposit ? [bnCollateralBalance] : [bnDeposits, bnWithdrawalLimit]}
-                onAction={({ bnAmount }) => { alert('Contract not available yet for this action') }}
+                onAction={({ bnAmount }) => handleAction(bnAmount)}
                 onMaxAction={({ bnAmount }) => { alert('Contract not available yet for this action') }}
                 actionLabel={isDeposit ? 'Deposit & Borrow' : 'Repay & Withdraw'}
                 showMaxBtn={false}
@@ -74,6 +115,7 @@ export const F2WalkthroughRecap = ({
                 hideInput={true}
                 hideButtons={false}
                 ButtonComp={StepNavBtn}
+                onSuccess={() => handleSuccess()}
             />
             </HStack>
         </HStack>
