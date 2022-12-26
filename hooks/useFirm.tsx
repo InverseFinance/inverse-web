fccc23a12ddd664e4e0102a8e11e12055c4f7976
webmass <webmass@inverse.finance>
2022-12-26 15:19:01 +0100

    Firm: dbr & market account events hooks

--- hooks/useFirm.tsx
+++ hooks/useFirm.tsx
@@ -1,13 +1,18 @@
 
-import { SWR } from "@app/types"
+import { F2Market, SWR } from "@app/types"
 import { fetcher } from '@app/util/web3'
 import { useCustomSWR } from "./useCustomSWR";
 import { useDBRMarkets } from "./useDBR";
 import { f2CalcNewHealth } from "@app/util/f2";
-import { getNumberToBn } from "@app/util/markets";
+import { getBnToNumber, getNumberToBn } from "@app/util/markets";
+import { useMultiContractEvents } from "./useContractEvents";
+import { DBR_ABI, F2_MARKET_ABI } from "@app/config/abis";
+import { getNetworkConfigConstants } from "@app/util/networks";
 
 const oneDay = 86400000;
 const oneYear = oneDay * 365;
+
+const { DBR } = getNetworkConfigConstants();
 
 export const useFirmPositions = (isShortfallOnly = false): SWR & {
   positions: any,
@@ -75,5 +80,44 @@ export const useDBRActiveHolders = (): SWR & {
     timestamp: data ? data.timestamp : 0,
     isLoading: !error && !data,
     isError: error,
+  }
+}
+
+export const useFirmMarketEvents = (market: F2Market, account: string) => {
+  const { groupedEvents, isLoading, error } = useMultiContractEvents([
+    [market.address, F2_MARKET_ABI, 'Deposit', [account]],
+    [market.address, F2_MARKET_ABI, 'Withdraw', [account]],
+    [market.address, F2_MARKET_ABI, 'Borrow', [account]],
+    [market.address, F2_MARKET_ABI, 'Repay', [account]],
+    [market.address, F2_MARKET_ABI, 'Liquidate', [account]],
+    // [market.address, F2_MARKET_ABI, 'CreateEscrow', [account]],
+    [DBR, DBR_ABI, 'ForceReplenish', [account]],
+  ], `firm-market-${market.address}-${account}`);
+
+  const events = groupedEvents.flat();
+  events.sort((a, b) => a.blockNumber !== b.blockNumber ? (b.blockNumber - a.blockNumber) : b.logIndex - a.logIndex);
+
+  return {
+    events: events.map(e => {
+      const isCollateralEvent = ['Deposit', 'Withdraw'].includes(e.event);
+      const decimals = isCollateralEvent ? market.underlying.decimals : 18;
+      return {
+        blockNumber: e.blockNumber,
+        txHash: e.transactionHash,    
+        amount: e.args.amount ? getBnToNumber(e.args.amount, decimals) : undefined,
+        deficit: e.args.deficit ? getBnToNumber(e.args.deficit, 18) : undefined,
+        repaidDebt: e.args.repaidDebt ? getBnToNumber(e.args.repaidDebt, 18) : undefined,
+        liquidatorReward: e.args.liquidatorReward ? getBnToNumber(e.args.liquidatorReward, 18) : undefined,
+        repayer: e.args.repayer,
+        to: e.args.to,
+        escrow: e.args.escrow,
+        replenisher: e.args.replenisher,
+        name: e.event,
+        isCollateralEvent,
+        tokenName: isCollateralEvent ? market.underlying.symbol : 'DOLA',
+      }
+    }),
+    isLoading,
+    error,
   }
 }
