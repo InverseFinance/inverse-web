904d4ac658d614e48b2ea25956b41d1918542ccc
webmass <webmass@inverse.finance>
2023-02-08 08:52:22 +0100

    Dwf: minor change

--- pages/partners/dwf.tsx
+++ pages/partners/dwf.tsx
@@ -156,7 +156,7 @@ export const DWFPage = () => {
     setMaxSlippage(newSlippage);
   }
 
-  const dailyLimitReached = dailyBuy >= dailyLimit;
+  const dailyLimitReached = dailyBuy > 0 && dailyBuy >= dailyLimit;
 
   return (
     <Layout>
