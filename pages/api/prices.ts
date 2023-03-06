5a014b91d2271d83449899c16f485095357bfc10
webmass <webmass@inverse.finance>
2023-03-06 09:58:31 +0100

    Transparency: bump versions

--- pages/api/prices.ts
+++ pages/api/prices.ts
@@ -9,7 +9,7 @@ import { COMPTROLLER_ABI, ORACLE_ABI } from '@app/config/abis'
 import { BigNumber, Contract } from 'ethers'
 import { formatUnits } from '@ethersproject/units'
 
-export const pricesCacheKey = `prices-v1.0.4`;
+export const pricesCacheKey = `prices-v1.0.5`;
 
 export default async function handler(req, res) {
 
