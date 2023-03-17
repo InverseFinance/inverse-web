bfb835ae276f37d6f2d41556dff969c796011ef2
webmass <webmass@inverse.finance>
2023-03-17 09:42:01 +0100

    Blog: disable ssg

--- pages/blog/merch/[...slug].js
+++ pages/blog/merch/[...slug].js
@@ -2,22 +2,11 @@ import { getDefaultProps } from '../../../blog/lib/utils'
 import { BLOG_LOCALES } from '../../../blog/lib/constants'
 import BlogMerch from '../../../blog/components/page-layouts/blog-merch'
 
-export default function BlogMerchSSG(props) {
+export default function BlogMerchSSR(props) {
   return BlogMerch(props)
 }
 
 // revalidation vya webhook
-export async function getStaticProps(context) {
-  return { ... await getDefaultProps(context), revalidate: false }
+export async function getServerSideProps(context) {
+  return getDefaultProps(context)
 }
-
-export async function getStaticPaths() {
-  if(!process.env.CONTENTFUL_SPACE_ID) {
-    return { paths: [], fallback: true }
-  }
-  return {
-    paths: BLOG_LOCALES.map(l => `/blog/merch/${l}`),
-    fallback: true,
-  }
-}
-
