e5ac87372bccc6cbab575a045d423c6a51b22af8
webmass <webmass@inverse.finance>
2023-03-20 09:07:58 +0100

    Generic hook for monthly bar charts

--- hooks/misc.tsx
+++ hooks/misc.tsx
@@ -1,3 +1,5 @@
+import { ONE_DAY_MS } from '@app/config/constants';
+import { SWR } from '@app/types';
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
 import { useRouter } from 'next/router';
@@ -33,6 +35,33 @@ export const useAccount = (ad?: string) => {
     const { account } = useWeb3React<Web3Provider>();
     const { query } = useRouter();
     const viewAs = (query?.viewAddress as string);
-    return  ad || viewAs ||  account;
+    return ad || viewAs || account;
+}
+
+export const useEventsAsChartData = (
+    events: (any & { timestamp: number })[],
+    yAccAttribute: string,
+    yAttribute: string,
+): SWR & { chartData: any } => {
+    const now = new Date();
+    const chartData = [...events.sort((a, b) => a.timestamp - b.timestamp).map(event => {
+        const date = new Date(event.timestamp);
+        return {
+            x: event.timestamp,
+            y: event[yAccAttribute],
+            yDay: event[yAttribute],
+            month: date.getUTCMonth(),
+            year: date.getUTCFullYear(),
+        }
+    })];
+
+    // add today's timestamp and zero one day before first supply
+    const minX = chartData.length > 0 ? Math.min(...chartData.map(d => d.x)) : 1577836800000;
+    chartData.unshift({ x: minX - ONE_DAY_MS, y: 0 });
+    chartData.push({ x: now, y: chartData[chartData.length - 1].y });
+
+    return {
+        chartData,
+    }
 }
 
