3f8307b0e3bb49adbc65645ee3092d378521e3fe
webmass <webmass@inverse.finance>
2023-03-22 14:47:16 +0100

    Liquidity: add bb-e-usd exception on apy

--- pages/api/transparency/liquidity.ts
+++ pages/api/transparency/liquidity.ts
@@ -142,10 +142,12 @@ export default async function handler(req, res) {
                     && y.underlyingTokens.join(',').toLowerCase() === lp.pairs?.join(',').toLowerCase();
             });
 
+            const apy = lp.address === '0x133d241F225750D2c92948E464A5a80111920331' ? 0 : yieldData?.apy;
+
             return {
                 ...lp,
                 lpName,
-                apy: yieldData?.apy,
+                apy,
                 apyMean30d: yieldData?.apyMean30d,
                 protocol,
                 project: defiLlamaProjectName || protocol,
@@ -157,7 +159,7 @@ export default async function handler(req, res) {
                 pairingDepth: tvl - dolaWorth,
                 dolaBalance: dolaWorth,
                 dolaWeight: dolaWorth / tvl * 100,
-                rewardDay: ownedAmount * (yieldData?.apy || 0) / 100 / 365,
+                rewardDay: ownedAmount * (apy || 0) / 100 / 365,
                 isFed: !!fedPol,
                 // subBalances,
             }
