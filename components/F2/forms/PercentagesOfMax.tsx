f7fa90db77248de7fd82c9e51afdaa22c0170ad5
webmass <webmass@inverse.finance>
2023-03-22 08:07:13 +0100

    Comp: add percentagesBar

--- components/F2/forms/PercentagesOfMax.tsx
+++ components/F2/forms/PercentagesOfMax.tsx
@@ -1,5 +1,5 @@
 import { AmountInfos } from "@app/components/common/Messages/AmountInfos"
-import { HStack } from "@chakra-ui/react"
+import { HStack, Text, TextProps } from "@chakra-ui/react"
 
 export const PercentagesOfMax = ({
     isSmallerThan728 = false,
@@ -22,5 +22,29 @@ export const PercentagesOfMax = ({
         <AmountInfos label={`${!isSmallerThan728 ? 'Borrow ' : ''}~50%`} format={false} value={halfMax} textProps={{ fontSize: '14px', onClick: () => onChange(halfMax.toFixed(0)) }} />
         <AmountInfos label={`${!isSmallerThan728 ? 'Borrow ' : ''}~75%`} format={false} value={threeQuarterMax} textProps={{ fontSize: '14px', onClick: () => onChange(threeQuarterMax.toFixed(0)) }} />
         <AmountInfos label={`${!isSmallerThan728 ? 'Borrow ' : ''}~99%`} format={false} value={_usableMax} textProps={{ fontSize: '14px', onClick: () => onChange(_usableMax.toFixed(0)) }} />
+    </HStack>
+}
+
+const DEFAULT_TICKS = [0, 25, 50, 75, 100];
+
+export const PercentagesBar = ({
+    onChange,
+    ticks = DEFAULT_TICKS,
+    tickProps,
+    showAsRepartition = false,
+}: {
+    onChange: (v: string) => void,
+    ticks?: number[],
+    tickProps?: TextProps,
+    showAsRepartition?: boolean,
+}) => {
+    return <HStack w='full' justify="space-between">
+        {
+            ticks.map(tick => {
+                return <Text color="accentTextColor" _hover={{ color: 'mainTextColor' }} cursor="pointer" fontSize='14px' onClick={() => onChange(tick)} {...tickProps}>
+                    {showAsRepartition ? `${100-tick}% | ${tick}%` : `${tick}%`}
+                </Text>
+            })
+        }
     </HStack>
 }
