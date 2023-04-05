0782ca8aab79da1691dec612b5622a59018ffc91
webmass <webmass@inverse.finance>
2023-04-05 10:15:58 +0200

    Firm: percentage bar positions

--- components/F2/forms/PercentagesOfMax.tsx
+++ components/F2/forms/PercentagesOfMax.tsx
@@ -27,6 +27,8 @@ export const PercentagesOfMax = ({
 
 const DEFAULT_TICKS = [0, 25, 50, 75, 100];
 
+const handleWidth = 14;
+
 export const PercentagesBar = ({
     onChange,
     ticks = DEFAULT_TICKS,
@@ -38,11 +40,24 @@ export const PercentagesBar = ({
     tickProps?: TextProps,
     showAsRepartition?: boolean,
 }) => {
-    return <HStack w='full' justify="space-between">
+    return <HStack w='full' justify="space-between" spacing="0">
         {
             ticks.map(tick => {
-                return <Text color="accentTextColor" _hover={{ color: 'mainTextColor' }} cursor="pointer" fontSize='14px' onClick={() => onChange(tick)} {...tickProps}>
-                    {showAsRepartition ? `${100-tick}% | ${tick}%` : `${tick}%`}
+                const isMiddle = tick !== 0 && tick !== 100;
+                return <Text
+                    key={tick}
+                    w='94px'
+                    position={isMiddle ? 'absolute' : 'relative'}
+                    left={isMiddle ? `calc(${tick}% + ${handleWidth/2-tick/100*handleWidth}px)` : undefined}
+                    transform={isMiddle ? `translateX(-${tick}%)` : undefined}
+                    color="accentTextColor"
+                    _hover={{ color: 'mainTextColor' }}
+                    cursor="pointer"
+                    fontSize='14px'
+                    onClick={() => onChange(tick)}
+                    {...tickProps}
+                >
+                    {showAsRepartition ? `${100 - tick}% | ${tick}%` : `${tick}%`}
                 </Text>
             })
         }
