6a518b5f292f8cd91ff48a673b48ad8aa1c55892
webmass <webmass@inverse.finance>
2023-04-05 16:12:37 +0200

    Firm: rewards => more responsive

--- components/F2/rewards/ZapperTokens.tsx
+++ components/F2/rewards/ZapperTokens.tsx
@@ -7,10 +7,11 @@ import { HStack, VStack, Text, Stack } from "@chakra-ui/react"
 
 export const ZapperTokens = ({ rewardsInfos, handleClaim }) => {
     const claimables = rewardsInfos?.tokens.filter(t => t.metaType === 'claimable');
+    claimables.sort((a,b) => b.balanceUSD - a.balanceUSD)
     const totalRewards = claimables.reduce((prev, curr) => prev + curr.balanceUSD, 0);
 
     return <VStack spacing='4' w='full' alignItems="flex-start">
-        <HStack spacing='8'>
+        <Stack spacing={{ base: '2', sm: '8' }} direction={{ base: 'column', sm: 'row' }}>
             <HStack>
                 <Text whiteSpace='nowrap' fontSize="18px" fontWeight="bold">Total Rewards:</Text>
                 <Text color="success" fontWeight="extrabold" fontSize="20px">{preciseCommify(totalRewards, 2, true)}</Text>
@@ -20,19 +21,19 @@ export const ZapperTokens = ({ rewardsInfos, handleClaim }) => {
                     Claim rewards
                 </RSubmitButton>
             }
-        </HStack>
-        <Stack spacing="4" direction={{ base: 'column', md: 'row' }}>
+        </Stack>
+        <Stack spacing="4" w='full' direction={{ base: 'column', sm: 'row' }}>
             {
                 claimables.map((t, i) => {
                     const underlying = getToken(TOKENS, t.address) || {};
-                    return <HStack key={t.address} w='full' border='1px solid #ccc' p='2' borderRadius='5px'>
+                    return <HStack justify='space-between' key={t.address} w={{ base: 'full', sm: 'fit-content' }} border='1px solid #ccc' p='2' borderRadius='5px'>
                         <VStack spacing="1" alignItems="flex-start" w='80px'>
                             <HStack>
                                 <UnderlyingItem {...underlying} label={underlying.symbol||t.symbol} textProps={{ fontSize: '14px', fontWeight: 'bold' }} />
                             </HStack>
                             <Text color='mainTextColorLight' fontSize='14px'>{shortenNumber(t.price, 2, true)}</Text>
                         </VStack>
-                        <VStack spacing="1" alignItems="flex-start">
+                        <VStack spacing="1" alignItems="flex-end">
                             <Text fontSize='14px'>{shortenNumber(t.balance, 2)}</Text>
                             <Text color="success" fontWeight="extrabold" fontSize='14px'>{preciseCommify(t.balanceUSD, 2, true)}</Text>
                         </VStack>
