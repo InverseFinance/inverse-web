8d2a70ca9e8a674a444906d7beb5ffc7f262b215
webmass <webmass@inverse.finance>
2023-04-06 16:31:42 +0200

    Util: zapper formatter

--- util/zapper.ts
+++ util/zapper.ts
@@ -1,7 +1,7 @@
 const baseUrl = 'https://api.zapper.xyz'
 
-export const getZapperApps = async (account: string) => {
-    const path = `/v2/balances/apps?addresses%5B%5D=${account}&networks%5B%5D=ethereum`
+export const getZapperApps = async (accounts: string[]) => {
+    const path = `/v2/balances/apps?addresses%5B%5D=${accounts.join(',')}&networks%5B%5D=ethereum`
     const res = await fetch(`${baseUrl}${path}`, {
         method: 'GET',
         headers: {
@@ -11,6 +11,32 @@ export const getZapperApps = async (account: string) => {
     });
 
     return await res.json();
+}
+
+export const formatAndFilterZapperData = (data, APP_GROUPS) => {
+    let appGroupPositions = [];
+    return data
+      .forEach(app => {
+        app.products.filter(
+          product => !!product.assets
+            .find(a => APP_GROUPS.includes(`${app.appId}+${a.groupId}`))
+        ).forEach(
+          product => {
+            appGroupPositions = appGroupPositions.concat(
+              product.assets.filter(a => APP_GROUPS.includes(`${app.appId}+${a.groupId}`))
+                .map(a => {
+                  return {
+                    updatedAt: app.updatedAt,
+                    timestamp: +(new Date(app.updatedAt)),
+                    appGroup: `${app.appId}+${a.groupId}`,
+                    tokens: a.tokens,
+                    balanceUSD: a.balanceUSD,
+                    address: a.address,
+                  }
+                })
+            )
+          })
+      })
 }
 
 const pointsUrl = 'https://api.zapper.xyz/v1/api-clients/points';
