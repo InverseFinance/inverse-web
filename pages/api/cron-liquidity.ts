4e7243ce82f63e2117f7f8574b93079cc4213059
webmass <webmass@inverse.finance>
2023-04-11 14:56:24 +0200

    Liquidity: daily snapshot & histo

---
+++ pages/api/cron-liquidity.ts
@@ -0,0 +1,39 @@
+import 'source-map-support'
+import { getCacheFromRedis, redisSetWithTimestamp } from '@app/util/redis';
+import { timestampToUTC } from '@app/util/misc';
+import { liquidityCacheKey } from './transparency/liquidity';
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') res.status(405).json({ success: false });
+  else if (req.headers.authorization !== `Bearer ${process.env.API_SECRET_KEY}`) return res.status(401).json({ success: false });
+
+  try {
+    const liquidityData = await (await fetch('https://www.inverse.finance/api/transparency/liquidity'))?.json();
+    const utcDate = timestampToUTC(+(new Date()));
+
+    await redisSetWithTimestamp(`liquidity-snapshot-${utcDate}`, {
+      liquidityApiVersion: liquidityCacheKey,
+      ...liquidityData,
+    });
+
+    const history = await getCacheFromRedis(`liquidity-history`, false, 0, true) || { entries: [] };
+    history.entries = history.entries.concat(liquidityData);
+
+    await redisSetWithTimestamp(`liquidity-history`, {
+      timestamp: +(new Date()),
+      entries: history.entries,
+    }, true);
+
+    res.status(200).json({
+      utcDate,
+      success: true,
+      dataTimestamp: liquidityData?.timestamp,
+      nbPools: liquidityData?.liquidity?.length,
+      liquidityApiVersion: liquidityCacheKey,
+      historyEntries: history.entries.length,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(200).json({ success: false })
+  }
+}
