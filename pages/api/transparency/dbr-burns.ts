fa3755c6d6650a4177d934268d702a09654458ec
webmass <webmass@inverse.finance>
2023-04-12 09:34:39 +0200

    Transparency: dbr burn stats

---
+++ pages/api/transparency/dbr-burns.ts
@@ -0,0 +1,75 @@
+import { Contract } from 'ethers'
+import 'source-map-support'
+import { DBR_ABI } from '@app/config/abis'
+import { getNetworkConfigConstants } from '@app/util/networks'
+import { getProvider } from '@app/util/providers';
+import { getCacheFromRedis, redisSetWithTimestamp } from '@app/util/redis'
+import { getBnToNumber } from '@app/util/markets'
+import { BURN_ADDRESS, CHAIN_ID } from '@app/config/constants';
+import { addBlockTimestamps, getCachedBlockTimestamps } from '@app/util/timestamps';
+import { NetworkIds } from '@app/types';
+
+const { DBR } = getNetworkConfigConstants();
+
+export default async function handler(req, res) {
+    const cacheKey = `dbr-burns-v1.0.0`;
+
+    try {
+        const validCache = await getCacheFromRedis(cacheKey, true, 1800, true);
+        if (validCache) {
+            res.status(200).json(validCache);
+            return
+        }
+
+        const provider = getProvider(CHAIN_ID);
+        const contract = new Contract(DBR, DBR_ABI, provider);
+
+        const archived = await getCacheFromRedis(cacheKey, false, 0, true);
+        const pastTotalEvents = archived?.totalBurns || [];
+
+        const lastKnownEvent = pastTotalEvents?.length > 0 ? (pastTotalEvents[pastTotalEvents.length - 1]) : {};
+        const newStartingBlock = lastKnownEvent ? lastKnownEvent?.blockNumber + 1 : 0;
+
+        const newBurnEvents = await contract.queryFilter(
+            contract.filters.Transfer(undefined, BURN_ADDRESS),
+            newStartingBlock ? newStartingBlock : 0x0,
+        );
+
+        const blocks = newBurnEvents.map(e => e.blockNumber);
+
+        await addBlockTimestamps(
+            blocks,
+            NetworkIds.mainnet,
+        );
+        const timestamps = await getCachedBlockTimestamps();
+
+        const newBurns = newBurnEvents.map(e => {
+            return {
+                timestamp: timestamps[NetworkIds.mainnet][e.blockNumber] * 1000,
+                blockNumber: e.blockNumber,
+                amount: getBnToNumber(e.args[2]),
+            };
+        }).filter(e => e.amount > 0);
+
+        const resultData = {
+            timestamp: +(new Date()),
+            totalBurns: pastTotalEvents.concat(newBurns),            
+        };
+
+        await redisSetWithTimestamp(cacheKey, resultData, true);
+
+        res.status(200).json(resultData)
+    } catch (err) {
+        console.error(err);
+        // if an error occured, try to return last cached results
+        try {
+            const cache = await getCacheFromRedis(cacheKey, false);
+            if (cache) {
+                console.log('Api call failed, returning last cache found');
+                res.status(200).json(cache);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
