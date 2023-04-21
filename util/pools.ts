8191f4d694d4480ab1664ae196eadb8201e5ac2d
webmass <webmass@inverse.finance>
2023-04-21 16:54:52 +0200

    Liquidity: fix $/day

--- util/pools.ts
+++ util/pools.ts
@@ -30,8 +30,7 @@ export const getPoolsAggregatedStats = (
     const avgDolaWeight = itemsWithoutChildren.reduce((prev, curr) => prev + (curr.dolaWeight / 100 * curr.tvl), 0) / tvl * 100;
     // pol, apy, should include derived pools such as Aura
     const pol = itemsWithoutChildren.reduce((prev, curr) => prev + curr.ownedAmount, 0);
-    const rewardDay = filteredItems.filter(lp => !lp.deduce).reduce((prev, curr) => prev + curr.rewardDay, 0)
-        + filteredItems.filter(lp => lp.isFed).reduce((prev, curr) => prev + curr.rewardDay, 0);
+    const rewardDay = filteredItems.filter(lp => !lp.deduce).reduce((prev, curr) => prev + curr.rewardDay, 0);       
     const avgApy = filteredItems.reduce((prev, curr) => prev + ((curr.apy || 0) / 100 * curr.tvl), 0) / tvl * 100;
 
     return {
