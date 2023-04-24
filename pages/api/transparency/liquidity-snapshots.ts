435e4db9cca58211c06ac178bd6f2fab20fa3ac8
webmass <webmass@inverse.finance>
2023-04-24 09:12:21 +0200

    Liquidity: snapshots api

---
+++ pages/api/transparency/liquidity-snapshots.ts
@@ -0,0 +1,16 @@
+import 'source-map-support'
+import { getCacheFromRedis } from '@app/util/redis'
+
+export default async function handler(req, res) {
+    try {
+        const validCache = await getCacheFromRedis('liquidity-history', false, 0, true);
+        if (validCache) {
+            res.status(200).json(validCache);
+            return
+        }
+        
+        res.status(400).json({ msg: 'liquidity-history not initialized' });
+    } catch (err) {
+        console.error(err);
+    }
+}
