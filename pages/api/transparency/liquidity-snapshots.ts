6f3f8d88b6bb0b76f6811f51f4ec0a0ab9775dc3
webmass <webmass@inverse.finance>
2023-04-24 13:19:11 +0200

    Liquidity: snapshot err case

--- pages/api/transparency/liquidity-snapshots.ts
+++ pages/api/transparency/liquidity-snapshots.ts
@@ -12,5 +12,6 @@ export default async function handler(req, res) {
         res.status(400).json({ msg: 'liquidity-history not initialized' });
     } catch (err) {
         console.error(err);
+        res.status(500).json({ status: 'ko' });
     }
 }
