6499d875fd64379782f7d38f6856e5a7d02c1506
webmass <webmass@inverse.finance>
2023-05-01 15:10:20 +0200

    Liquidity: dola biggest LPs and bridges

--- components/Transparency/DolaBridges.tsx
+++ components/Transparency/DolaBridges.tsx
@@ -1,4 +1,4 @@
-import { Flex, HStack, Stack, Text, Image } from "@chakra-ui/react"
+import { Flex, Stack, Text, Image } from "@chakra-ui/react"
 import Table from "../common/Table"
 import { preciseCommify } from "@app/util/misc"
 import { NETWORKS_BY_CHAIN_ID } from "@app/config/networks"
@@ -21,43 +21,48 @@ const CellText = ({ ...props }) => {
     return <Text align="center" fontSize="12px" {...props} />
 }
 
-const FilterItem = ({ ...props }) => {
-    return <HStack fontSize="14px" fontWeight="normal" justify="flex-start" {...props} />
-}
-
 const noPropagation = (e: React.MouseEvent<HTMLElement>) => e.stopPropagation();
+
+const defaultBridge = { url: 'https://app.multichain.org/#/router', name: 'Multichain' };
+const nativeBridges = {
+    'Optimism': { url: 'https://app.optimism.io/bridge/deposit', name: 'Native bridge' },
+    'Arbitrum': { url: 'https://bridge.arbitrum.io/', name: 'Native bridge' },
+    'Polygon': { url: 'https://wallet.polygon.technology/polygon/bridge/deposit', name: 'Native bridge' },
+    'Avalanche': { url: 'https://core.app/bridge/', name: 'Native bridge' },
+    'BSC': defaultBridge,
+    'Ethereum': defaultBridge,
+};
+
 
 const columns = [
     {
         field: 'networkName',
         label: 'Chain',
-        showFilter: true,
-        filterWidth: '70px',
-        filterItemRenderer: ({ chainId }) => <FilterItem>
-            <Image src={NETWORKS_BY_CHAIN_ID[chainId].image} h='20px' w='20px' borderRadius="50px" title={NETWORKS_BY_CHAIN_ID[chainId].name} />
-            <Text>{NETWORKS_BY_CHAIN_ID[chainId].name}</Text>
-        </FilterItem>,
-        header: ({ ...props }) => <ColHeader minWidth="80px" justify="center"  {...props} />,
+        header: ({ ...props }) => <ColHeader minWidth="80px" justify="flex-start"  {...props} />,
         value: ({ chainId, networkName }) => {
             const net = NETWORKS_BY_CHAIN_ID[chainId];
-            return <Cell onClick={noPropagation} minWidth='80px' spacing="2" justify="center" alignItems="center" direction="row">
+            return <Cell onClick={noPropagation} minWidth='80px' spacing="2" justify="flex-start" alignItems="center" direction="row">
                 <Image src={net.image} ignoreFallback={true} title={net.name} alt={net.name} w={5} h={5} mr="2" />
+                <CellText>{networkName}</CellText>
             </Cell>
         },
-    },   
+    },
     {
-        field: 'tvlChain',
-        label: 'TVL (Chain)',
+        field: 'mainBridge',
+        label: 'Main Bridge',
         header: ({ ...props }) => <ColHeader minWidth="190px" justify="center"  {...props} />,
-        value: ({ tvlChain }) => {
+        value: ({ networkName }) => {
+            const bridge = nativeBridges[networkName] || defaultBridge;
             return <Cell minWidth="190px" justify="center" fontSize="15px">
-                <CellText>{preciseCommify(tvlChain, 0, true)}</CellText>
+                <Link fontSize="12px" textDecoration="underline" isExternal _target="_blank" href={bridge.url}>
+                    {bridge.name} <ExternalLinkIcon />
+                </Link>
             </Cell>
         },
     },
     {
         field: 'apy',
-        label: 'Highest APY',
+        label: 'Highest LP APY',
         header: ({ ...props }) => <ColHeader minWidth="170px" justify="center"  {...props} />,
         value: ({ apy, top1Apy }) => {
             const link = getLpLink(top1Apy);
@@ -72,7 +77,7 @@ const columns = [
     },
     {
         field: 'tvl',
-        label: 'Highest TVL',
+        label: 'Highest LP TVL',
         header: ({ ...props }) => <ColHeader minWidth="190px" justify="center"  {...props} />,
         value: ({ tvl, top1Tvl }) => {
             const link = getLpLink(top1Tvl);
@@ -90,7 +95,13 @@ const columns = [
 export const DolaBridges = ({
     items
 }) => {
-    return <Container label="By chains">
+    return <Container
+        noPadding
+        p="0"
+        label="Biggest LPs by chain and their bridges"
+        description="Learn more on Bridging DOLA across chains"
+        href={'https://docs.inverse.finance/inverse-finance/inverse-finance/product-guide/tokens/dola'}
+    >
         <Table
             key="chainId"
             columns={columns}
