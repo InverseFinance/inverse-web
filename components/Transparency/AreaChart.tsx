a109637b2936d2ca891159aa1afc076db8a0a856
webmass <webmass@inverse.finance>
2023-05-15 08:40:08 +0200

    AreaChart: zoom options, combo

--- components/Transparency/AreaChart.tsx
+++ components/Transparency/AreaChart.tsx
@@ -1,7 +1,7 @@
 import { shortenNumber } from '@app/util/markets';
-import { VictoryChart, VictoryLabel, VictoryAxis, VictoryArea, VictoryTheme, VictoryClipContainer, VictoryVoronoiContainer, VictoryAreaProps, VictoryAxisProps, VictoryLabelProps } from 'victory';
+import { VictoryChart, VictoryLabel, VictoryAxis, VictoryArea, VictoryTheme, VictoryClipContainer, VictoryVoronoiContainer, VictoryAreaProps, VictoryAxisProps, VictoryLabelProps, VictoryZoomContainer, VictoryBrushContainer, createContainer } from 'victory';
 import moment from 'moment'
-import { Box, useMediaQuery } from '@chakra-ui/react';
+import { Box, VStack, useMediaQuery } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { FlyoutTooltip } from './FlyoutTooltip';
 import { useAppTheme } from '@app/hooks/useAppTheme';
@@ -12,6 +12,8 @@ const strokeColors = {
     secondary: '#00FF8A',
     info: '#43a0e2',
 }
+
+const VictoryZoomVoronoiContainer = createContainer("zoom", "voronoi");
 
 export type AreaChartProps = {
     data: CoordinatesArray,
@@ -27,6 +29,7 @@ export type AreaChartProps = {
     isDollars?: boolean,
     isPerc?: boolean,
     autoMinY?: boolean,
+    allowZoom?: boolean,
     mainColor?: 'primary' | 'secondary' | 'info',
     titleProps?: VictoryLabelProps,
 };
@@ -47,13 +50,15 @@ export const AreaChart = ({
     isPerc = false,
     autoMinY = false,
     titleProps,
+    allowZoom = false,
 }: AreaChartProps) => {
     const [isLargerThan] = useMediaQuery('(min-width: 900px)');
     const [rightPadding, setRightPadding] = useState(50);
+    const [selectedDomain, setSelectedDomain] = useState(undefined);
     const maxY = data.length > 0 ? Math.max(...data.map(d => d.y)) : 95000000;
     const minY = data.length > 0 ? Math.min(...data.map(d => d.y)) : 0;
     const { themeStyles } = useAppTheme();
-    
+
     const _axisStyle = axisStyle || {
         tickLabels: { fill: themeStyles.colors.mainTextColor, fontFamily: 'Inter', fontSize: '12px' },
         grid: {
@@ -68,62 +73,109 @@ export const AreaChart = ({
         setRightPadding(isLargerThan ? 50 : 20)
     }, [isLargerThan]);
 
+    const handleZoom = (domain) => {
+        setSelectedDomain(domain);
+    }
+
+    const zoomProps = { zoomDimension: 'x', zoomDomain: selectedDomain, onZoomDomainChange: handleZoom };
+    const voronoiProps = {
+        mouseFollowTooltips: true, voronoiDimension: 'x', labelComponent: <FlyoutTooltip />,
+        labels: ({ datum }) => {
+            return (
+                moment(datum.x).format('MMM Do YYYY') + '\n' + `${shortenNumber(datum.y, 2, isDollars)}${isPerc ? '%' : ''}`
+            )
+        }
+    };
+
     return (
-        <Box
-            width={width}
-            height={height}
-            position="relative"
-        >
-            <VictoryChart
+        <VStack width={width} spacing="0" position="relative">
+            <Box
                 width={width}
                 height={height}
-                theme={VictoryTheme.grayscale}
-                animate={{ duration: 500 }}
-                scale={{ x: "time" }}
-                padding={{ top: 50, bottom: 50, left: 50, right: rightPadding }}
-                containerComponent={!showTooltips ? undefined :
-                    <VictoryVoronoiContainer
-                        mouseFollowTooltips={true}
-                        voronoiDimension="x"
-                        labelComponent={<FlyoutTooltip />}
-                        labels={({ datum }) => {
-                            return (
-                                moment(datum.x).format('MMM Do YYYY') + '\n' + `${shortenNumber(datum.y, 2, isDollars)}${isPerc ? '%' : ''}`
-                            )
-                        }}
-                    />
-                }
+                position="relative"
             >
-                {
-                    !!title && <VictoryLabel text={title} style={{ fill: themeStyles.colors.mainTextColor, fontFamily: 'Inter', fontSize: '16px' }} x={Math.floor(width / 2)} y={20} textAnchor="middle" {...titleProps} />
-                }
-                <VictoryAxis style={_axisStyle} dependentAxis tickFormat={(t) => `${shortenNumber(t, t > 1000000 ? 2 : 0, isDollars)}${isPerc ? '%' : ''}`} />
-                <VictoryAxis style={_axisStyle} />
-                <VictoryArea
-                    domain={{ y: [autoMinY ? minY - _yPad < 0 ? 0 : minY - _yPad : 0, maxY + _yPad] }}
-                    groupComponent={<VictoryClipContainer clipId="area-chart" />}
-                    data={data}
-                    labelComponent={
-                        <VictoryLabel
-                            dx={-rightPadding - 30}
-                            textAnchor="start"
-                            verticalAnchor="start"
-                        />
+                <VictoryChart
+                    width={width}
+                    height={height}
+                    theme={VictoryTheme.grayscale}
+                    animate={{ duration: 500 }}
+                    scale={{ x: "time" }}
+                    padding={{ top: 50, bottom: 50, left: 50, right: rightPadding }}
+                    containerComponent={
+                        showTooltips && allowZoom ?
+                            <VictoryZoomVoronoiContainer {...zoomProps} {...voronoiProps} />
+                            : showTooltips && !allowZoom ?
+                                <VictoryVoronoiContainer {...voronoiProps} /> :
+                                !showTooltips && allowZoom ? <VictoryZoomContainer {...zoomProps} /> : null
                     }
-                    labels={
-                        ({ data, index }) => {
-                            const isMax = (maxY === data[index].y && index > 0 && maxY !== data[index - 1].y);
-                            const pointLabel = data[index].eventPointLabel;
-                            return !!pointLabel ? pointLabel : (showLabels || (isMax && showMaxY) ? `${isMax && 'High: '}${shortenNumber(data[index].y, 2, isDollars)}${isPerc ? '%' : ''}` : '')
+                >
+                    {
+                        !!title && <VictoryLabel text={title} style={{ fill: themeStyles.colors.mainTextColor, fontFamily: 'Inter', fontSize: '16px' }} x={Math.floor(width / 2)} y={20} textAnchor="middle" {...titleProps} />
+                    }
+                    <VictoryAxis style={_axisStyle} dependentAxis tickFormat={(t) => `${shortenNumber(t, 0, isDollars)}${isPerc ? '%' : ''}`} />
+                    <VictoryAxis style={_axisStyle} />
+                    <VictoryArea
+                        domain={{ y: [autoMinY ? minY - _yPad < 0 ? 0 : minY - _yPad : 0, maxY + _yPad] }}
+                        groupComponent={<VictoryClipContainer clipId="area-chart" />}
+                        data={data}
+                        labelComponent={
+                            <VictoryLabel
+                                dx={-rightPadding - 30}
+                                textAnchor="start"
+                                verticalAnchor="start"
+                            />
                         }
-                    }
-                    style={{
-                        data: { fillOpacity: 0.9, fill: `url(#${mainColor}-gradient)`, stroke: strokeColors[mainColor], strokeWidth: 1 },
-                        labels: { fill: themeStyles.colors.mainTextColor, fontSize: '12px', fontFamily: 'Inter' }
-                    }}
-                    interpolation={interpolation}
-                />
-            </VictoryChart>
-        </Box >
+                        labels={
+                            ({ data, index }) => {
+                                const isMax = (maxY === data[index].y && index > 0 && maxY !== data[index - 1].y);
+                                const pointLabel = ''//data[index].eventPointLabel;
+                                return !!pointLabel ? pointLabel : (showLabels || (isMax && showMaxY) ? `${isMax && 'High: '}${shortenNumber(data[index].y, 2, isDollars)}${isPerc ? '%' : ''}` : '')
+                            }
+                        }
+                        style={{
+                            data: { fillOpacity: 0.9, fill: `url(#${mainColor}-gradient)`, stroke: strokeColors[mainColor], strokeWidth: 1 },
+                            labels: { fill: themeStyles.colors.mainTextColor, fontSize: '12px', fontFamily: 'Inter' }
+                        }}
+                        interpolation={interpolation}
+                    />
+                </VictoryChart>
+            </Box>
+            {
+                allowZoom && <Box
+                    width={width}
+                    height={50}
+                    position="relative"
+                >
+                    <VictoryChart
+                        width={width}
+                        height={50}
+                        theme={VictoryTheme.grayscale}
+                        animate={{ duration: 500 }}
+                        scale={{ x: "time" }}
+                        padding={{ top: 0, bottom: 0, left: 50, right: rightPadding }}
+                        containerComponent={
+                            <VictoryBrushContainer
+                                responsive={false}
+                                brushDimension="x"
+                                brushDomain={selectedDomain}
+                                onBrushDomainChange={handleZoom}
+                            />
+                        }
+                    >
+                        <VictoryAxis style={_axisStyle} dependentAxis tickFormat={(t) => ``} />
+                        <VictoryAxis style={_axisStyle} />
+                        <VictoryArea
+                            domain={{ y: [autoMinY ? minY - _yPad < 0 ? 0 : minY - _yPad : 0, maxY + _yPad] }}
+                            groupComponent={<VictoryClipContainer clipId="area-chart" />}
+                            data={data}
+                            style={{
+                                data: { fillOpacity: 0.2, fill: `url(#${mainColor}-gradient)`, stroke: strokeColors[mainColor], strokeWidth: 1 },
+                            }}
+                            interpolation={interpolation}
+                        />
+                    </VictoryChart>
+                </Box>
+            }
+        </VStack>
     )
 }
