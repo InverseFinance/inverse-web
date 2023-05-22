c23a647f9aa5953f49ff892c12af2e89748cc1c2
webmass <webmass@inverse.finance>
2023-05-22 12:41:22 +0200

    Firm: inv positions

--- pages/api/f2/firm-positions.ts
+++ pages/api/f2/firm-positions.ts
@@ -112,7 +112,7 @@ export default async function handler(req, res) {
     });
 
     const resultData = {
-      positions: positions.filter(p => p.debt > 0),
+      positions: positions.filter(p => p.debt > 0 || p.deposits > 0.01),
       // marketUsersAndEscrows,
       timestamp: +(new Date()),
     }
