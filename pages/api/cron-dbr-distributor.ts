64e20a92cdb8e37aea73e6cbe866cf48d89ed96a
webmass <webmass@inverse.finance>
2023-06-06 08:08:58 +0200

    DBR distributor add missing return

--- pages/api/cron-dbr-distributor.ts
+++ pages/api/cron-dbr-distributor.ts
@@ -38,6 +38,7 @@ export default async function handler(req, res) {
       res.status(404).json({
         success: false,
       });
+      return
     }
 
     const provider = getProvider(NetworkIds.mainnet);
