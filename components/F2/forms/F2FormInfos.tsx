1899f062b15c9e58f70df6f84f3976bd35253956
webmass <webmass@inverse.finance>
2023-06-13 17:23:29 +0200

    Firm: wording

--- components/F2/forms/F2FormInfos.tsx
+++ components/F2/forms/F2FormInfos.tsx
@@ -230,7 +230,7 @@ export const F2FormInfos = (props: { debtAmountNumInfo: number, collateralAmount
             },
             {
                 tooltip: "DBR tokens needed for the borrow, they will be automatically used to cover borrowing interests over time. Don't sell them unless you know what you're doing! When auto-buying extra DBRs are added as cost to cover the auto-buyed DBRs.",
-                title: `${isAutoDBR ? 'Auto-buy ' : ''}DBR cost`,
+                title: `${isAutoDBR ? 'Auto-buy DBR cost' : 'DBR worth at market price'}`,
                 value: dbrCover > 0 && isDeposit ? `~${shortenNumber(dbrCover, 2)} DBRs (${shortenNumber(dbrCoverDebt, 2, true)})` : '-',
             },
         ],
