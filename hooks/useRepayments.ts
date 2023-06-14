537ec7263348c11b1fa0352ddb66c11a3b447dbb
webmass <webmass@inverse.finance>
2023-06-14 15:54:43 +0200

    Update from main

--- hooks/useRepayments.ts
+++ hooks/useRepayments.ts
@@ -1,9 +1,9 @@
 import { SWR } from "@app/types"
 import { useCacheFirstSWR } from "./useCustomSWR"
-import { fetcher, fetcher60sectimeout } from "@app/util/web3"
+import { fetcher60sectimeout } from "@app/util/web3"
 
 export const useRepayments = (): SWR => {
-  const { data, error } = useCacheFirstSWR(`/api/transparency/repayments?v7`, fetcher, fetcher60sectimeout);
+  const { data, error } = useCacheFirstSWR(`/api/transparency/repayments?v7`, fetcher60sectimeout);
   const _data = data || {};
   _data['totalDolaIncludingIOURepayedByDAO'] = (_data['totalDolaRepayedByDAO']||[]).concat((_data['dolaForIOUsRepayedByDAO']||[]))
   return {
