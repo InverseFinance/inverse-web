34be7f1318a745e7f8297416f4e123e60661fa08
webmass <webmass@inverse.finance>
2023-06-23 11:52:07 +0200

    Firm: portfolio value staking via histo balance

--- components/F2/WorthEvoChartContainer.tsx
+++ components/F2/WorthEvoChartContainer.tsx
@@ -1,5 +1,5 @@
 
-import { useFirmMarketEvolution, useHistoricalPrices } from "@app/hooks/useFirm";
+import { useEscrowBalanceEvolution, useFirmMarketEvolution, useHistoricalPrices } from "@app/hooks/useFirm";
 import { F2Market } from "@app/types";
 import { useContext, useEffect, useState } from "react";
 import { useAccount } from "@app/hooks/misc";
@@ -25,14 +25,15 @@ export const WorthEvoChartContainer = ({
         setChartWidth(isLargerThan ? maxWidth : (screen.availWidth || screen.width) - 50)
     }, [isLargerThan, maxWidth]);
 
-    const { deposits } = useContext(F2MarketContext);
+    const { deposits, escrow } = useContext(F2MarketContext);
     const { prices } = useHistoricalPrices(market.underlying.coingeckoId);
     const { prices: dbrPrices } = useHistoricalPrices('dola-borrowing-right');
-    const { events, depositedByUser } = useFirmMarketEvolution(market, account);
+    const { events, depositedByUser, blockNumbers } = useFirmMarketEvolution(market, account);
+    const { evolution: escrowBalanceEvolution } = useEscrowBalanceEvolution(account, escrow, market.address, blockNumbers);
 
     const start = events ? events.find(e => e.actionName === 'Deposit')?.timestamp : undefined;
 
-    const collateralRewards = (deposits) - depositedByUser;
+    const collateralRewards = Math.max((deposits) - depositedByUser, 0);
 
     const pricesAtEvents = events.map(e => {
         const price = prices.find(p => timestampToUTC(p[0]) === timestampToUTC(e.timestamp))?.[1];
@@ -52,16 +53,18 @@ export const WorthEvoChartContainer = ({
 
     const data = relevantPrices.map((p, i) => {
         const event = events.find(e => !e.isClaim && e.timestamp === p[0]);
+        const histoEscrowBalanceFromApi = escrowBalanceEvolution?.findLast(e => e.timestamp <= p[0])?.balance;        
         const lastCollateralEvent = events.findLast(e => !e.isClaim && e.timestamp <= p[0]);
         const unstakedCollateralBalance = Math.max(lastCollateralEvent?.unstakedCollateralBalance || 0, 0);
+        const balance = histoEscrowBalanceFromApi ?? unstakedCollateralBalance;
         const debt = Math.max(lastCollateralEvent?.debt || 0, 0);
         const claimEvent = (!event || event?.actionName !== 'ForceReplenish') ? events.find(e => e.isClaim && e.timestamp === p[0]) : undefined;
         const lastClaimEvent = events.findLast(e => e.isClaim && e.timestamp <= p[0]);
         const claims = lastClaimEvent?.claims || 0;
         const dbrPrice = dbrPrices.find(dbrPrice => timestampToUTC(dbrPrice[0]) === timestampToUTC(p[0]))?.[1] || 0;
         const timeProgression = (p[0] - start) / (now - start);
-        // TODO: better estimation
-        const estimatedStakedBonus = unstakedCollateralBalance ? Math.max(collateralRewards * timeProgression, 0) : 0;
+
+        const estimatedStakedBonus = balance - unstakedCollateralBalance;
         const claimsUsd = claims * dbrPrice;
         return {
             timestamp: p[0],
@@ -73,7 +76,8 @@ export const WorthEvoChartContainer = ({
             isEvent: !!event,
             event,
             worth: unstakedCollateralBalance * p[1],
-            totalWorth: claimsUsd + unstakedCollateralBalance * p[1] + estimatedStakedBonus * p[1],
+            stakedWorth: balance * p[1],
+            totalWorth: claimsUsd + balance * p[1],
             debt,
             depositedByUser,
             claims,
