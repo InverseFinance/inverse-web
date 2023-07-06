22f78390ff384a5a47ba93550b0c1b065feae0d6
webmass <webmass@inverse.finance>
2023-07-06 17:43:27 +0200

    ALE: leverage up util

---
+++ util/firm-ale.ts
@@ -0,0 +1,77 @@
+import { BigNumber, Contract } from "ethers";
+import { getNetworkConfigConstants } from "./networks";
+import { JsonRpcSigner } from "@ethersproject/providers";
+import { F2_ALE_ABI } from "@app/config/abis";
+import { getFirmSignature } from "./f2";
+import { F2Market } from "@app/types";
+import { get0xQuote } from "./zero";
+
+const { F2_ALE, DOLA } = getNetworkConfigConstants();
+
+export const getAleContract = (signer: JsonRpcSigner) => {
+    return new Contract(F2_ALE, F2_ALE_ABI, signer);
+}
+
+export const prepareLeveragePosition = async (
+    signer: JsonRpcSigner,
+    market: F2Market,
+    dolaToBorrow: BigNumber,
+) => {
+    const signatureResult = await getFirmSignature(signer, market.address, dolaToBorrow, 'BorrowOnBehalf');
+    if (signatureResult) {
+        const { deadline, r, s, v } = signatureResult;
+        let get0xQuoteResult;
+        try {
+            get0xQuoteResult = await get0xQuote(market.collateral, DOLA, dolaToBorrow.toString());
+        } catch (e) {
+            console.log(e);
+            return Promise.reject("Getting a quote from 0x failed");
+        }
+        const { data, allowanceTarget, to: swapTarget, value } = get0xQuoteResult;
+        return leveragePosition(
+            signer,
+            dolaToBorrow,
+            market.collateral,
+            allowanceTarget,
+            swapTarget,
+            data,
+            deadline,
+            v,
+            r,
+            s,
+            '0x',
+            value,
+        );
+    }
+    return Promise.reject("Signature failed or canceled");
+}
+
+export const leveragePosition = (
+    signer: JsonRpcSigner,
+    dolatoBorrow: BigNumber,
+    buyAd: string,
+    zeroXspender: string,
+    zeroXtarget: string,
+    swapData: string,
+    deadline: number,
+    v: number,
+    r: string,
+    s: string,
+    helperTransformData: string,
+    value: string,
+) => {
+    return getAleContract(signer)
+        .leveragePosition(            
+            dolatoBorrow,
+            buyAd,
+            zeroXspender,
+            zeroXtarget,
+            swapData,
+            deadline,
+            v,
+            r,
+            s,
+            helperTransformData,
+            { value },
+        );
+}
