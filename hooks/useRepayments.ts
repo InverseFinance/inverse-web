d5607f08b398bd516c40efd4963965cf62a83004
webmass <webmass@inverse.finance>
2023-07-06 14:06:35 +0200

    Repayments: add loader

--- hooks/useRepayments.ts
+++ hooks/useRepayments.ts
@@ -2,7 +2,7 @@ import { SWR } from "@app/types"
 import { useCacheFirstSWR } from "./useCustomSWR"
 import { fetcher60sectimeout } from "@app/util/web3"
 
-export const useRepayments = (): SWR => {
+export const useRepayments = (): SWR & { data: any, isLoading: boolean, isError: boolean } => {
   const { data, error } = useCacheFirstSWR(`/api/transparency/repayments?v=1`, fetcher60sectimeout);
   const _data = data || {};
   _data['totalDolaIncludingIOURepaidByDAO'] = (_data['totalDolaRepaidByDAO']||[]).concat((_data['dolaForIOUsRepaidByDAO']||[]))
