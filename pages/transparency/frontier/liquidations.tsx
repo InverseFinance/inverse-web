30f3e8de94fe14611d7403f2ab365858a39db3a4
webmass <webmass@inverse.finance>
2023-07-24 10:22:05 +0200

    Transparency: fonrtier liquidations naming

--- pages/transparency/frontier/liquidations.tsx
+++ pages/transparency/frontier/liquidations.tsx
@@ -1,7 +1,7 @@
 import Layout from '@app/components/common/Layout'
 import { AppNav } from '@app/components/common/Navbar'
 import Head from 'next/head'
-import { TransparencyFrontierTabs, TransparencyTabs } from '@app/components/Transparency/TransparencyTabs'
+import { TransparencyFrontierTabs } from '@app/components/Transparency/TransparencyTabs'
 import { LiquidationsTable } from '@app/components/Transparency/LiquidationsTable'
 
 export const LiquidationsPage = () => {
@@ -10,11 +10,11 @@ export const LiquidationsPage = () => {
         <Layout>
             <Head>
                 <title>{process.env.NEXT_PUBLIC_TITLE} - Transparency Liquidations</title>
-                <meta name="og:title" content="Inverse Finance - Transparency Liquidations" />
-                <meta name="og:description" content="Transparency: Liquidations" />
+                <meta name="og:title" content="Inverse Finance - Frontier Liquidations" />
+                <meta name="og:description" content="Frontier: Liquidations" />
                 <meta name="og:image" content="https://inverse.finance/assets/social-previews/transparency-portal.png" />
-                <meta name="description" content="Transparency: Liquidations" />
-                <meta name="keywords" content="Inverse Finance, transparency, liquidations" />
+                <meta name="description" content="Frontier: Liquidations" />
+                <meta name="keywords" content="Inverse Finance, frontier, liquidations" />
             </Head>
             <AppNav active="Transparency" activeSubmenu="Frontier (deprecated)" hideAnnouncement={true} />
             <TransparencyFrontierTabs active="frontier-liquidations" />
