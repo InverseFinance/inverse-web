f2ebbaff9dd1293edbe1871943d90f6b8ddc3044
webmass <webmass@inverse.finance>
2023-07-24 17:08:45 +0200

    Newsletter: update newsletter for active campaign

--- components/common/Newsletter/index.tsx
+++ components/common/Newsletter/index.tsx
@@ -3,9 +3,11 @@ import { Box } from '@chakra-ui/react'
 export const Newsletter = () => {
     return <Box overflow="hidden" h="250px">
         <iframe
-            src="https://inversestarship.substack.com/embed"
+            src="https://inverse36431.activehosted.com/f/1"
+            data-tf-redirect-target="_self"
             width="100%"
             height="320"
+            sandbox="allow-forms allow-scripts"
             frameBorder="0"
             allowTransparency={true}
             style={{ transform: 'translateY(-30px)' }}
