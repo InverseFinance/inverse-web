bd3935f84cef6e27ea5f58f94f16cafa49007339
webmass <webmass@inverse.finance>
2023-09-14 14:10:36 +0200

    Governance: delegation minor ui changes

--- components/Governance/SignDelegation.tsx
+++ components/Governance/SignDelegation.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Flex, Text, Textarea, useClipboard } from '@chakra-ui/react';
+import { Text, Textarea, VStack, useClipboard } from '@chakra-ui/react';
 import { EditIcon, CopyIcon } from '@chakra-ui/icons';
 import { InfoMessage } from '@app/components/common/Messages';
 import { SubmitButton } from '@app/components/common/Button';
@@ -48,7 +48,7 @@ export const SignDelegation = ({
                         <Text mt="2" mb="2" fontWeight="bold">
                             Delegation will apply for both {process.env.NEXT_PUBLIC_REWARD_TOKEN_SYMBOL} and x{process.env.NEXT_PUBLIC_REWARD_TOKEN_SYMBOL}.
                         </Text>
-                        <Text mt="2" mb="2" fontWeight="bold">
+                        <Text textDecoration="underline" mt="2" mb="2" fontWeight="bold">
                             Once signed, you will need to {
                                 isSelf ?
                                     'submit the signature data' :
@@ -65,19 +65,19 @@ export const SignDelegation = ({
 
             {
                 signature ?
-                    <Flex direction="column" mt="3">
-                        <Text align="center">Delegation Signature :</Text>
+                    <VStack spacing="4" mt="3" alignItems="flex-start">
+                        <Text fontWeight="bold" align="left">Delegation Signature :</Text>
                         <Textarea value={signature} readOnly borderWidth="0px" fontSize="sm" p={1.5} />
-                        <SubmitButton mt="2" onClick={onCopy} alignItems="center">
+                        <SubmitButton onClick={onCopy} alignItems="center">
                             <CopyIcon mr="2" boxSize={3} /> {hasLastSigCopied ? 'Copied !' : 'Copy'}
                         </SubmitButton>
-                    </Flex>
+                    </VStack>
                     : null
             }
             {
                 hasLastSigCopied &&
                 <InfoMessage
-                    alertProps={{ mt: "5", fontSize: '14px', w: 'full' }}
+                    alertProps={{ mt: "5", fontSize: '16px', w: 'full', fontWeight: 'bold' }}
                     description={
                         isSelf ?
                             'You can now submit your signature via the "SUBMIT SIGNATURES" button, the process is the same as delegation signatures'
