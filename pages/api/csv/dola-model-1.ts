57d9296c02ab67265c58576911a07812810c8cb0
webmass <webmass@inverse.finance>
2023-09-14 17:49:26 +0200

    Csv api: dola model 1

--- pages/api/csv/dola-model-1.ts
+++ pages/api/csv/dola-model-1.ts
@@ -1,10 +1,10 @@
 import { getCacheFromRedis, redisSetWithTimestamp } from "@app/util/redis";
-import { liquidityCacheKey } from "../../transparency/liquidity";
-import { repaymentsCacheKey } from "../../transparency/repayments";
+import { liquidityCacheKey } from "../transparency/liquidity";
+import { repaymentsCacheKey } from "../transparency/repayments";
 
 export default async (req, res) => {
     const cacheDuration = 240;
-    const cacheKey = 'feds-csv-v1';
+    const cacheKey = 'dola-modal-v1';
     res.setHeader('Cache-Control', `public, max-age=${cacheDuration}`);
 
     try {
@@ -18,23 +18,23 @@ export default async (req, res) => {
             getCacheFromRedis(repaymentsCacheKey, false),
         ]);
 
-        const feds = liquidityData.liquidity.filter(d => d.isFed);
+        const feds = liquidityData.liquidity.filter(d => d.isFed || d.address === '0x8272e1a3dbef607c04aa6e5bd3a1a134c8ac063b');
         const totalBorrowsOnFirm = liquidityData.firmBorrows;
         const currentDolaBadDebt = badDebtData.badDebts.DOLA.badDebtBalance;
 
         let csvData = `DOLA bad debt:,${currentDolaBadDebt},FiRM borrows:,${totalBorrowsOnFirm},Liquidity Cache:,~5min,Liquidity timestamp:,${liquidityData.timestamp},Bad debt timestamp:,${badDebtData.timestamp}\n`;
-        csvData += `Fed,Supply,DOLA balance,Pairing Depth\n`;
+        csvData += `LP,Fed,Fed Supply,DOLA balance,Pairing Depth\n`;
         feds.forEach((lp) => {
             const parentLp = liquidityData.liquidity.filter(l => !!l.deduce).find(l => l.deduce.includes(lp.address));
             const balanceSource = parentLp || lp;
-            csvData += `${lp.fedName},${lp.fedSupply},${balanceSource?.dolaBalance},${balanceSource?.pairingDepth}\n`;
+            csvData += `${lp.lpName},${lp.fedName||'n/a'},${lp.fedSupply||0},${balanceSource?.dolaBalance||0},${balanceSource?.pairingDepth||0}\n`;
         });
 
         redisSetWithTimestamp(cacheKey, { csvData });
 
         // Set response headers to indicate that you're serving a CSV file
         res.setHeader("Content-Type", "text/csv");
-        res.setHeader("Content-Disposition", "attachment; filename=inverse-feds.csv");
+        res.setHeader("Content-Disposition", "attachment; filename=dola-model-1.csv");
 
         // Send the CSV data as the response
         res.status(200).send(csvData);
@@ -43,7 +43,7 @@ export default async (req, res) => {
         const validCache = await getCacheFromRedis(cacheKey, false);
         const csvData = validCache?.csvData || `DOLA bad debt:,0,FiRM borrows:,0,Cache:,~5min,Liquidity timestamp:,0,Bad debt timestamp:,0,An error occured\n`;
         res.setHeader("Content-Type", "text/csv");
-        res.setHeader("Content-Disposition", "attachment; filename=inverse-feds.csv");
+        res.setHeader("Content-Disposition", "attachment; filename=dola-model-1.csv");
         // Send the CSV data as the response
         res.status(200).send(csvData);
     }
