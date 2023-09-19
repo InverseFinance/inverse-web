3f5a0a972f1aab45b19878945aeb25c913ebf9ac
webmass <webmass@inverse.finance>
2023-09-19 12:35:50 +0200

    Firm: stCvx apy

--- pages/api/f2/fixed-markets.ts
+++ pages/api/f2/fixed-markets.ts
@@ -5,7 +5,7 @@ import { getNetworkConfigConstants } from '@app/util/networks'
 import { getProvider } from '@app/util/providers';
 import { getCacheFromRedis, getCacheFromRedisAsObj, redisSetWithTimestamp } from '@app/util/redis'
 import { TOKENS } from '@app/variables/tokens'
-import { getBnToNumber, getCvxCrvAPRs, getCvxFxsAPRs, getDSRData, getGOhmData, getStYcrvData, getStethData } from '@app/util/markets'
+import { getBnToNumber, getCvxCrvAPRs, getCvxFxsAPRs, getDSRData, getGOhmData, getStCvxData, getStYcrvData, getStethData } from '@app/util/markets'
 import { BURN_ADDRESS, CHAIN_ID, ONE_DAY_MS, ONE_DAY_SECS } from '@app/config/constants';
 import { frontierMarketsCacheKey } from '../markets';
 import { cgPricesCacheKey } from '../prices';
@@ -137,9 +137,10 @@ export default async function handler(req, res) {
       getCvxCrvAPRs(provider),
       getCvxFxsAPRs(provider),
       getDSRData(),
+      getStCvxData(),
     ]);
 
-    let [stethData, gohmData, stYcrvData, cvxCrvData, cvxFxsData, dsrData] = externalYieldResults.map(r => {
+    let [stethData, gohmData, stYcrvData, cvxCrvData, cvxFxsData, dsrData, stCvxData] = externalYieldResults.map(r => {
       return r.status === 'fulfilled' ? r.value : {};
     });
 
@@ -160,6 +161,7 @@ export default async function handler(req, res) {
       'INV': invFrontierMarket.supplyApy || 0,
       'st-yCRV': stYcrvData?.apy || 0,
       'DAI': dsrData?.apy || 0,
+      'st-CVX': stCvxData?.apy || 0,
     };
 
     const xinvExRate = getBnToNumber(xinvExRateBn);
