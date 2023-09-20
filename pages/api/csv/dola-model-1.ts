7b67336e1b8c0b075529024c0549c53d8aa9fdbd
webmass <webmass@inverse.finance>
2023-09-20 09:02:22 +0200

    bump api

--- pages/api/csv/dola-model-1.ts
+++ pages/api/csv/dola-model-1.ts
@@ -3,7 +3,7 @@ import { repaymentsCacheKey } from "../transparency/repayments";
 
 export default async (req, res) => {
     const cacheDuration = 900;
-    const cacheKey = 'dola-modal-v1.0.0';
+    const cacheKey = 'dola-modal-v1.0.1';
     res.setHeader('Cache-Control', `public, max-age=${cacheDuration}`);
 
     try {
