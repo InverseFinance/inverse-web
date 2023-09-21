508cbe2ca579a74fc95d90299a67a7c1913ccdb5
webmass <webmass@inverse.finance>
2023-09-21 17:41:17 +0200

    Add SimpleModal

---
+++ components/common/Modal/SimpleModal.tsx
@@ -0,0 +1,47 @@
+import { Modal } from '@app/components/common/Modal';
+import { ModalProps, Stack, Text } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+import { useAppTheme } from '@app/hooks/useAppTheme';
+
+export type Props = {
+    title?: string,
+    isOpen: boolean
+    onClose: () => void
+    children?: ReactNode
+    minW?: any
+    modalProps?: ModalProps
+}
+
+const SimpleModal = ({
+    title = '',
+    isOpen,
+    onClose,
+    children,
+    minW,
+    modalProps,
+}: Props) => {
+    const { themeName } = useAppTheme();
+
+    return (
+        <Modal
+            onClose={onClose}
+            isOpen={isOpen}
+            scrollBehavior="inside"
+            header={
+                title ?
+                    <Stack minWidth={24} direction="row" align="center" >
+                        <Text>{title}</Text>
+                    </Stack>
+                    : undefined
+            }
+            minW={minW}
+            className={`blurred-container ${themeName}-bg`}
+            bg="transparent"
+            {...modalProps}
+        >
+            {children ? children : <></>}
+        </Modal>
+    )
+}
+
+export default SimpleModal;
