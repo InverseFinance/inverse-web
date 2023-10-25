f1b6ee74cb3e9cf44ac2ba2d48000e920a1535ba
webmass <webmass@inverse.finance>
2023-10-25 07:04:27 +0200

    POA: add UK

--- config/proof-of-agreement-texts.ts
+++ config/proof-of-agreement-texts.ts
@@ -1,4 +1,4 @@
-export const POA_VERSION = '1.0.1';
+export const POA_VERSION = '1.0.2';
 
 export const POA_TEXTS = {
     '1.0.0': [
@@ -10,6 +10,12 @@ export const POA_TEXTS = {
     '1.0.1': [
         `I have read and understood, and do hereby agree as a "User" to the Terms, including all future amendments thereto. Such agreement is irrevocable and will apply to all of the Site.`,
         `I'm at least 18 years of age (or the age of legal majority in the jurisdiction in which I reside), and not a citizen or resident of the USA or subject to any personal, national or international Sanctions.`,
+        `I acknowledge and agree that the Site solely provides access and information for data on the blockchain and that displayed information may be approximate or may contain inaccuracies. I accept that Site operators have no custody over my funds, ability or duty to transact on my behalf or power to reverse my transactions. The Site does not provide any warranty with respect to any of its provided operations.`,
+    ],
+    // add UK
+    '1.0.2': [
+        `I have read and understood, and do hereby agree as a "User" to the Terms, including all future amendments thereto. Such agreement is irrevocable and will apply to all of the Site.`,
+        `I'm at least 18 years of age (or the age of legal majority in the jurisdiction in which I reside), and not a citizen or resident of the USA or UK or subject to any personal, national or international Sanctions.`,
         `I acknowledge and agree that the Site solely provides access and information for data on the blockchain and that displayed information may be approximate or may contain inaccuracies. I accept that Site operators have no custody over my funds, ability or duty to transact on my behalf or power to reverse my transactions. The Site does not provide any warranty with respect to any of its provided operations.`,
     ],
 }
