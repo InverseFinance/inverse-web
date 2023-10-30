bb32cf4f6d148c5de01cc463ce90f7548716a695
webmass <webmass@inverse.finance>
2023-10-30 08:56:41 +0100

    wip: dola circ evo

--- pages/api/dola/circulating-supply-evolution.ts
+++ pages/api/dola/circulating-supply-evolution.ts
@@ -1,16 +1,17 @@
 import { Contract } from 'ethers'
 import 'source-map-support'
-import { DOLA_ABI, INV_ABI } from '@app/config/abis'
+import { DOLA_ABI } from '@app/config/abis'
 import { getProvider } from '@app/util/providers';
 import { getCacheFromRedis, redisSetWithTimestamp } from '@app/util/redis'
 import { BigNumber } from 'ethers';
 import { getNetworkConfigConstants } from '@app/util/networks';
 import { getBnToNumber } from '@app/util/markets'
 import { BLOCKS_PER_DAY, CHAIN_ID } from '@app/config/constants';
-import { throttledPromises, timestampToUTC } from '@app/util/misc';
+import { getOrClosest, throttledPromises, timestampToUTC } from '@app/util/misc';
 import { getGroupedMulticallOutputs } from '@app/util/multicall';
 import { addBlockTimestamps, getCachedBlockTimestamps } from '@app/util/timestamps';
 import { NetworkIds } from '@app/types';
+import { CHAIN_TOKENS, getToken } from '@app/variables/tokens';
 
 const {
   DOLA,
@@ -44,19 +45,20 @@ const CHAIN_BLOCKS_PER_DAY = {
   [NetworkIds.arbitrum]: 1 / 0.3 * 86400 * 30,
 }
 
-const farmers = FEDS.filter(fed => !!fed.incomeChainId && !!fed.incomeSrcAd)
+const FARMERS = FEDS.filter(fed => !!fed.incomeChainId && !!fed.incomeSrcAd)
   .map(fed => {
     return [[fed.incomeSrcAd, fed.incomeChainId]].concat(fed.oldIncomeSrcAds ? fed.oldIncomeSrcAds.map(ad => [ad, fed.incomeChainId]) : [[]]);
   })
   .flat()
-  .map(fed => {
-    return [fed[0], fed[1]];
+  .map(fedData => {
+    return [fedData[0], fedData[1]];
   });
+
+const FARMERS_CHAIN_IDS = [...new Set(FARMERS.map(f => f[1]))];
 
 const mainnetExcluded = [
   // AN_DOLA
   '0x7Fcb7DAC61eE35b3D4a51117A7c58D53f0a8a670',
-  // ...farmers,
   ...F2_MARKETS.map(m => [m.address, m.startingBlock]),
 ];
 
@@ -68,7 +70,7 @@ const getXchainTimestamps = async () => {
     const currentBlock = await provider.getBlockNumber();
     const intIncrement = Math.floor(CHAIN_BLOCKS_PER_DAY[net]);
     const nbDays = (currentBlock - startingBlock) / intIncrement;
-    const blocks = Array.from({ length: Math.ceil(nbDays) }, (_, i) => startingBlock + i * intIncrement).filter(b => b < (currentBlock));    
+    const blocks = Array.from({ length: Math.ceil(nbDays) }, (_, i) => startingBlock + i * intIncrement).filter(b => b < (currentBlock));
     await addBlockTimestamps(
       blocks,
       net,
@@ -90,24 +92,66 @@ export default async function handler(req, res) {
     //   res.status(200).send(validCache);
     //   return
     // }
-    await getXchainTimestamps()    
+    // await getXchainTimestamps()
 
     const cachedTimestamps = await getCachedBlockTimestamps();
     // per chain, map an utc date with a blockNumber
-    const utcBlockValues = {};
-    const blockUtcValues = {};
+    const utcKeyBlockValues = {};
+    const blockKeyUtcValue = {};
 
     [NetworkIds.mainnet, NetworkIds.arbitrum, NetworkIds.base, NetworkIds.optimism].forEach(chainId => {
-      if (!utcBlockValues[chainId]) utcBlockValues[chainId] = {};
-      if (!blockUtcValues[chainId]) blockUtcValues[chainId] = {};
-      Object.entries(cachedTimestamps[chainId]).forEach(([bn, ts]) => {
+      if (!utcKeyBlockValues[chainId]) utcKeyBlockValues[chainId] = {};
+      if (!blockKeyUtcValue[chainId]) blockKeyUtcValue[chainId] = {};
+      Object.entries(cachedTimestamps[chainId]).forEach(([block, ts]) => {
         const utcDate = timestampToUTC(ts * 1000);
-        utcBlockValues[chainId][utcDate] = bn;
-        blockUtcValues[chainId][bn] = utcDate;
+        utcKeyBlockValues[chainId][utcDate] = block;
+        blockKeyUtcValue[chainId][block] = utcDate;
       });
-    });
+    });    
 
-    const mainnetBlocks = Object.keys(utcBlockValues[CHAIN_ID])
+    const chainBalances = {};
+
+    for (let chainId of FARMERS_CHAIN_IDS) {
+      const chainFarmers = FARMERS.filter(f => f[1] === chainId);
+      const chainDola = getToken(CHAIN_TOKENS[chainId], 'DOLA');
+      const chainDolaContract = new Contract(chainDola.address!, DOLA_ABI, getProvider(chainId));
+      const chainBlocks = Object.values(utcKeyBlockValues[chainId])
+        .map(v => parseInt(v))
+        .filter(v => v >= CHAIN_START_BLOCKS[chainId]);
+
+      const balances = await throttledPromises(
+        (block: number) => {
+          return getGroupedMulticallOutputs([
+            chainFarmers.map(excludedData => {
+              const farmerAd = excludedData[0];
+              return {
+                contract: chainDolaContract,
+                functionName: 'balanceOf',
+                params: [farmerAd],
+                // 0 value if before startingBlock
+                forceFallback: FARMER_START_BLOCKS[farmerAd] > block,
+                fallbackValue: BigNumber.from('0'),
+              }
+            }),
+          ],
+            Number(chainId),
+            block,
+          );
+        },
+        chainBlocks,
+        5,
+        100,
+      );
+      const totalBalancesPerBlock = balances.map(blockFarmerBalances => blockFarmerBalances.flat().map(v => getBnToNumber(v)).reduce((prev, curr) => prev+curr, 0))
+      chainBalances[chainId] = {
+        balancesOnUtcDate: chainBlocks.map((block, i) => blockKeyUtcValue[chainId][block]),
+        blocks: chainBlocks,
+        balances: totalBalancesPerBlock,
+        asUtcObj: chainBlocks.reduce((prev, curr, i) => ({ ...prev, [blockKeyUtcValue[chainId][curr]]: totalBalancesPerBlock[i] }), {}),
+      };
+    }
+
+    const mainnetBlocks = Object.values(utcKeyBlockValues[CHAIN_ID])
       .map(v => parseInt(v))
       .filter(v => v >= CHAIN_START_BLOCKS[CHAIN_ID]);
 
@@ -137,21 +181,28 @@ export default async function handler(req, res) {
       100,
     );
 
-    const mainnetEvolution = mainnetData.map((dataAtBlock, i) => {
-      const [totalSupplyBn, balances] = dataAtBlock;      
-      const excluded = balances.reduce((prev, curr) => getBnToNumber(curr) + prev, 0);
-      const totalSupply = getBnToNumber(totalSupplyBn);
-      const circSupply = totalSupply - excluded;
+    const evolution = mainnetData.map((dataAtBlock, i) => {
+      const [totalSupplyBn, balances] = dataAtBlock;
+      const mainnetExcluded = balances.reduce((prev, curr) => getBnToNumber(curr) + prev, 0);
+      const totalSupply = getBnToNumber(totalSupplyBn);      
+      const utcDate = blockKeyUtcValue[CHAIN_ID][mainnetBlocks[i]];
+      const farmersExcluded = FARMERS_CHAIN_IDS.map(chainId => {
+        return getOrClosest(chainBalances[chainId], utcDate)||0;
+      }).reduce((prev, curr) => prev + curr, 0);
+      const circSupply = totalSupply - mainnetExcluded;
+      const circSupplyV2 = circSupply - farmersExcluded;
       return {
-        utcDate: blockUtcValues[CHAIN_ID][mainnetBlocks[i]],
+        utcDate,
         totalSupply,
         circSupply,
-        excluded,
+        circSupplyV2,
+        mainnetExcluded,
+        farmersExcluded,
         block: mainnetBlocks[i],
       }
     });
 
-    return res.status(200).send({ mainnetEvolution });
+    return res.status(200).send({ evolution });
   } catch (err) {
     console.error(err);
     // if an error occured, try to return last cached results
