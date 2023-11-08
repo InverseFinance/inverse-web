131e80327451df2b47ee8d105615706183bf1821
webmass <webmass@inverse.finance>
2023-11-08 15:02:05 +0100

    Features: no early access

--- config/features.ts
+++ config/features.ts
@@ -24,8 +24,8 @@ export const FEATURE_FLAGS: Features = CHAIN_FEATURE_FLAGS[CHAIN_ID] || defaultF
 export const INV_STAKED_MIN_FOR_EXTRA_FEATURES = 10;
 // Unlock some features to INV stakers first as an exclusive benefit
 export const INV_STAKERS_ONLY = {
-    firmLeverage: true,
-    lpZaps: true,
+    firmLeverage: false,
+    lpZaps: false,
 }
 export const isInvPrimeMember = (invStaked = 0) => {
     return invStaked >= INV_STAKED_MIN_FOR_EXTRA_FEATURES;
