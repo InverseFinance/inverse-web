3310a5e268d0582946a8227f1ebeee1f73696577
webmass <webmass@inverse.finance>
2023-11-10 14:22:19 +0100

    ALE: about modal

---
+++ components/F2/Modals/AboutAleModal.tsx
@@ -0,0 +1,25 @@
+import { Text, VStack } from "@chakra-ui/react"
+import Link from "@app/components/common/Link";
+import InfoModal from "@app/components/common/Modal/InfoModal";
+
+export const AboutAleModal = ({
+    isOpen,
+    onClose,
+}) => {
+    return <InfoModal
+        title="About the Accelerated Leverage Engine"
+        isOpen={isOpen}
+        onClose={onClose}
+        modalProps={{ minW: { base: '98vw', lg: '600px' }, scrollBehavior: 'inside' }}
+    >
+        <VStack spacing="4" p='4' alignItems="flex-start">
+            <Text>
+                The Accelerated Leverage Engine (ALE) is a feature that allows to increase the exposition to the collateral asset by flash-minting DOLA, buying the collateral on DEXes (via the 0x protocol and api) depositing the collateral, and then borrowing DOLA to burn the flash-minted DOLA.
+            </Text>
+            <Text>The result is similar to a "spot leverage"</Text>
+            <Link href="https://docs.inverse.finance/" isExternal target="_blank">
+                Learn more about ALE
+            </Link>
+        </VStack>
+    </InfoModal>
+}
