781b77178448ad045cecac98be2f61b45ad24e7f
webmass <webmass@inverse.finance>
2023-11-20 08:41:27 +0100

    Cron: add daily snapshot for utc dates blocks

---
+++ pages/api/cron-daily-block-timestamp.ts
@@ -0,0 +1,57 @@
+import 'source-map-support'
+import { getProvider } from '@app/util/providers';
+import { getCacheFromRedisAsObj, redisSetWithTimestamp } from '@app/util/redis'
+import { getNetworkConfigConstants } from '@app/util/networks';
+import { timestampToUTC } from '@app/util/misc';
+import { NetworkIds } from '@app/types';
+import { ARCHIVED_UTC_DATES_BLOCKS } from '@app/fixtures/utc-dates-blocks';
+
+const {
+  FEDS,  
+} = getNetworkConfigConstants();
+
+const FARMERS = FEDS.filter(fed => !!fed.incomeChainId && !!fed.incomeSrcAd)
+  .map(fed => {
+    return [[fed.incomeSrcAd, fed.incomeChainId]].concat(fed.oldIncomeSrcAds ? fed.oldIncomeSrcAds.map(ad => [ad, fed.incomeChainId]) : []);
+  })
+  .flat()
+  .map(fedData => {
+    return [fedData[0], fedData[1]];
+  });
+
+const FARMERS_CHAIN_IDS = [...new Set(FARMERS.map(f => f[1]))];
+
+export const DAILY_UTC_CACHE_KEY = `utc-dates-blocks`;
+
+export default async function handler(req, res) {  
+  try {
+    const cacheDuration = 3600;
+    res.setHeader('Cache-Control', `public, max-age=${cacheDuration}`);
+    // cache is now updated via daily cron job
+    const { data: cachedData, isValid } = await getCacheFromRedisAsObj(DAILY_UTC_CACHE_KEY, false, cacheDuration) || { cachedData: ARCHIVED_UTC_DATES_BLOCKS, isValid: false };
+
+    if (cachedData && isValid) {
+      res.status(200).send(cachedData);
+      return
+    }
+
+    // per chain, map an utc date with a blockNumber
+    const utcKeyBlockValues = {};
+    const utcDate = timestampToUTC(Date.now());
+
+    for(let chainId of [NetworkIds.mainnet, ...FARMERS_CHAIN_IDS]) {
+        const provider = getProvider(chainId);
+        const currentBlock = await provider.getBlockNumber();
+
+      if (!utcKeyBlockValues[chainId]) utcKeyBlockValues[chainId] = {};      
+      utcKeyBlockValues[chainId][utcDate] = currentBlock;
+    };
+
+    await redisSetWithTimestamp('utc-dates-blocks', utcKeyBlockValues);
+
+    return res.status(200).send({ success: true });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ success: false, err });
+  }
+}
