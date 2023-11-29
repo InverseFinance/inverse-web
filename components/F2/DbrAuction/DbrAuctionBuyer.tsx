7bb1a5207aa79a3777028ccd44fb0ba4eeb08d2f
webmass <webmass@inverse.finance>
2023-11-29 09:47:57 +0100

    DbrAuctions: refacto & add price infos

--- components/F2/DbrAuction/DbrAuctionBuyer.tsx
+++ components/F2/DbrAuction/DbrAuctionBuyer.tsx
@@ -2,7 +2,7 @@ import { VStack, Text, HStack, Divider } from "@chakra-ui/react"
 import { DBR_AUCTION_ADDRESS, sellDolaForDbr } from "@app/util/dbr-auction"
 import useEtherSWR from "@app/hooks/useEtherSWR";
 import { useWeb3React } from "@web3-react/core";
-import { getBnToNumber, getNumberToBn, smartShortNumber } from "@app/util/markets";
+import { getBnToNumber, getNumberToBn, shortenNumber, smartShortNumber } from "@app/util/markets";
 import { TextInfo } from "../../common/Messages/TextInfo";
 import { SimpleAmountForm } from "../../common/SimpleAmountForm";
 import { useState } from "react";
@@ -11,17 +11,42 @@ import { parseEther } from "@ethersproject/units";
 import { BigNumber } from "ethers";
 import { Input } from "../../common/Input";
 import Container from "../../common/Container";
+import { useDBRSwapPrice } from "@app/hooks/useDBR";
 
 const { DOLA } = getNetworkConfigConstants();
+
+const defaultRefAmount = '10000';
+
+const ListLabelValues = ({ items }: { items: { label: string, value: string | any, color?: string }[]}) => {
+    return <VStack w='full' spacing="2" alignItems="flex-start">
+        {
+            items.map(item => {
+                return <HStack key={item.label} w='full' justify="space-between">
+                <Text fontSize="14px" color={item.color}>
+                    {item.label}:
+                </Text>
+                {typeof item.value === 'string' ? <Text color={item.color} fontSize="14px" fontWeight="bold">{item.value}</Text> : item.value}
+            </HStack>
+            })
+        }
+    </VStack>
+}
 
 export const DbrAuctionBuyer = () => {
     const { provider } = useWeb3React();
     const [amount, setAmount] = useState('');
     const [slippage, setSlippage] = useState('1');
+    const { price: dbrPrice } = useDBRSwapPrice(amount || defaultRefAmount);    
     const { data } = useEtherSWR([
-        [DBR_AUCTION_ADDRESS, 'getDbrOut', parseEther(amount || '0')]
+        [DBR_AUCTION_ADDRESS, 'getDbrOut', parseEther(amount || '0')],
+        [DBR_AUCTION_ADDRESS, 'getDbrOut', parseEther(defaultRefAmount)],
     ]);
-    const minDbrOut = data && data[0] ? getNumberToBn(getBnToNumber(data[0]) * (1 - parseFloat(slippage) / 100)) : BigNumber.from('0');
+    const refDbrOut = data && data[1] ? getBnToNumber(data[1]) : 0;
+    const estimatedDbrOut = data && data[0] ? getBnToNumber(data[0]) : 0;
+    const minDbrOut = data && data[0] ? getNumberToBn(estimatedDbrOut * (1 - parseFloat(slippage) / 100)) : BigNumber.from('0');
+    const minDbrOutNum = getBnToNumber(minDbrOut);
+    const dbrAuctionPrice = estimatedDbrOut > 0 ? estimatedDbrOut / parseFloat(amount) : refDbrOut / parseFloat(defaultRefAmount);
+    const auctionPriceColor = !dbrPrice || !dbrAuctionPrice ? undefined : dbrAuctionPrice >= dbrPrice ? 'success' : 'warning';
 
     const sell = async () => {
         return sellDolaForDbr(provider?.getSigner(), parseEther(amount), minDbrOut);
@@ -54,23 +79,16 @@ export const DbrAuctionBuyer = () => {
                     showBalance={true}
                 />
             </VStack>
+            <ListLabelValues items={[
+                { label: `Estimated amount to receive`, value: estimatedDbrOut > 0 ? `${shortenNumber(estimatedDbrOut, 2)} DBR` : '-' },
+                { label: `Price via auction`, color: auctionPriceColor, value: dbrAuctionPrice > 0 ? `~${shortenNumber(dbrAuctionPrice, 2)} DBR per DOLA` : '-' },
+                { label: `Price via Curve`, value: dbrPrice ? `~${shortenNumber(dbrPrice, 2)} DBR per DOLA` : '-' },
+            ]} />            
             <Divider />
-            <VStack spacing="2" alignItems="flex-start">
-                <HStack>
-                    <Text fontSize="14px">
-                        Max. slippage %:
-                    </Text>
-                    <Input py="0" maxH="30px" w='90px' value={slippage} onChange={(e) => setSlippage(e.target.value.replace(/[^0-9.]/, '').replace(/(\..*)\./g, '$1'))} />
-                </HStack>
-                <HStack>
-                    <Text fontSize="14px">
-                        Min. DBR to receive:
-                    </Text>
-                    <Text fontWeight="bold">
-                        {minDbrOut > 0 ? smartShortNumber(getBnToNumber(minDbrOut), 2, false, true) : '-'}
-                    </Text>
-                </HStack>
-            </VStack>
+            <ListLabelValues items={[
+                { label: `Max. slippage %`, value: <Input py="0" maxH="30px" w='90px' value={slippage} onChange={(e) => setSlippage(e.target.value.replace(/[^0-9.]/, '').replace(/(\..*)\./g, '$1'))} /> },
+                { label: `Min. DBR to receive`, value: minDbrOutNum > 0 ? smartShortNumber(minDbrOutNum, 2, false, true) : '-' },                
+            ]} />            
         </VStack>
     </Container>
 }
--- components/F2/DbrAuction/DbrAuctionInfos.tsx
+++ components/F2/DbrAuction/DbrAuctionInfos.tsx
@@ -5,14 +5,39 @@ import { DBR_AUCTION_ADDRESS } from "@app/util/dbr-auction"
 import { getBnToNumber } from "@app/util/markets"
 import { preciseCommify } from "@app/util/misc"
 import { ExternalLinkIcon } from "@chakra-ui/icons"
-import { HStack, Stack, Text, VStack } from "@chakra-ui/react"
+import { HStack, SkeletonText, Stack, Text, VStack } from "@chakra-ui/react"
 
-export const DbrAuctionInfos = () => {
-    const { data } = useEtherSWR(
+const TextLoader = () => <SkeletonText pt="2" skeletonHeight={2} noOfLines={1} height={'24px'} width={'90px'} />;
+
+const useDbrAuction = (): {
+    dolaReserve: number;
+    dbrReserve: number;
+    dbrRatePerYear: number;
+    maxDbrRatePerYear: number;
+    isLoading: boolean;
+    hasError: boolean;
+} => {
+    const { data: reserves, error } = useEtherSWR(
         [DBR_AUCTION_ADDRESS, 'getCurrentReserves']
     );
-    const dolaReserve = data ? getBnToNumber(data._dolaReserve) : 0;
-    const dbrReserve = data ? getBnToNumber(data._dbrReserve) : 0;
+    const { data: dbrRate, error: dbrRateError } = useEtherSWR(
+        [DBR_AUCTION_ADDRESS, 'dbrRatePerYear']
+    );
+    const { data: maxDbrRate, error: maxDbrRateError } = useEtherSWR(
+        [DBR_AUCTION_ADDRESS, 'maxDbrRatePerYear']
+    );
+    return {
+        dolaReserve: reserves ? getBnToNumber(reserves._dolaReserve) : 0,
+        dbrReserve: reserves ? getBnToNumber(reserves._dbrReserve) : 0,
+        dbrRatePerYear: reserves ? getBnToNumber(dbrRate) : 0,
+        maxDbrRatePerYear: reserves ? getBnToNumber(maxDbrRate) : 0,
+        isLoading: (!reserves && !error) || (!dbrRate && !dbrRateError) || (!maxDbrRate && !maxDbrRateError),
+        hasError: !!error || !!dbrRateError || !!maxDbrRateError,
+    }
+}
+
+export const DbrAuctionInfos = () => {
+    const { dolaReserve, dbrReserve, dbrRatePerYear, maxDbrRatePerYear, isLoading } = useDbrAuction();
     return <InfoMessage
         showIcon={false}
         alertProps={{ fontSize: '12px', mb: '8' }}
@@ -30,15 +55,26 @@ export const DbrAuctionInfos = () => {
                 <Link textDecoration="underline" href='https://docs.inverse.finance/inverse-finance/inverse-finance/product-guide/tokens/dbr' isExternal target="_blank">
                     Learn more about DBR <ExternalLinkIcon />
                 </Link>
-                <Text fontSize="14px" fontWeight="bold">Auction Reserves</Text>
-                <VStack w='full'>
+                <Text fontSize="14px" fontWeight="bold">Auction Current Reserves</Text>
+                <VStack w='full' spacing="0">
                     <HStack w='full'>
                         <Text>- DOLA reserves:</Text>
-                        <Text fontWeight="bold">{preciseCommify(dolaReserve, 0)}</Text>
+                        {isLoading ? <TextLoader /> : <Text fontWeight="bold">{preciseCommify(dolaReserve, 0)}</Text>}
                     </HStack>
                     <HStack w='full'>
-                        <Text>- DOLA reserves:</Text>
-                        <Text fontWeight="bold">{preciseCommify(dbrReserve, 0)}</Text>
+                        <Text>- DBR reserves:</Text>
+                        {isLoading ? <TextLoader /> : <Text fontWeight="bold">{preciseCommify(dbrReserve, 0)}</Text>}
+                    </HStack>
+                </VStack>
+                <Text fontSize="14px" fontWeight="bold">Auction Contract Parameters</Text>
+                <VStack w='full' spacing="0">
+                    <HStack w='full'>
+                        <Text>- DBR rate per year:</Text>
+                        {isLoading ? <TextLoader /> : <Text fontWeight="bold">{preciseCommify(dbrRatePerYear, 0)}</Text>}
+                    </HStack>
+                    <HStack w='full'>
+                        <Text>- Max. DBR rate per year:</Text>
+                        {isLoading ? <TextLoader /> : <Text fontWeight="bold">{preciseCommify(maxDbrRatePerYear, 0)}</Text>}
                     </HStack>
                 </VStack>
             </Stack>
