50306d389fdaab36ea407c15853adacf6fc51094
webmass <webmass@inverse.finance>
2023-11-30 07:30:03 +0100

    DbrAuction: add usd price

--- components/F2/DbrAuction/DbrAuctionBuyer.tsx
+++ components/F2/DbrAuction/DbrAuctionBuyer.tsx
@@ -11,7 +11,7 @@ import { parseEther } from "@ethersproject/units";
 import { BigNumber } from "ethers";
 import { Input } from "../../common/Input";
 import Container from "../../common/Container";
-import { useTriCryptoSwap } from "@app/hooks/useDBR";
+import { useDBRPrice, useTriCryptoSwap } from "@app/hooks/useDBR";
 
 const { DOLA } = getNetworkConfigConstants();
 
@@ -36,17 +36,18 @@ export const DbrAuctionBuyer = () => {
     const { provider } = useWeb3React();
     const [amount, setAmount] = useState('');
     const [slippage, setSlippage] = useState('1');
-    const { price: dbrPrice, isLoading: isCurvePriceLoading } = useTriCryptoSwap(parseFloat(amount || defaultRefAmount), 0, 1);
+    const { price: dbrSwapPrice, isLoading: isCurvePriceLoading } = useTriCryptoSwap(parseFloat(amount || defaultRefAmount), 0, 1);
     const { data } = useEtherSWR([
         [DBR_AUCTION_ADDRESS, 'getDbrOut', parseEther(amount || '0')],
         [DBR_AUCTION_ADDRESS, 'getDbrOut', parseEther(defaultRefAmount)],
     ]);
+    const { price: dbrPrice } = useDBRPrice();    
     const refDbrOut = data && data[1] ? getBnToNumber(data[1]) : 0;
     const estimatedDbrOut = data && data[0] ? getBnToNumber(data[0]) : 0;
     const minDbrOut = data && data[0] ? getNumberToBn(estimatedDbrOut * (1 - parseFloat(slippage) / 100)) : BigNumber.from('0');
     const minDbrOutNum = getBnToNumber(minDbrOut);
     const dbrAuctionPrice = estimatedDbrOut > 0 ? estimatedDbrOut / parseFloat(amount) : refDbrOut / parseFloat(defaultRefAmount);
-    const auctionPriceColor = !dbrPrice || !dbrAuctionPrice ? undefined : dbrAuctionPrice >= dbrPrice ? 'success' : 'warning';
+    const auctionPriceColor = !dbrSwapPrice || !dbrAuctionPrice ? undefined : dbrAuctionPrice >= dbrSwapPrice ? 'success' : 'warning';
     const isInvalidSlippage = !slippage || parseFloat(slippage) <= 0 || parseFloat(slippage) >= 20;
     const isDisabled = isInvalidSlippage || !amount || parseFloat(amount) <= 0;
 
@@ -95,14 +96,14 @@ export const DbrAuctionBuyer = () => {
             </VStack>
             <Divider />
             <ListLabelValues items={[
-                { label: `Estimated amount to receive`, value: estimatedDbrOut > 0 ? `${shortenNumber(estimatedDbrOut, 2)} DBR` : '-' },
+                { label: `Estimated amount to receive`, value: estimatedDbrOut > 0 ? `${shortenNumber(estimatedDbrOut, 2)} DBR (${shortenNumber(estimatedDbrOut * dbrPrice, 2, true)})` : '-' },
                 { label: `Price via auction`, color: auctionPriceColor, value: dbrAuctionPrice > 0 ? `~${shortenNumber(dbrAuctionPrice, 2)} DBR per DOLA` : '-' },
-                { label: `Price via Curve`, value: !isCurvePriceLoading && dbrPrice > 0 ? `~${shortenNumber(dbrPrice, 2)} DBR per DOLA` : '-' },
+                { label: `Price via Curve`, value: !isCurvePriceLoading && dbrSwapPrice > 0 ? `~${shortenNumber(dbrSwapPrice, 2)} DBR per DOLA` : '-' },
             ]} />
             <Divider />
             <ListLabelValues items={[
                 { label: `Max. slippage %`, value: auctionSlippageInput },
-                { label: `Min. DBR to receive`, value: minDbrOutNum > 0 ? smartShortNumber(minDbrOutNum, 2, false, true) : '-' },
+                { label: `Min. to receive`, value: minDbrOutNum > 0 ? `${smartShortNumber(minDbrOutNum, 2, false, true)} DBR (${shortenNumber(minDbrOutNum * dbrPrice, 2, true)})` : '-' },
             ]} />
         </VStack>
     </Container>
