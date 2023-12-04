4797040c0d0ded6fcfda9d4a695c89f0b12ff4b2
webmass <webmass@inverse.finance>
2023-12-04 15:23:26 +0100

    InverseAlerts: twitter card

--- pages/firm/index.tsx
+++ pages/firm/index.tsx
@@ -56,7 +56,17 @@ export const F2PAGE = ({
                 <meta name="og:description" content="FiRM is Inverse Finance's Fixed Rate Market, borrow DOLA with the DOLA Borrowing Right token DBR. Rethink the way you borrow!" />
                 <meta name="description" content="FiRM is Inverse Finance's Fixed Rate Market, borrow DOLA with the DOLA Borrowing Right token DBR. Rethink the way you borrow!" />
                 <meta name="og:image" content="" />
-                <meta name="twitter:image" content={isTwitterAlert ? 'https://inverse.finance/assets/social-previews/inverse-alert.jpg' : ''} />
+                {
+                    isTwitterAlert ?
+                        <>
+                            <meta property="twitter:card" content="summary_large_image" />
+                            <meta name="twitter:image" content="https://inverse.finance/assets/social-previews/inverse-alert.jpg" />
+                        </>
+                        : <>
+                            <meta name="twitter:image" content="" />
+                            <meta property="twitter:card" content="" />
+                        </>
+                }
                 <meta name="twitter:image:alt" content="FiRM" />
             </Head>
             <AppNav active="Borrow" activeSubmenu="FiRM" />
