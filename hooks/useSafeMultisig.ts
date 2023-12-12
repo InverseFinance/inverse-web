43eaed6169e8fd7b2d9bd5f2fcd7bd5823c3f6c2
webmass <webmass@inverse.finance>
2023-12-12 08:03:21 +0100

    Multisig: multisig but not connected via safe app

--- hooks/useSafeMultisig.ts
+++ hooks/useSafeMultisig.ts
@@ -1,10 +1,21 @@
+import { MULTISIG_ABI } from "@app/config/abis";
 import { useWeb3React } from "@web3-react/core";
 import { GnosisSafe } from "@web3-react/gnosis-safe";
+import { Contract } from "ethers";
+import useSWR from "swr";
 
 export const useMultisig = () => {
-    const { connector, account } = useWeb3React();
+    const { connector, account, provider } = useWeb3React();
+    const { data, error } = useSWR(`is-multisig-${account}`, async () => {
+        if(!account || !provider) return undefined;
+        const contract = new Contract(account, MULTISIG_ABI, provider.getSigner());
+        const owners = await contract.getOwners();
+        return Array.isArray(owners);
+    }, {
+        shouldRetryOnError: false,
+    });
     if(!account || !connector) return { isSafeMultisigConnector: false, isMultisig: false };
     const isSafeConnector =  !!connector && connector instanceof GnosisSafe;
-    if(isSafeConnector) return { isSafeMultisigConnector: true, isMultisig: true };
-    return { isSafeMultisigConnector: false, isMultisig: false }
+    if(isSafeConnector) return { isSafeMultisigConnector: true, isMultisig: true };    
+    return { isSafeMultisigConnector: false, isMultisig: !error && data }
 }
