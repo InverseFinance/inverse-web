d27e5978fc45ecb01e9ee684831b2a5aedcff3aa
webmass <webmass@inverse.finance>
2024-01-11 15:15:04 +0400

    DbrAuction: direct buy case

--- util/dbr-auction.ts
+++ util/dbr-auction.ts
@@ -5,7 +5,7 @@ import { BigNumber, Contract } from "ethers";
 export const DBR_AUCTION_ADDRESS = '0x933cBE81313d9dD523dF6dC9B899A7AF8Ba073e3';
 export const DBR_AUCTION_HELPER_ADDRESS = '0xC7D5E6FA4D5B4b4A82b14a256008DAfAF5232ADb';
 
-export const getDbrAuctionContract = (signerOrProvider: JsonRpcSigner) => {
+export const getDbrAuctionContract = (signerOrProvider: JsonRpcSigner, auctionAddress = DBR_AUCTION_ADDRESS) => {
     return new Contract(DBR_AUCTION_ADDRESS, DBR_AUCTION_ABI, signerOrProvider);
 }
 
@@ -13,10 +13,10 @@ export const getDbrAuctionHelperContract = (signerOrProvider: JsonRpcSigner, hel
     return new Contract(helperAddress, DBR_AUCTION_HELPER_ABI, signerOrProvider);
 }
 
-// export const sellDolaForDbr = async (signerOrProvider: JsonRpcSigner, dolaToSell: BigNumber, minDbrOut: BigNumber) => {
-//     const contract = getDbrAuctionContract(signerOrProvider);
-//     return contract.buyDBR(dolaToSell, minDbrOut);
-// }
+export const sellDolaForDbr = async (signerOrProvider: JsonRpcSigner, dolaToSell: BigNumber, minDbrOut: BigNumber,  auctionAddress = DBR_AUCTION_ADDRESS) => {
+    const contract = getDbrAuctionContract(signerOrProvider, auctionAddress);
+    return contract.buyDBR(dolaToSell, minDbrOut, await signerOrProvider.getAddress());
+}
 
 export const swapExactDolaForDbr = (signerOrProvider: JsonRpcSigner, dolaToSell: BigNumber, minDbrOut: BigNumber, helperAddress = DBR_AUCTION_HELPER_ADDRESS) => {
     const contract = getDbrAuctionHelperContract(signerOrProvider, helperAddress);
