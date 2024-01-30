9548c3a562425c3ee116a494c2b8be6d3c88723f
webmass <webmass@inverse.finance>
2024-01-30 18:04:50 +0400

    Dashboard: init page

---
+++ components/F2/UserDashboard.tsx
@@ -0,0 +1,109 @@
+import { SimpleGrid, Stack, StackProps, Text, VStack } from "@chakra-ui/react"
+import { smartShortNumber } from "@app/util/markets";
+import { useAccountDBR, useAccountF2Markets, useDBRMarkets, useDBRPrice } from '@app/hooks/useDBR';
+import { useAccount } from '@app/hooks/misc';
+import { SkeletonList } from "@app/components/common/Skeleton";
+import { preciseCommify } from "@app/util/misc";
+import { lightTheme } from "@app/variables/theme";
+import moment from "moment";
+import { PieChartRecharts } from "../Transparency/PieChartRecharts";
+import { useStakedInFirm } from "@app/hooks/useFirm";
+import { usePrices } from "@app/hooks/usePrices";
+import { BigTextLoader } from "../common/Loaders/BigTextLoader";
+
+const DashBoardCard = (props: StackProps) => {
+    return <Stack direction={'row'} borderRadius="5px" bgColor="white" p="8" alignItems="flex-start" boxShadow="0 4px 5px 5px #33333322" {...props} />
+}
+
+const NumberItem = ({ isLoading = false, value = 0, price = undefined, label = '', isUsd = false, precision = 0 }) => {
+    return <VStack alignItems="flex-end" w='full'>
+        {
+            isLoading ? <BigTextLoader /> : <Text fontWeight="extrabold" fontSize="32px" color={lightTheme.colors.mainTextColor}>
+                {!value ? '-' : preciseCommify(value, precision, isUsd)}{!!price || !value ? ` (${smartShortNumber(value * price, 2, true)})` : ''}
+            </Text>
+        }
+        <Text fontSize="20px" fontWeight="bold" color={lightTheme.colors.mainTextColorLight}>{label}</Text>
+    </VStack>
+}
+
+const NumberCard = ({ isLoading = false, value = 0, label = '', price = undefined, isUsd = false, precision = 0 }) => {
+    return <DashBoardCard>
+        <NumberItem isLoading={isLoading} price={price} value={value} label={label} isUsd={isUsd} precision={precision} />
+    </DashBoardCard>
+}
+
+const StringItem = ({ value = '', label = '', isLoading = false }) => {
+    return <VStack alignItems="flex-end" w='full'>
+        {
+            isLoading ? <BigTextLoader /> : <Text fontWeight="extrabold" fontSize="36px" color={lightTheme.colors.mainTextColor}>{value}</Text>
+        }
+        <Text fontSize="20px" fontWeight="bold" color={lightTheme.colors.mainTextColorLight}>{label}</Text>
+    </VStack>
+}
+
+const StringCard = ({ value = '', label = '', isLoading = false }) => {
+    return <DashBoardCard>
+        <StringItem isLoading={isLoading} value={value} label={label} />
+    </DashBoardCard>
+}
+
+const PieItem = ({ data, activeFill = lightTheme.colors.mainTextColor, fill = lightTheme.colors.mainTextColorLight, width = 600, height = 250, dataKey = 'value', nameKey = 'name', precision = 2, isUsd = false }) => {
+    return <PieChartRecharts
+        isUsd={isUsd}
+        precision={precision}
+        width={width}
+        height={height}
+        data={data}
+        dataKey={dataKey}
+        nameKey={nameKey}
+        cx="50%"
+        cy="50%"
+        outerRadius={50}
+        activeFill={activeFill}
+        fill={fill}
+    />
+}
+
+const NumberAndPieCard = ({ isLoading, fill, activeFill, data, value, label, width = 350, height = 250, dataKey = 'value', nameKey = 'name', precision = 2, isUsd = false }) => {
+    return <DashBoardCard alignItems="center" justify="space-around" px="16">
+        <PieItem fill={fill} activeFill={activeFill} data={data} width={width} height={height} dataKey={dataKey} nameKey={nameKey} precision={precision} isUsd={isUsd} />
+        <NumberItem isLoading={isLoading} value={value} label={label} precision={precision} isUsd={isUsd} />
+    </DashBoardCard>
+}
+
+export const UserDashboard = ({
+    account
+}: {
+    account: string
+}) => {
+    const { markets, isLoading } = useDBRMarkets();
+    const { prices } = usePrices();    
+    const { priceUsd: dbrPrice } = useDBRPrice();
+    const accountMarkets = useAccountF2Markets(markets, account);
+    const { stakedInFirm } = useStakedInFirm(account);
+    const { debt, dbrExpiryDate, signedBalance: dbrBalance } = useAccountDBR(account);
+
+    const totalTotalSuppliedUsd = accountMarkets.reduce((prev, curr) => prev + curr.deposits * curr.price, 0);
+    const marketsWithDeposits = accountMarkets.filter(m => m.depositsUsd > 1).sort((a, b) => b.depositsUsd - a.depositsUsd);
+    const marketsWithDebt = accountMarkets.filter(m => m.debt > 0).sort((a, b) => b.debt - a.debt);
+
+    return <VStack w='full'>
+        {
+            isLoading ?
+                <SkeletonList /> :
+                <>
+                    <SimpleGrid columns={{ base: 1, sm: 2 }} pb="4" spacing="8" w="100%" >
+                        <NumberAndPieCard isLoading={isLoading} value={totalTotalSuppliedUsd} label="Total supplied" precision={0} isUsd={true} data={marketsWithDeposits} dataKey="depositsUsd" />
+                        <NumberAndPieCard isLoading={isLoading} fill={lightTheme.colors.warning} activeFill={lightTheme.colors.error} value={debt} label="DOLA debt" precision={0} isUsd={false} data={marketsWithDebt} dataKey="debt" />
+                    </SimpleGrid>
+                    <SimpleGrid columns={{ base: 1, sm: 4 }} pb="4" spacing="8" w="100%" >
+                        <NumberCard isLoading={isLoading} price={prices?.['inverse-finance']?.usd} value={stakedInFirm} label="INV staked in FiRM" precision={0} />
+                        <NumberCard isLoading={isLoading} value={8000} label="DOLA staked" precision={0} />
+                        <NumberCard isLoading={isLoading} price={dbrPrice} value={dbrBalance} label="DBR balance" precision={0} />
+                        <StringCard isLoading={isLoading} value={debt > 0 ? moment(dbrExpiryDate).format('MMM Do YYYY') : '-'} label="DBR depletion date" />
+                    </SimpleGrid>
+                </>
+
+        }
+    </VStack>
+}
---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,40 @@
+import { Image, VStack, Text, HStack } from '@chakra-ui/react'
+import { ErrorBoundary } from '@app/components/common/ErrorBoundary'
+import Layout from '@app/components/common/Layout'
+import { AppNav } from '@app/components/common/Navbar'
+import Head from 'next/head'
+import { useAccount } from '@app/hooks/misc'
+import { UserDashboard } from '@app/components/F2/UserDashboard'
+
+const firmImages = {
+    'dark': 'firm-final-logo-white.png',
+    'light': 'firm-final-logo.png',
+}
+
+export const UserDashboardPage = () => {
+    const account = useAccount();
+    return (
+        <Layout>
+            <Head>
+                <title>Inverse Finance - Dashboard</title>
+                <meta name="og:description" content="FiRM is Inverse Finance's Fixed Rate Market, borrow DOLA with the DOLA Borrowing Right token DBR. Rethink the way you borrow!" />
+                <meta name="description" content="FiRM is Inverse Finance's Fixed Rate Market, borrow DOLA with the DOLA Borrowing Right token DBR. Rethink the way you borrow!" />
+                <meta name="og:image" content="https://inverse.finance/assets/social-previews/firm-page.png" />
+            </Head>
+            <AppNav active="Borrow" activeSubmenu="FiRM" />
+            <ErrorBoundary>
+                <VStack pt="4" spacing="4" w='full' maxW={{ base: '75rem', '2xl': '90rem' }}>
+                    <HStack alignItems="center" justify="space-between" w='full'>
+                        <Text fontWeight="extrabold" fontSize="40px">My Dashboard</Text>
+                        <Image w='200px' src={`/assets/firm/${firmImages[themeName]}`} />
+                    </HStack>
+                    <ErrorBoundary description="Failed to Markets">
+                        <UserDashboard account={account} />
+                    </ErrorBoundary>
+                </VStack>
+            </ErrorBoundary>
+        </Layout>
+    )
+}
+
+export default UserDashboardPage
