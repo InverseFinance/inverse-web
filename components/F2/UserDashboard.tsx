362f7de802f7d4030e6d750153d1f76704825262
webmass <webmass@inverse.finance>
2024-01-31 10:34:22 +0400

    Dashboard: call to actions fallback

--- components/F2/UserDashboard.tsx
+++ components/F2/UserDashboard.tsx
@@ -1,7 +1,6 @@
-import { SimpleGrid, Stack, StackProps, Text, VStack, HStack } from "@chakra-ui/react"
+import { SimpleGrid, Stack, StackProps, Text, VStack, HStack, Popover, PopoverTrigger, PopoverContent, PopoverBody } from "@chakra-ui/react"
 import { shortenNumber, smartShortNumber } from "@app/util/markets";
 import { useAccountDBR, useAccountF2Markets, useDBRMarkets, useDBRPrice } from '@app/hooks/useDBR';
-import { useAccount } from '@app/hooks/misc';
 import { SkeletonList } from "@app/components/common/Skeleton";
 import { preciseCommify } from "@app/util/misc";
 import { lightTheme } from "@app/variables/theme";
@@ -10,28 +9,47 @@ import { PieChartRecharts } from "../Transparency/PieChartRecharts";
 import { useStakedInFirm } from "@app/hooks/useFirm";
 import { usePrices } from "@app/hooks/usePrices";
 import { BigTextLoader } from "../common/Loaders/BigTextLoader";
+import { RSubmitButton } from "../common/Button/RSubmitButton";
+import Link from "../common/Link";
+
 
 const DashBoardCard = (props: StackProps) => {
-    return <Stack direction={'row'} borderRadius="5px" bgColor="white" p="8" alignItems="center" boxShadow="0 4px 5px 5px #33333322" {...props} />
+    if (props.href) {
+        return <Popover trigger="hover">
+            <PopoverTrigger>
+                <Stack direction={'row'} borderRadius="5px" bgColor="white" p="8" alignItems="center" boxShadow="0 4px 5px 5px #33333322" {...props} />
+            </PopoverTrigger>
+            <PopoverContent border="1px solid #ccc" _focus={{ outline: 'none' }} maxW="70px">
+                <PopoverBody>
+                    <Link href={props.href}>
+                        Go to
+                    </Link>
+                </PopoverBody>
+            </PopoverContent>
+        </Popover>
+    }
+    return <Stack position="relative" direction={'row'} borderRadius="5px" bgColor="white" p="8" alignItems="center" boxShadow="0 4px 5px 5px #33333322" {...props} />
 }
 
-const NumberItem = ({ footer = undefined, isLoading = false, value = 0, price = undefined, label = '', isUsd = false, precision = 0 }) => {
-    return <VStack position="relative" spacing="0" justify="center" alignItems="flex-end" w='full'>
+const NumberItem = ({ noDataFallback = '-', footer = undefined, isLoading = false, value = 0, price = undefined, label = '', isUsd = false, precision = 0 }) => {
+    return <VStack spacing="0" justify="center" alignItems="flex-end" w='full'>
         <VStack alignItems="flex-end" spacing="2">
             {
                 isLoading ? <BigTextLoader /> : <Text fontWeight="extrabold" fontSize={price ? '28px' : '36px'} color={lightTheme.colors.mainTextColor}>
-                    {!value ? '-' : value > 100000 ? smartShortNumber(value, 2, isUsd) : preciseCommify(value, precision, isUsd)}{!!price && !!value ? ` (${smartShortNumber(value * price, 2, true)})` : ''}
+                    {!value ? noDataFallback : value > 100000 ? smartShortNumber(value, 2, isUsd) : preciseCommify(value, precision, isUsd)}{!!price && !!value ? ` (${smartShortNumber(value * price, 2, true)})` : ''}
                 </Text>
             }
-            <Text fontSize="20px" fontWeight="bold" color={lightTheme.colors.mainTextColorLight}>{label}</Text>
+            {
+                (!!value || (!value && noDataFallback === '-')) && <Text fontSize="20px" fontWeight="bold" color={lightTheme.colors.mainTextColorLight}>{label}</Text>
+            }
         </VStack>
         {footer}
     </VStack>
 }
 
-const NumberCard = ({ footer = undefined, isLoading = false, value = 0, label = '', price = undefined, isUsd = false, precision = 0 }) => {
-    return <DashBoardCard>
-        <NumberItem isLoading={isLoading} price={price} value={value} label={label} isUsd={isUsd} precision={precision} footer={footer} />
+const NumberCard = ({ noDataFallback = undefined, href = '', footer = undefined, isLoading = false, value = 0, label = '', price = undefined, isUsd = false, precision = 0 }) => {
+    return <DashBoardCard href={href}>
+        <NumberItem noDataFallback={noDataFallback} isLoading={isLoading} price={price} value={value} label={label} isUsd={isUsd} precision={precision} footer={footer} />
     </DashBoardCard>
 }
 
@@ -67,12 +85,48 @@ const PieItem = ({ data, activeFill = lightTheme.colors.mainTextColor, fill = li
     />
 }
 
-const NumberAndPieCard = ({ isLoading, fill, activeFill, data, value, label, width = 350, height = 250, dataKey = 'value', nameKey = 'name', precision = 2, isUsd = false }) => {
+const NumberAndPieCard = ({ isLoading, noDataFallback = undefined, fill, activeFill, data, value, label, width = 350, height = 250, dataKey = 'value', nameKey = 'name', precision = 2, isUsd = false }) => {
     return <DashBoardCard alignItems="center" justify="space-around" px="16">
-        <PieItem fill={fill} activeFill={activeFill} data={data} width={width} height={height} dataKey={dataKey} nameKey={nameKey} precision={precision} isUsd={isUsd} />
+        {
+            !isLoading && !data?.length ? noDataFallback : <PieItem fill={fill} activeFill={activeFill} data={data} width={width} height={height} dataKey={dataKey} nameKey={nameKey} precision={precision} isUsd={isUsd} />
+        }
         <NumberItem isLoading={isLoading} value={value} label={label} precision={precision} isUsd={isUsd} />
     </DashBoardCard>
 }
+
+const CardFooter = ({ labelLeft = '', labelRight = '' }) => {
+    return <HStack position="absolute" left="0" right="0" px="8" bottom="8px" w='full' justify="space-between">
+        <Text color={lightTheme.colors.mainTextColorLight} fontSize="14px">
+            {labelLeft}
+        </Text>
+        <Text color={lightTheme.colors.mainTextColorLight} fontSize="14px">
+            {labelRight}
+        </Text>
+    </HStack>
+}
+
+const CallToAction = ({ href = '', ...props }) => {
+    return <RSubmitButton bgColor="accentTextColor" px="8" h="60px" fontSize="28px" fontWeight="bold" {...props} />
+}
+
+const SmallCallToAction = ({ href = '', ...props }) => {
+    return <RSubmitButton fontSize="18px" bgColor="accentTextColor" {...props} />
+}
+
+const BigLinkBtn = ({ href = '', ...props }) => {
+    return <Link href={href} isExternal={true}>
+        <CallToAction {...props} />
+    </Link>
+}
+const SmallLinkBtn = ({ href = '', ...props }) => {
+    return <Link href={href} isExternal={true}>
+        <SmallCallToAction {...props} />
+    </Link>
+}
+
+const BorrowDola = <BigLinkBtn href="/firm">Borrow DOLA</BigLinkBtn>;
+const SupplyAssets = <BigLinkBtn href="/firm">Supply Assets</BigLinkBtn>;
+const StakeINV = <BigLinkBtn href="/firm">Stake INV</BigLinkBtn>;
 
 export const UserDashboard = ({
     account
@@ -97,20 +151,28 @@ export const UserDashboard = ({
                 <SkeletonList /> :
                 <>
                     <SimpleGrid columns={{ base: 1, sm: 2 }} pb="4" spacing="8" w="100%" >
-                        <NumberAndPieCard isLoading={isLoading} value={totalTotalSuppliedUsd} label="My collaterals" precision={0} isUsd={true} data={marketsWithDeposits} dataKey="depositsUsd" />
-                        <NumberAndPieCard isLoading={isLoading} fill={lightTheme.colors.warning} activeFill={lightTheme.colors.error} value={debt} label="My DOLA debt" precision={0} isUsd={false} data={marketsWithDebt} dataKey="debt" />
+                        <NumberAndPieCard noDataFallback={SupplyAssets} isLoading={isLoading} value={totalTotalSuppliedUsd} label="My collaterals" precision={0} isUsd={true} data={marketsWithDeposits} dataKey="depositsUsd" />
+                        <NumberAndPieCard noDataFallback={BorrowDola} isLoading={isLoading} fill={lightTheme.colors.warning} activeFill={lightTheme.colors.error} value={debt} label="My DOLA debt" precision={0} isUsd={false} data={marketsWithDebt} dataKey="debt" />
                     </SimpleGrid>
                     <SimpleGrid columns={{ base: 1, sm: 4 }} pb="4" spacing="8" w="100%" >
-                        <NumberCard footer={<HStack position="absolute" bottom="-25px" w='full' justify="space-between">
-                            <Text color={lightTheme.colors.mainTextColorLight} fontSize="14px">
-                                INV APR: <b>{shortenNumber(invMarket?.supplyApy, 2)}%</b>
-                            </Text>
-                            <Text color={lightTheme.colors.mainTextColorLight} fontSize="14px">
-                                DBR APR: <b>{shortenNumber(invMarket?.dbrApr, 2)}%</b>
-                            </Text>
-                        </HStack>} isLoading={isLoading} price={prices?.['inverse-finance']?.usd} value={stakedInFirm} label="INV staked in FiRM" precision={0} />
-                        <NumberCard isLoading={isLoading} value={8000} label="DOLA staked" precision={0} />
-                        <NumberCard isLoading={isLoading} price={dbrPrice} value={dbrBalance} label="DBR balance" precision={0} />
+                        <NumberCard footer={
+                            <CardFooter
+                                labelLeft={<>INV APR: <b>{shortenNumber(invMarket?.supplyApy, 2)}%</b></>}
+                                labelRight={<>DBR APR: <b>{shortenNumber(invMarket?.dbrApr, 2)}%</b></>}
+                            />
+                        } noDataFallback={StakeINV} isLoading={isLoading} price={prices?.['inverse-finance']?.usd} value={stakedInFirm} label="INV staked in FiRM" precision={0} />
+                        <NumberCard footer={
+                            <CardFooter
+                                labelRight={<>sDOLA APY: <b>{shortenNumber(5, 2)}%</b></>}
+                            />
+                        } isLoading={isLoading} value={8000} label="DOLA staked" precision={0} />
+                        <NumberCard
+                            footer={
+                                <CardFooter
+                                    labelRight={<>Price: <b>{shortenNumber(dbrPrice, 4, true)}</b></>}
+                                />
+                            }
+                            isLoading={isLoading} price={dbrPrice} value={dbrBalance} label="DBR balance" precision={0} />
                         <StringCard isLoading={isLoading} value={debt > 0 ? moment(dbrExpiryDate).format('MMM Do YYYY') : '-'} label="DBR depletion date" />
                     </SimpleGrid>
                 </>
