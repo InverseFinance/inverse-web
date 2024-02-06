1d9be0cf9e843bf66af3354f4ee3e5cdd9de92ab
webmass <webmass@inverse.finance>
2024-02-06 11:12:02 +0400

    Firm: dbr histo chart refacto

--- components/F2/Infos/DbrHistoBalanceChart.tsx
+++ components/F2/Infos/DbrHistoBalanceChart.tsx
@@ -1,18 +1,18 @@
 import { DefaultCharts } from "@app/components/Transparency/DefaultCharts";
 import { SkeletonBlob } from "@app/components/common/Skeleton";
 import { useDBRBalanceHisto } from "@app/hooks/useDBR";
+import { preciseCommify } from "@app/util/misc";
 import { VStack, useMediaQuery, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 const maxChartWidth = 600;
 
 export const DbrHistoBalanceChart = ({
-    account,
+    account
 }: {
     account: string
 }) => {
-    const { evolution, isLoading } = useDBRBalanceHisto(account);
-
+    const { evolution, currentBalance, isLoading } = useDBRBalanceHisto(account);
     const [autoChartWidth, setAutoChartWidth] = useState<number>(maxChartWidth);
     const [isLargerThan] = useMediaQuery(`(min-width: ${maxChartWidth}px)`);
 
@@ -35,7 +35,7 @@ export const DbrHistoBalanceChart = ({
                 chartData={evolution}
                 isDollars={false}
                 smoothLineByDefault={true}
-                areaProps={{ title: <>&nbsp;</>, forceStaticRangeBtns: true, id: 'dbr-balance-histo-chart', showRangeBtns: true, yLabel: 'Historical DBR balance', useRecharts: true, simplifyData: true, showMaxY: false, showTooltips: true, autoMinY: true, mainColor: 'info', allowZoom: true }}
+                areaProps={{ title: typeof currentBalance === 'number' ? `Current DBR balance: ${preciseCommify(currentBalance, 2)}` : undefined, forceStaticRangeBtns: true, id: 'dbr-balance-histo-chart', showRangeBtns: true, yLabel: 'Historical DBR balance', useRecharts: true, simplifyData: true, showMaxY: false, showTooltips: true, autoMinY: true, mainColor: 'info', allowZoom: true }}
             />
         }
     </VStack>
