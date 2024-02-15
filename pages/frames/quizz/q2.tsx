80b81b94e4d93c31f39caa038ec1875bccc17bec
webmass <webmass@inverse.finance>
2024-02-15 19:02:34 +0400

    Frames: fix path

--- pages/frames/quizz/q2.tsx
+++ pages/frames/quizz/q2.tsx
@@ -7,8 +7,8 @@ export default function Page() {
         <title>Question 2: sDOLA derives its yield from</title>
         <meta property="og:title" content="Question 2: sDOLA derives its yield from" />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://inverse.finance/assets/sDOLA.x512.png" />
-        <meta property="og:image" content="https://inverse.finance/assets/sDOLA.x512.png" />
+        <meta property="fc:frame:image" content="https://inverse.finance/assets/sDOLAx512.png" />
+        <meta property="og:image" content="https://inverse.finance/assets/sDOLAx512.png" />
         <meta property="fc:frame:button:1" content="Loans to Janet Yellen" />
         <meta property="fc:frame:button:2" content="FiRM fixed rate lending market revenues" />        
         <meta property="fc:frame:button:3" content="Loans to futures traders" />
