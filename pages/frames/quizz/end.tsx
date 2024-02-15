90e809fbd1c0581534a16a81c090c41020d2fa60
webmass <webmass@inverse.finance>
2024-02-15 16:56:07 +0400

    Frames: quizz test

--- pages/frames/quizz/end.tsx
+++ pages/frames/quizz/end.tsx
@@ -7,8 +7,8 @@ export default function Page() {
         <title>Thank you for completing the quizz!</title>
         <meta property="og:title" content="Thank you for completing the quizz!" />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="http://localhost:3000/assets/sDOLA.x512.png" />
-        <meta property="og:image" content="http://localhost:3000/assets/sDOLA.x512.png" />
+        <meta property="fc:frame:image" content="https://inverse.finance/assets/sDOLA.x512.png" />
+        <meta property="og:image" content="https://inverse.finance/assets/sDOLA.x512.png" />
       </Head>   
     </>
   );
