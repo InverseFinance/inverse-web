0373bc962d9717e85f14fd0277afd4ffd34e6cad
webmass <webmass@inverse.finance>
2024-02-15 18:42:03 +0400

    Frames: testing

--- pages/api/frames/quizz/q2.ts
+++ pages/api/frames/quizz/q2.ts
@@ -6,14 +6,14 @@ const options = {
   3: 'Incorrect. sDOLA derives its yield from FiRM fixed rate lending market revenues!',
 };
 
-export default async function getResponse(req, res) {
+export default async function handler(req, res) {
   const data = req.body;
   const buttonId = data?.untrustedData?.buttonIndex;
   const fid = data?.untrustedData?.fid;
   const url = data?.untrustedData?.url;
 
-  if(!fid || !options[buttonId]) {
-    res.status(200).send(getErrorFrame(url));
+  if (!fid || !options[buttonId]) {
+    return res.status(200).send(getErrorFrame(url));
   }
 
   return res.status(200).send(`<!DOCTYPE html><html><head>
@@ -29,8 +29,4 @@ export default async function getResponse(req, res) {
     <p>Result: ${options[buttonId] || 'Invalid answer'}</p>
   </body>
   </html>`);
-}
-
-export async function POST(req, res) {
-  return getResponse(req, res);
 }
