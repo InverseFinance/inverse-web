fcae31832a90a4bb2720cd5e6d42ac5740c8a8b3
webmass <webmass@inverse.finance>
2024-02-19 15:22:28 +0400

    Frames: newsletter retry frame

--- pages/api/frames/newsletter/submit.ts
+++ pages/api/frames/newsletter/submit.ts
@@ -1,4 +1,4 @@
-import { FRAME_BASE_URL, getErrorFrame, getSuccessFrame } from "@app/util/frames";
+import { FRAME_BASE_URL, getRetryFrame, getSuccessFrame } from "@app/util/frames";
 import { getFarcasterUserAddresses, isFollowingInverseFarcaster, setFrameCheckAction, validateFrameMessage } from "@app/util/frames-server";
 import { getTimestampFromUTCDate } from "@app/util/misc";
 import { getNewsletterRegisterFrame } from "./register-frame";
@@ -20,17 +20,18 @@ export default async function handler(req, res) {
     const buttonId = data?.untrustedData?.buttonIndex;
     const unverifiedEmail = data?.untrustedData?.inputText;
     const unverifiedFid = data?.untrustedData?.fid;
-    const url = data?.untrustedData?.url;
+    // const url = data?.untrustedData?.url;
+    const backUrl = FRAME_BASE_URL+'/api/frames/newsletter/submit';
 
     const now = Date.now();
 
     const isFollowingInverse = await isFollowingInverseFarcaster(unverifiedFid);
     if (!isFollowingInverse) {
-        return res.status(200).send(getErrorFrame(url, '/api/frames/follow-inverse-first?v=1'));
+        return res.status(200).send(getRetryFrame(backUrl, '/api/frames/follow-inverse-first?v=1'));
     }
 
     if (now > contestEndTimestamp) {
-        return res.status(200).send(getErrorFrame(url, 'api/frames/newsletter/image-contest-ended'));
+        return res.status(200).send(getRetryFrame(backUrl, 'api/frames/newsletter/image-contest-ended'));
     }
     else if (buttonId === 1 && !unverifiedEmail) {
         return res.status(200).send(getNewsletterRegisterFrame());
@@ -38,18 +39,18 @@ export default async function handler(req, res) {
     else {
         const isLocal = FRAME_BASE_URL.includes('localhost');
         if (!isLocal && !data?.trustedData?.messageBytes) {
-            return res.status(200).send(getErrorFrame(url));
+            return res.status(200).send(getRetryFrame(backUrl));
         }
 
         const verifiedMessage = !isLocal ? await validateFrameMessage(data?.trustedData?.messageBytes) : {};
         if (!isLocal && (!verifiedMessage.valid || !verifiedMessage.message.data.fid)) {
-            return res.status(200).send(getErrorFrame(url));
+            return res.status(200).send(getRetryFrame(backUrl));
         }
         const fid = isLocal ? unverifiedFid : verifiedMessage.message.data.fid;
         const email = isLocal ? unverifiedEmail : Buffer.from(verifiedMessage.message.data.frameActionBody.inputText, 'base64');
         
         if (!email || !isValidEmail(email)) {
-            return res.status(200).send(getErrorFrame(url, 'api/frames/newsletter/image-invalid-email?'));
+            return res.status(200).send(getRetryFrame(backUrl, 'api/frames/newsletter/image-invalid-email?'));
         }
         const fidAddresses = isLocal ? [BURN_ADDRESS] : await getFarcasterUserAddresses(fid);
 
--- util/frames.ts
+++ util/frames.ts
@@ -1,6 +1,6 @@
 export const FRAME_BASE_URL = process.env.VERCEL_ENV === 'production' ? 'https://inverse.finance' : !process.env.VERCEL_URL ? 'http://localhost:3000' : `https://${process.env.VERCEL_URL}`;
 
-export const getErrorFrame = (url: string, imageUrl = 'api/frFames/invalid-image') => {
+export const getErrorFrame = (url: string, imageUrl = 'api/frames/invalid-image') => {
     return `<!DOCTYPE html><html><head>
     <title>Invalid request</title>
     <meta property="og:title" content="Invalid request:" />
@@ -14,6 +14,22 @@ export const getErrorFrame = (url: string, imageUrl = 'api/frFames/invalid-image
     <p>Invalid request</p>
   </body>
   </html>`
+}
+
+export const getRetryFrame = (url: string, imageUrl = 'api/frames/invalid-image') => {
+  return `<!DOCTYPE html><html><head>
+  <title>Invalid request</title>
+  <meta property="og:title" content="Invalid request:" />
+  <meta property="fc:frame" content="vNext" />
+  <meta property="fc:frame:image" content="${FRAME_BASE_URL}/${imageUrl}" />
+  <meta property="og:image" content="${FRAME_BASE_URL}/${imageUrl}" />    
+  <meta property="fc:frame:button:1" content="Retry" />
+  <meta property="fc:frame:post_url" content="${url}" />
+</head>
+<body>
+  <p>Invalid request</p>
+</body>
+</html>`
 }
 
 export const getSuccessFrame = (imageUrl = 'api/frames/invalid-image') => {
