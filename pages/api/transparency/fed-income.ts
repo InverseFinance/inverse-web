937d38d978ec0093cb50f1acb066d44028f4b964
webmass <webmass@inverse.finance>
2024-03-29 15:15:39 +0400

    bump

--- pages/api/transparency/fed-income.ts
+++ pages/api/transparency/fed-income.ts
@@ -164,7 +164,7 @@ export default async function handler(req, res) {
     const { FEDS, DOLA, TREASURY } = getNetworkConfigConstants(NetworkIds.mainnet);
 
     const archiveCacheKey = `revenues-v1.0.25`;
-    const cacheKey = `revenues-v1.0.27`;
+    const cacheKey = `revenues-v1.0.271`;
 
     try {
 
