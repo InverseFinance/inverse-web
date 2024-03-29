66d728082e6185e8ccf2f76411e3a47a3a5b5b30
webmass <webmass@inverse.finance>
2024-03-29 14:52:45 +0400

    Covalent: combine recent and paginated to get ~100tx

--- util/covalent.ts
+++ util/covalent.ts
@@ -22,12 +22,24 @@ export const getTxsOf_deprecated = async (ad: string, pageSize = 1000, pageNumbe
     return res.json();
 }
 
+// cannot get that directly, need to call the recent api+the page before :/
 export const getLast100TxsOf = async (ad: string, chainId = process.env.NEXT_PUBLIC_CHAIN_ID) => {
     const _chainId = chainId === '31337' ? '1' : chainId
-    const path = `/${_chainId}/address/${ad}/transactions_v3/page/0/?quote-currency=USD&format=JSON&key=${process.env.COVALENT_API_KEY}`
-    const res = await fetch(`${baseUrl}${path}`);
-
-    return res.json();
+    const recentPath = `/${_chainId}/address/${ad}/transactions_v3/?quote-currency=USD&format=JSON&key=${process.env.COVALENT_API_KEY}`    
+    const res = await fetch(`${baseUrl}${recentPath}`);
+    const result = await res.json();
+    if(result?.data?.items){
+        const currentPage = result.data.current_page;
+        if(currentPage > 0 && result?.data?.items?.length < 100){
+            const path = `/${_chainId}/address/${ad}/transactions_v3/page/${currentPage - 1}/?quote-currency=USD&format=JSON&key=${process.env.COVALENT_API_KEY}`
+            const res = await fetch(`${baseUrl}${path}`);
+            const previousPage = await res.json();
+            if(previousPage?.data?.items){
+                result.data.items = [...result.data.items, ...previousPage.data.items];
+            }
+        }
+    }
+    return result;
 }
 
 export const getTx = async (txHash: string, chainId = process.env.NEXT_PUBLIC_CHAIN_ID) => {
