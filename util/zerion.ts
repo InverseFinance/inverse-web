42dc3d86d3d08d43a3ecfa32545b3dc626c7d058
webmass <webmass@inverse.finance>
2024-04-02 18:43:11 +0400

    Zerion: aggreg non-wallet positions as usd

--- util/zerion.ts
+++ util/zerion.ts
@@ -6,61 +6,93 @@ import { isAddress } from "ethers/lib/utils";
 const WALLET_ZERION_URL = 'https://api.zerion.io/v1/wallets';
 
 export const fetchZerionWithRetry = async (
-  wallet = '',
-  chainCodeName = 'ethereum',
-  maxRetries = 1,
-  currentRetry = 0,
+    wallet = '',
+    chainCodeName = 'ethereum',
+    maxRetries = 1,
+    currentRetry = 0,
 ): Promise<Response | undefined> => {
-  let response;
-  const url = `${WALLET_ZERION_URL}/${wallet}/positions/?filter[positions]=no_filter&currency=usd&filter[chain_ids]=${chainCodeName}&filter[trash]=only_non_trash&sort=value`;
-  try {
-    const bearer = btoa(`${process.env.ZERION_KEY}:`);
-    response = await fetch(url, {
-      headers: {
-        'Authorization': `Basic ${bearer}`,        
-        'accept': 'application/json',
-      },
-    });
-  } catch (e) {
-    console.log(e)
-  }
+    let response;
+    const url = `${WALLET_ZERION_URL}/${wallet}/positions/?filter[positions]=no_filter&currency=usd&filter[chain_ids]=${chainCodeName}&filter[trash]=only_non_trash&sort=value`;
+    try {
+        const bearer = btoa(`${process.env.ZERION_KEY}:`);
+        response = await fetch(url, {
+            headers: {
+                'Authorization': `Basic ${bearer}`,
+                'accept': 'application/json',
+            },
+        });
+    } catch (e) {
+        console.log(e)
+    }
 
-  if (response?.status !== 200 && currentRetry < maxRetries) {
-    await new Promise((r) => setTimeout(() => r(true), 1050));
-    return await fetchZerionWithRetry(wallet, chainCodeName, maxRetries, currentRetry + 1);
-  };
-  return formatZerionWalletResponse(response);
+    if (response?.status !== 200 && currentRetry < maxRetries) {
+        await new Promise((r) => setTimeout(() => r(true), 1050));
+        return await fetchZerionWithRetry(wallet, chainCodeName, maxRetries, currentRetry + 1);
+    };
+    return formatZerionWalletResponse(response);
 }
 
 export const formatZerionWalletResponse = async (response) => {
-    if(response.status !== 200) {
+    if (response.status !== 200) {
         console.log(response)
         return [];
     }
-    const responseData = await response.json();    
-    const data = responseData.data.map((position) => {
-        const splitData = position.id.split('-');   
-        let address = splitData[0];
-        const chainCodeName = splitData[1].toLowerCase();
-        if(!isAddress(address)) {
-            address = position.attributes.fungible_info?.implementations.find(imp => imp.chain_id === chainCodeName)?.address;
-        }        
-        const chainTokens = CHAIN_TOKENS[NETWORKS.find(net => net.codename === chainCodeName)?.id] || {};
-        const listedToken = getToken(chainTokens, address);
-        const token = listedToken?.symbol ? listedToken : {
-            address,
-            decimals: position.attributes.quantity?.decimals||18,
-            name: position.attributes.name,
-            symbol: position.attributes.name,
-            image: TOKEN_IMAGES.DOLA,
-            protocolImage: PROTOCOL_IMAGES[(position.attributes.protocol||'').toUpperCase()],
-        }
+    const responseData = await response.json();
+
+    // filter out Frontier
+    const cleanData = responseData.filter(p => p.attributes.name !== 'Inverse Lending');
+
+    const uniqueNonWalletsKeys = [
+        ...new Set(
+            cleanData
+                .filter((position) => position.attributes?.position_type !== 'wallet')
+                .map((position) => `${position.attributes.protocol}-${position.attributes.name}`)
+        )
+    ]
+
+    const nonWalletPositions = uniqueNonWalletsKeys.map(key => {
+        const item = cleanData.find((position) => `${position.attributes.protocol}-${position.attributes.name}` === key);
+        const totalValue = cleanData.filter(r => `${r.attributes.protocol}-${r.attributes.name}` === key).reduce((prev, curr) => prev + curr.attributes.value, 0);
         return {
-            token,
-            balance: position.attributes?.quantity?.float||0,
-            price: position.attributes?.price,
+            balance: totalValue,
+            price: 1,
+            onlyUsdValue: true,
             allowance: 0,
-        };
-    });
-    return data;
+            token: {
+                decimals: 18,
+                name: item.attributes.name,
+                symbol: item.attributes.name,
+                image: TOKEN_IMAGES.DOLA,
+                protocolImage: PROTOCOL_IMAGES[(item.attributes.protocol || '').toUpperCase()],
+            }
+        }
+    })
+
+    const walletPositions = cleanData
+        .filter(r => !uniqueNonWalletsKeys.includes(`${r.attributes.protocol}-${r.attributes.name}`))
+        .map((position) => {
+            const splitData = position.id.split('-');
+            let address = splitData[0];
+            const chainCodeName = splitData[1].toLowerCase();
+            if (!isAddress(address)) {
+                address = position.attributes.fungible_info?.implementations.find(imp => imp.chain_id === chainCodeName)?.address;
+            }
+            const chainTokens = CHAIN_TOKENS[NETWORKS.find(net => net.codename === chainCodeName)?.id] || {};
+            const listedToken = getToken(chainTokens, address);
+            const token = listedToken?.symbol ? listedToken : {         
+                address,
+                decimals: position.attributes.quantity?.decimals || 18,
+                name: position.attributes.fungible_info.name || position.attributes.name,
+                symbol: position.attributes.fungible_info.symbol || position.attributes.name,
+                image: position.attributes.fungible_info?.icon?.url || TOKEN_IMAGES.DOLA,
+                protocolImage: PROTOCOL_IMAGES[(position.attributes.protocol || '').toUpperCase()],
+            }                        
+            return {
+                token,                
+                balance: position.attributes?.quantity?.float || 0,
+                price: position.attributes?.price,
+                allowance: 0,
+            };
+        });    
+    return [...walletPositions, ...nonWalletPositions];
 }
