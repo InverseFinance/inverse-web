786e830e424cae48f8b8f25d8aed49da8f488f7a
webmass <webmass@inverse.finance>
2024-07-11 14:19:57 +0400

    sDOLA: savings opportunity ad swap link & visual update

--- components/sDola/SavingsOpportunities.tsx
+++ components/sDola/SavingsOpportunities.tsx
@@ -3,12 +3,15 @@ import { useDBRPrice } from "@app/hooks/useDBR";
 import { useToken } from "@app/hooks/useToken";
 import { useStakedDola } from "@app/util/dola-staking";
 import { getToken, TOKENS } from "@app/variables/tokens";
-import { HStack, Text, VStack } from "@chakra-ui/react"
+import { HStack, Stack, Text, VStack } from "@chakra-ui/react"
 import { DashBoardCard } from "../F2/UserDashboard";
 import { InfoMessage } from "../common/Messages";
 import { preciseCommify } from "@app/util/misc";
 import { shortenNumber } from "@app/util/markets";
 import { UnderlyingItemBlock } from "../common/Assets/UnderlyingItemBlock";
+import { ChevronDownIcon, ChevronRightIcon, ExternalLinkIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+import Link from "../common/Link";
 
 const USDC = getToken(TOKENS, 'USDC');
 const DAI = getToken(TOKENS, 'DAI');
@@ -20,24 +23,25 @@ const DOLA = getToken(TOKENS, 'DOLA');
 const EarningsProjections = ({ apy, total }: { apy: number, total: number }) => {
     const oneYearEarnings = apy / 100 * total;
     const thirtyDayEarnings = oneYearEarnings / 365 * 30;
-    return <HStack w='full' justify="space-between">
+    return <Stack direction={{ base: 'column', xl: 'row' }} w='full' justify="space-between">
         <VStack alignItems="flex-start">
-            <Text>30-day projection</Text>
+            <Text color="mainTextColorLight">30-day projection</Text>
             <Text fontWeight="extrabold" fontSize="30px">+{shortenNumber(thirtyDayEarnings, 2, true)}</Text>
         </VStack>
         <VStack alignItems="flex-start">
-            <Text>1-year projection</Text>
+            <Text color="mainTextColorLight">1-year projection</Text>
             <Text fontWeight="extrabold" fontSize="30px">+{shortenNumber(oneYearEarnings, 2, true)}</Text>
         </VStack>
         <VStack alignItems="flex-start">
-            <Text>Resulting APY</Text>
+            <Text color="mainTextColorLight">Resulting APY</Text>
             <Text fontWeight="extrabold" fontSize="30px">{shortenNumber(apy, 2)}%</Text>
         </VStack>
-    </HStack>
+    </Stack>
 }
 
 export const SavingsOpportunities = () => {
     const account = useAccount();
+    const [showTokens, setShowTokens] = useState(true);
 
     const { balance: dolaBalance } = useToken(DOLA.address, account);
     const { balance: usdcBalance } = useToken(USDC.address, account);
@@ -61,7 +65,7 @@ export const SavingsOpportunities = () => {
     const { apy: currentApy } = useStakedDola(dbrDolaPrice);
     const { apy: apyAfterDeposit } = useStakedDola(dbrDolaPrice, totalStables);
 
-    if(totalStables < 10) {
+    if (totalStables < 10) {
         return null;
     }
 
@@ -76,23 +80,34 @@ export const SavingsOpportunities = () => {
                 } />
                 <EarningsProjections apy={apyAfterDeposit} total={totalStables} />
                 <VStack alignItems="flex-start">
-                    <Text fontWeight="bold" textDecoration="underline">
-                        Stables that you can put to work:
+                    <Text fontWeight="bold" textDecoration="underline" cursor="pointer" onClick={() => setShowTokens(!showTokens)}>
+                        Stables that you can put to work: {showTokens ? <ChevronDownIcon /> : <ChevronRightIcon />}
                     </Text>
-                    <HStack w='200px' justify="space-between">
-                        <Text>Token</Text>
-                        <Text>Balance</Text>
-                    </HStack>
                     {
-                        balances
-                            .filter(b => b.balance > 1)
-                            .map(b => {
-                                return <HStack key={b.token.address} w='200px' justify="space-between">
-                                    <UnderlyingItemBlock {...b.token} label={b.token.symbol} />
-                                    <Text>{shortenNumber(b.balance, 2)}</Text>
-                                </HStack>
-                            })
+                        showTokens && <>
+                            <HStack w='300px' justify="space-between">
+                                <Text w="33%" color="mainTextColorLight">Token</Text>
+                                <Text w="33%" color="mainTextColorLight">Balance</Text>
+                                <Text w="33%">&nbsp;</Text>
+                            </HStack>
+                            {
+                                balances
+                                    .filter(b => b.balance > 1)
+                                    .map(b => {
+                                        return <HStack key={b.token.address} w='300px' justify="space-between">
+                                            <UnderlyingItemBlock {...b.token} label={b.token.symbol} w="33%"/>
+                                            <Text w="33%">{shortenNumber(b.balance, 2)}</Text>
+                                            {
+                                                b.token.symbol !== 'DOLA' ? <Link w="33%" href={`https://swap.defillama.com/?chain=ethereum&from=${b.token.address}&tab=swap&to=${DOLA.address}`} textDecoration="underline">
+                                                    Swap it <ExternalLinkIcon />
+                                                </Link> : <Text w='33%'>&nbsp;</Text>
+                                            }
+                                        </HStack>
+                                    })
+                            }
+                        </>
                     }
+
                 </VStack>
             </VStack>
         </VStack>
