5f0007ebb8b8c289f83faf902574c39e6c81b2df
webmass <webmass@inverse.finance>
2024-07-15 13:00:04 +0400

    Survery: add snippet

---
+++ public/qualaroo.js
@@ -0,0 +1,8 @@
+var _kiq = _kiq || [];
+(function () {
+    console.log('yoooo')
+    setTimeout(function () {
+        var d = document, f = d.getElementsByTagName('script')[0], s = d.createElement('script'); s.type = 'text/javascript';
+        s.async = true; s.src = 'https://cl.qualaroo.com/ki.js/85739/jRS.js';s.id="yoo"; f.parentNode.insertBefore(s, f);
+    }, 1);
+})();
