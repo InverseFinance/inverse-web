c6c45faff3f55cb3100baac4e44374a7f5ca4766
webmass <webmass@inverse.finance>
2024-07-26 17:23:11 +0400

    Referral: url handling

--- components/common/Modal/ReferralModal.tsx
+++ components/common/Modal/ReferralModal.tsx
@@ -1,15 +1,18 @@
 import { VStack, Text } from "@chakra-ui/react"
 import ConfirmModal from "./ConfirmModal"
 import { useWeb3React } from "@web3-react/core";
-import { InfoMessage } from "../Messages";
+import { InfoMessage, SuccessMessage } from "../Messages";
 import { Input } from "../Input";
 import { useEffect, useState } from "react";
 import { isAddress } from "ethers/lib/utils";
+import { useRouter } from "next/router";
 
-export const REFERRAL_MSG = 'I have been referred by: ';
+export const getReferralMsg = (account: string, referrer: string) => {
+    return `Referral proof signature\n\nMy account:\n${account}\n\nMy referrer:\n ` + referrer;
+}
 
-export const saveReferral = async (referrer: string, sig: string) => {
-    const res = await fetch(`/api/referral?r=${referrer}`, {
+export const saveReferral = async (referrer: string, account: string, sig: string) => {
+    const res = await fetch(`/api/referral?r=${referrer}&account=${account}`, {
         method: 'POST',
         body: JSON.stringify({ sig }),
         headers: {
@@ -24,15 +27,18 @@ export const ReferralModal = ({
     referrer,
     onOk = () => { },
     isOpen = false,
+    onOpen = () => { },
     onClose = () => { },
     onCancel = () => { },
-    onSuccess = () => { },
 }: {
     referrer: string,
 }) => {
+    const { query } = useRouter();
     const { provider, account } = useWeb3React();
     const [refAddress, setRefAddress] = useState(referrer);
     const [isInvalid, setIsInvalid] = useState(false);
+    const [isInited, setIsInited] = useState(false);
+    const [isSuccess, setIsSuccess] = useState(false);
 
     const handleRefAddress = (address: string) => {
         setRefAddress(address);
@@ -42,18 +48,31 @@ export const ReferralModal = ({
         setIsInvalid(!refAddress ? false : !isAddress(refAddress));
     }, [refAddress]);
 
+    useEffect(() => {
+        if (!isInited && !refAddress && !!query?.referrer && isAddress(query?.referrer)) {
+            setIsInited(true);
+            setRefAddress(query.referrer);
+            onOpen();
+        }
+    }, [query, refAddress, isInited]);
+
+    const onSuccess = () => {
+        setIsSuccess(true);
+        setTimeout(() => {
+            // onClose();
+            // onOk();
+            window.location.href = window.location.href.replace('referrer=', 'wasReferredBy=');
+        }, 1300);
+    }
+
     const handleOk = async () => {
         if (provider && !!account) {
             const signer = provider?.getSigner();
-            const sig = await signer.signMessage(REFERRAL_MSG + refAddress).catch(() => '');
+            const sig = await signer.signMessage(getReferralMsg(account, refAddress)).catch(() => '');
             if (!!sig) {
-                const saveRes = await saveReferral(account, sig);
+                const saveRes = await saveReferral(refAddress, account, sig);
                 if (saveRes.status === 200) {
-                    if (onSuccess) {
-                        onSuccess();
-                    }
-                    onClose();
-                    onOk();
+                    onSuccess();
                 }
             }
         } else {
@@ -70,15 +89,20 @@ export const ReferralModal = ({
         }}
         isOpen={isOpen}
         okLabel="Sign"
-        modalProps={{ scrollBehavior: 'inside', minW: { base: '98vw', lg: '700px' }  }}
+        modalProps={{ scrollBehavior: 'inside', minW: { base: '98vw', lg: '700px' } }}
         okDisabled={isInvalid || !refAddress}
     >
         <VStack p='4' spacing="4" alignItems="flex-start">
-            <VStack alignItems="flex-start" w='full'>
-                <Text fontWeight="bold">Referrer's address:</Text>
-                <Input isInvalid={isInvalid} border={isInvalid ? '1px solid red' : ''} type="string" value={refAddress} onChange={(e) => handleRefAddress(e.target.value)} />
-            </VStack>
-            <InfoMessage alertProps={{ w: 'full' }} description="Please sign a message with your wallet to confirm your who referred to you." />
+            {
+                isSuccess ? <SuccessMessage alertProps={{ fontSize: '18px', fontWeight: 'bold', w: { base: 'full', sm: 'auto' } }} iconProps={{ height: 50, width: 50 }} description="Referrer registration complete!" />
+                    : <>
+                        <VStack alignItems="flex-start" w='full'>
+                            <Text fontWeight="bold">Referrer's address:</Text>
+                            <Input isInvalid={isInvalid} border={isInvalid ? '1px solid red' : ''} type="string" value={refAddress} onChange={(e) => handleRefAddress(e.target.value)} />
+                        </VStack>
+                        <InfoMessage alertProps={{ w: 'full' }} description="Please sign a message with your wallet to confirm who is your referrer. This action does not cost you anything" />
+                    </>
+            }
         </VStack>
     </ConfirmModal>
 }
