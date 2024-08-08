8d30dbab5534e6c6f111616f3d523f96706c05db
webmass <webmass@inverse.finance>
2024-08-08 17:10:50 +0400

    Referral: api refacto

--- pages/api/referral.ts
+++ pages/api/referral.ts
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
         method,
     } = req
 
-    const { r, account, csv, affiliate } = query;
+    const { r, account, csv } = query;
 
     const key = `referrals`;
     const cachedResult = await getCacheFromRedis(key, false, 600);
@@ -36,62 +36,56 @@ export default async function handler(req, res) {
                 res.setHeader("Content-Type", "text/csv");
                 res.setHeader("Content-Disposition", "attachment; filename=referrals.csv");
                 res.status(200).send(CSV);
+                return;
             }
-            else if (!!affiliate && (isAddress(affiliate) || affiliate === 'all')) {
-                const affiliateReferrals = Object.entries(cachedResult?.referrals || {})
-                    .filter(([referred, refData]) => affiliate === 'all' || refData.affiliate.toLowerCase() === affiliate.toLowerCase())
-                    .map(([referred, refData]) => ({
-                        referred,
-                        affiliate: refData.affiliate,
-                        timestamp: refData.timestamp,
-                        beforeReferralDueTokensAccrued: refData.beforeReferralDueTokensAccrued,
-                        blockNumber: refData.blockNumber,
-                    }));
+            const affiliateReferrals = Object.entries(cachedResult?.referrals || {})
+                .map(([referred, refData]) => ({
+                    referred,
+                    affiliate: refData.affiliate,
+                    timestamp: refData.timestamp,
+                    beforeReferralDueTokensAccrued: refData.beforeReferralDueTokensAccrued,
+                    blockNumber: refData.blockNumber,
+                }));
 
-                const affiliates = [...new Set(affiliateReferrals.map(rd => rd.affiliate))];
+            const affiliates = [...new Set(affiliateReferrals.map(rd => rd.affiliate))];
 
-                const dbrContract = new Contract(DBR, DBR_ABI, provider);
-                const GWGaddress = MULTISIGS.find(m => m.shortName === 'GWG')?.address;
+            const dbrContract = new Contract(DBR, DBR_ABI, provider);
+            const GWGaddress = MULTISIGS.find(m => m.shortName === 'GWG')?.address;
 
-                // gwg dbr transfer events
-                const gwgPaymentEvents = await dbrContract.queryFilter(dbrContract.filters.Transfer(GWGaddress));
-                const affiliatePayments = {};
-                const affiliatePaymentEvents = [];
+            // gwg dbr transfer events
+            const gwgPaymentEvents = await dbrContract.queryFilter(dbrContract.filters.Transfer(GWGaddress));
+            const affiliatePayments = {};
+            const affiliatePaymentEvents = [];
 
-                gwgPaymentEvents.forEach(event => {
-                    if(affiliates.includes(event.args.to)){
-                        if(!affiliatePayments[event.args.to]) {
-                            affiliatePayments[event.args.to] = 0;
-                        }
-                        const amount = getBnToNumber(event.args.amount);
-                        affiliatePayments[event.args.to] += amount;
-                        affiliatePaymentEvents.push({
-                            amount,
-                            txHash: event.transactionHash,
-                            affiliate: event.args.to,
-                            blockNumber: event.blockNumber,
-                        });
+            gwgPaymentEvents.forEach(event => {
+                if (affiliates.includes(event.args.to)) {
+                    if (!affiliatePayments[event.args.to]) {
+                        affiliatePayments[event.args.to] = 0;
                     }
-                });
+                    const amount = getBnToNumber(event.args.amount);
+                    affiliatePayments[event.args.to] += amount;
+                    affiliatePaymentEvents.push({
+                        amount,
+                        txHash: event.transactionHash,
+                        affiliate: event.args.to,
+                        blockNumber: event.blockNumber,
+                    });
+                }
+            });
 
-                return res.status(200).json({
-                    timestamp: Date.now(),
-                    referrals: affiliateReferrals,
-                    affiliateAddresses: affiliates,
-                    referralAddresses: affiliateReferrals.map(rd => rd.referred),
-                    affiliatePaymentEvents,
-                });
-            }
-            else {
-                return res.status(200).json(cachedResult || { referrals: {} });
-            }
+            return res.status(200).json({
+                timestamp: Date.now(),
+                referrals: affiliateReferrals,
+                affiliateAddresses: affiliates,
+                affiliatePaymentEvents,
+            });
             break
         case 'POST':
 
             if (!r || !isAddress(r) || r === BURN_ADDRESS || !account || !isAddress(account) || account === BURN_ADDRESS || r.toLowerCase() === account.toLowerCase()) {
                 res.status(400).json({ status: 'error', message: 'Invalid address' });
                 return;
-            } else if(!!cachedResult?.referrals[account]) {
+            } else if (!!cachedResult?.referrals[account]) {
                 return res.status(400).json({ status: 'error', message: 'A referral has been already registered' });
             }
 
@@ -120,7 +114,7 @@ export default async function handler(req, res) {
                     return
                 }
             };
-            
+
             const blockNumber = await provider.getBlockNumber();
             const beforeReferralDueTokensAccrued = getBnToNumber((await dbrContract.dueTokensAccrued(sigAddress)));
 
