96ed3e521b212c7c23b1f5352e3ab5b0873bf2a3
webmass <webmass@inverse.finance>
2024-08-20 13:57:17 +0400

    RefBrochure: add personas section

--- pages/affiliate/apply.tsx
+++ pages/affiliate/apply.tsx
@@ -96,8 +96,11 @@ export const businessChecks = [
 const mainColor = "#252627";
 const cardBg = "#323334";
 const cardBorder = "#57595C";
+const whitish = "#FFFCF9";
+const lightish = "#D9D9D9";
 
-const DarkCard = (props) => <DashBoardCard position="relative" p="0" gap="8" direction="column" color="white" bgColor={cardBg} border={`1px solid ${cardBorder}`} {...props} />
+const DarkCard = (props) => <DashBoardCard position="relative" p="0" gap="8" direction="column" color={whitish} bgColor={cardBg} border={`1px solid ${cardBorder}`} {...props} />
+const GrayCard = (props) => <DashBoardCard position="relative" p="0" gap="8" direction="column" color={whitish} bgColor={'#7C7F83'} border={`1px solid ${'#A4A6A8'}`} {...props} />
 
 const MainTitle = (props) => <Text fontWeight="extrabold" fontSize="9vw" color={mainColor} {...props} />
 const BigTitle = (props) => <Text fontWeight="bold" fontSize="48px" color={mainColor} {...props} />
@@ -142,30 +145,30 @@ const SectionOne = () => <VStack alignItems="flex-start" position="relative" p={
 </VStack>
 
 const InfoBar = () => <VStack zIndex="2" overflow="hidden" transform="translateY(-40px) rotate(2deg)" bgColor={lightTheme.colors.accentTextColor} w='110%'>
-    <Text fontWeight="extrabold" whiteSpace="nowrap" textOverflow="clip" color="white" fontSize="68px">
+    <Text fontWeight="extrabold" whiteSpace="nowrap" textOverflow="clip" color={whitish} fontSize="68px">
         INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO INFO
     </Text>
 </VStack>
 
 const FaqBar = () => <VStack zIndex="2" overflow="hidden" transform="translateY(-40px) rotate(-2deg)" bgColor={lightTheme.colors.accentTextColor} w='110%'>
-    <Text fontWeight="extrabold" whiteSpace="nowrap" textOverflow="clip" color="white" fontSize="68px">
+    <Text fontWeight="extrabold" whiteSpace="nowrap" textOverflow="clip" color={whitish} fontSize="68px">
         FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ FAQ
     </Text>
 </VStack>
 
 const HowDoesItWorkSection = () => <VStack spacing="8" transform="translateY(-70px)" w='full' alignItems="flex-start" position="relative" px={{ base: 8, lg: 20 }} py="20" bgColor={mainColor}>
-    <Stack w='full' pl={{ base: 0, lg: '20%' }} spacing="8" color="white" direction={{ base: 'column', lg: 'row' }} alignItems={{ base: 'flex-start', lg: 'flex-end' }}>
+    <Stack w='full' pl={{ base: 0, lg: '20%' }} spacing="8" color={whitish} direction={{ base: 'column', lg: 'row' }} alignItems={{ base: 'flex-start', lg: 'flex-end' }}>
         <VStack spacing="0" alignItems="flex-start">
-            <BigTitle whiteSpace="nowrap" color="white" textTransform="uppercase">How does</BigTitle>
-            <BigTitle color="white" textTransform="uppercase">it work?</BigTitle>
+            <BigTitle whiteSpace="nowrap" color={whitish} textTransform="uppercase">How does</BigTitle>
+            <BigTitle color={whitish} textTransform="uppercase">it work?</BigTitle>
         </VStack>
-        <Title transform="translateY(-11px)" color="white" borderBottom={`1px solid ${lightTheme.colors.secAccentTextColor}`}>
+        <Title transform="translateY(-11px)" color={whitish} borderBottom={`1px solid ${lightTheme.colors.secAccentTextColor}`}>
             It's quite simple
         </Title>
     </Stack>
     <SimpleGrid gap="8" columns={{ base: 1, lg: 3 }}>
         <DarkCard alignItems="flex-start">
-            <Text px="8" pt="8" color="white">
+            <Text px="8" pt="8" color={whitish}>
                 <b style={{ color: lightTheme.colors.secAccentTextColor }}>Fill out the application form below to become a FiRM Affiliate</b>. Our team will review your application to ensure you meet the criteria.
             </Text>
             <Link href="/affiliate/register" textDecoration="underline" px="8" mb="140px" color={lightTheme.colors.secAccentTextColor}>
@@ -174,19 +177,68 @@ const HowDoesItWorkSection = () => <VStack spacing="8" transform="translateY(-70
             <Image right="0" position="absolute" bottom="0" src="/assets/affiliate/step1.png" w="100px" />
         </DarkCard>
         <DarkCard>
-            <Text mb="140px" p="8" color="white">
+            <Text mb="140px" p="8" color={whitish}>
                 Once approved, create and share your unique Affiliate link with your audiences. <b style={{ color: lightTheme.colors.secAccentTextColor }}>Promote FIRM to your audiences</b> as much or as little as you like.
             </Text>
             <Image transform="translate3d(-41px, 30px, 0)" position="absolute" bottom="0" src="/assets/affiliate/step2.png" w="90%" />
         </DarkCard>
         <DarkCard>
-            <Text mb="140px" p="8" color="white">
+            <Text mb="140px" p="8" color={whitish}>
                 <b style={{ color: lightTheme.colors.secAccentTextColor }}>Sit back and earn!</b> As the borrowers you refer spend DBR on new DOLA loans, you'll receive 10% of their spent DBR.
             </Text>
             <Image position="absolute" left="0" bottom="0" src="/assets/affiliate/step3.png" w="75%" />
         </DarkCard>
     </SimpleGrid>
 </VStack>
+
+const WhoIsEligibleSection = () => <VStack spacing="8" transform="translateY(-70px)" w='full' alignItems="flex-start" position="relative" px={{ base: 8, lg: 20 }} py="20" bgColor={mainColor}>
+    <Stack w='full' pl={{ base: 0, lg: '20%' }} spacing="8" color={whitish} direction={{ base: 'column', lg: 'row' }} alignItems={{ base: 'flex-start', lg: 'flex-end' }}>
+        <VStack spacing="0" alignItems="flex-start">
+            <BigTitle whiteSpace="nowrap" color={whitish} textTransform="uppercase">Who is</BigTitle>
+            <BigTitle color={whitish} textTransform="uppercase">eligible?</BigTitle>
+        </VStack>
+        <VStack w="345px" alignItems="center">
+            <GrayCard>
+                <Image right="0" position="absolute" bottom="-5px" src="/assets/affiliate/influencers.png" w="343px" h="200px" />
+            </GrayCard>
+            <Text textAlign="justify" p="4" color={whitish} fontSize="16px">
+                The program is brand new and we're initially limiting signups to influencers, community leaders, and others with a sizeable audience, including:
+            </Text>
+        </VStack>
+    </Stack>
+    <SimpleGrid mt="10" gap="8" columns={{ base: 1, lg: 5 }}>
+        {
+            personas.map((persona, i) => {
+                return <VStack alignItems="flex-start">
+                    <GrayCard h="200px" w="200px">
+                        <Image position="absolute" bottom="0" src={`/assets/affiliate/persona${i + 1}.png`} w="100%" {...persona.imageProps} />
+                    </GrayCard>
+                    <Text whiteSpace="break-spaces" fontWeight="bold" pt="4" pb="2" color={whitish} fontSize="16px">
+                        {persona.title}
+                    </Text>
+                    {
+                        !!persona.subtitle && <Text color={lightish} fontSize="14px">
+                            {persona.subtitle}
+                        </Text>
+                    }
+                </VStack>
+            })
+        }
+    </SimpleGrid>
+</VStack>
+
+const personas = [
+    { title: 'Individuals', imageProps: { w: "80%" } },
+    { title: 'Social media\nInfluencers', imageProps: { maxH: "220px" }, subtitle: 'with 5,000+ followers or subscribers on one or more social media platforms including Twitter, YouTube, Facebook, and Instagram.' },
+    { title: 'Financial\nLeaders', subtitle: 'with a community of 500+ members on one or more community groups on Telegram, Facebook, Discord, WeChat, and/or Reddit.' },
+    { title: 'Opinion\nLeaders', imageProps: { bottom: "-7px", left: "-8px" }, subtitle: 'with a community of 500+ members on one or more community groups on Telegram, Facebook, Discord, WeChat, and/or Reddit.' },
+    { title: 'DEX or Similar\nTrading Platforms' },
+    { title: 'Businesses or\nOrganizations' },
+    { title: 'Those with a user\nbase of 2,000+' },
+    { title: 'Market analysis\nplatforms with\n5,000+ daily visits' },
+    { title: 'Industry Media\nPlatforms', imageProps: { left: '-4px' }  },
+    { title: 'Crypto Funds', imageProps: { left: '-18px' } },
+]
 
 export const FirmAffiliateRegisterPage = () => {
     const [name, setName] = useState('');
@@ -239,6 +291,7 @@ export const FirmAffiliateRegisterPage = () => {
                     <SectionOne />
                     <InfoBar />
                     <HowDoesItWorkSection />
+                    <WhoIsEligibleSection />
                 </VStack>
             </ErrorBoundary>
         </Layout>
