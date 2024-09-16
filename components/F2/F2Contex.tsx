c142f96cb8dd5572e7fcd53b538baca9e55a37de
webmass <webmass@inverse.finance>
2024-09-16 14:03:07 +0400

    Firm: underlying case not for lps

--- components/F2/F2Contex.tsx
+++ components/F2/F2Contex.tsx
@@ -97,7 +97,7 @@ export const F2Context = ({
     const colDecimals = market.underlying.decimals;
 
     // deposit and leverage can use either the underlying or the collateral as input
-    const hasUnderlyingAsInputCase = mode === 'Deposit & Borrow' && useLeverage && (market.aleData.buySellToken !== market.collateral && market.aleData.buySellToken !== BURN_ADDRESS);
+    const hasUnderlyingAsInputCase = mode === 'Deposit & Borrow' && !!market.underlyingSymbol && useLeverage && (market.aleData.buySellToken !== market.collateral && market.aleData.buySellToken !== BURN_ADDRESS);
     const isUnderlyingAsInputCase = hasUnderlyingAsInputCase && isUnderlyingAsInputCaseSelected;
     const inputToken = isUnderlyingAsInputCase ? market.aleData.buySellToken : market.collateral;
 
