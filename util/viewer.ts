95ab97aaeee42e1654a2fc2c89c4c74b287fe2cf
webmass <webmass@inverse.finance>
2024-09-25 13:56:15 +0400

    Viewer: util as service

--- util/viewer.ts
+++ util/viewer.ts
@@ -71,6 +71,65 @@ type MarketData = {
     isPaused: boolean;
 };
 
+type PriceData = {
+    dolaPrice: number;
+    dbrPrice: number;
+    dbrPriceInDola: number;
+    dbrPriceInInv: number;
+    invPrice: number;
+}
+
+export const inverseViewer = (providerOrSigner: Provider | JsonRpcSigner) => {
+    const contract = new Contract(VIEWER_CONTRACT_ADDRESS, VIEWER_ABI, providerOrSigner);
+    return {
+        contract,
+        firm: {
+            getMarketListAndAccountListBreakdown: contract.getMarketListAndAccountListBreakdown,
+            getAccountDebtData: contract.getAccountDebtData,
+            getAccountInvBreakdown: contract.getAccountInvBreakdown,
+            getPositionData: contract.getAccountPosition,
+            getMarketData: contract.getMarketData,
+            getMarketListData: contract.getMarketListData,
+            getAccountPosition: contract.getAccountPosition,
+            getAccountDbrClaimableRewards: contract.getAccountDbrClaimableRewards,
+            getAccountFirmDelegate: contract.getAccountFirmDelegate,
+            getAccountFirmStakedInv: contract.getAccountFirmStakedInv,
+            getAccountListDebtData: contract.getAccountListDebtData,
+            getAccountPositionsForMarketList: contract.getAccountPositionsForMarketList,
+            getDepletionTimestamp: contract.getDepletionTimestamp,
+        },
+        inv: {
+            getAccountAssetsInSInvV1: contract.getAccountAssetsInSInvV1,
+            getAccountAssetsInSInvV2: contract.getAccountAssetsInSInvV2,
+            getAccountFirmDelegate: contract.getAccountFirmDelegate,
+            getAccountFirmStakedInv: contract.getAccountFirmStakedInv,
+            getAccountFrontierStakedInv: contract.getAccountFrontierStakedInv,
+            getAccountGovBreakdown: contract.getAccountGovBreakdown,
+            getAccountInvBalancesBreakdown: contract.getAccountInvBalancesBreakdown,
+            getAccountInvVotes: contract.getAccountInvVotes,
+            getAccountTotalAssetsInSInvs: contract.getAccountTotalAssetsInSInvs,
+            getAccountTotalInv: contract.getAccountTotalInv,
+            getAccountTotalStakedInv: contract.getAccountTotalStakedInv,
+            getAccountTotalVotes: contract.getAccountTotalVotes,
+            getAccountVotesAtProposalStart: contract.getAccountVotesAtProposalStart,
+            getAccountXinvVotes: contract.getAccountXinvVotes,
+            getTotalInvStaked: contract.getTotalInvStaked,
+            getTotalSInvAssets: contract.getTotalSInvAssets,
+            getTotalSInvSupply: contract.getTotalSInvSupply,
+            xinvExchangeRate: contract.xinvExchangeRate,
+        },
+        prices: {
+            getDbrPrice: contract.getDbrPrice,
+            getDbrPriceInDola: contract.getDbrPriceInDola,
+            getDbrPriceInInv: contract.getDbrPriceInInv,
+            getDolaPrice: contract.getDolaPrice,
+            getInvOraclePrice: contract.getInvOraclePrice,
+            getInvPrice: contract.getInvPrice,
+            getInverseTokensPrices: contract.getInverseTokensPrices,
+        }
+    };
+};
+
 export const getViewerContract = (providerOrSigner: Provider | JsonRpcSigner) => {
     return new Contract(VIEWER_CONTRACT_ADDRESS, VIEWER_ABI, providerOrSigner);
 };
@@ -96,7 +155,159 @@ export const getMarketData = async (providerOrSigner: Provider | JsonRpcSigner, 
     return contract.getMarketData(market);
 };
 
-export const getMarketsData = async (providerOrSigner: Provider | JsonRpcSigner): Promise<MarketData[]> => {
+export const getMarketListData = async (providerOrSigner: Provider | JsonRpcSigner, markets: string[]): Promise<MarketData[]> => {
     const contract = getViewerContract(providerOrSigner);
-    return contract.getMarketsData();
+    return contract.getMarketListData(markets);
+};
+
+export const getAccountPosition = async (providerOrSigner: Provider | JsonRpcSigner, market: string, account: string): Promise<PositionData> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountPosition(market, account);
+};
+
+// Add the following new functions:
+
+export const getAccountAssetsInSInvV1 = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountAssetsInSInvV1(account);
+};
+
+export const getAccountAssetsInSInvV2 = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountAssetsInSInvV2(account);
+};
+
+export const getAccountDbrClaimableRewards = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountDbrClaimableRewards(account);
+};
+
+export const getAccountFirmDelegate = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<string> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountFirmDelegate(account);
+};
+
+export const getAccountFirmStakedInv = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountFirmStakedInv(account);
+};
+
+export const getAccountFrontierStakedInv = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountFrontierStakedInv(account);
+};
+
+export const getAccountGovBreakdown = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<AccountGovernanceBreakdown> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountGovBreakdown(account);
+};
+
+export const getAccountInvBalancesBreakdown = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<AccountInvBalancesBreakdown> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountInvBalancesBreakdown(account);
+};
+
+export const getAccountInvVotes = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountInvVotes(account);
+};
+
+export const getAccountListDebtData = async (providerOrSigner: Provider | JsonRpcSigner, accounts: string[]): Promise<AccountDebtData[]> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountListDebtData(accounts);
+};
+
+export const getAccountPositionsForMarketList = async (providerOrSigner: Provider | JsonRpcSigner, markets: string[], account: string): Promise<PositionData[]> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountPositionsForMarketList(markets, account);
+};
+
+export const getAccountTotalAssetsInSInvs = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountTotalAssetsInSInvs(account);
+};
+
+export const getAccountTotalInv = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountTotalInv(account);
+};
+
+export const getAccountTotalStakedInv = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountTotalStakedInv(account);
+};
+
+export const getAccountTotalVotes = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountTotalVotes(account);
+};
+
+export const getAccountVotesAtProposalStart = async (providerOrSigner: Provider | JsonRpcSigner, account: string, proposalId: number): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountVotesAtProposalStart(account, proposalId);
+};
+
+export const getAccountXinvVotes = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getAccountXinvVotes(account);
+};
+
+export const getDbrPrice = async (providerOrSigner: Provider | JsonRpcSigner): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getDbrPrice();
+};
+
+export const getDbrPriceInDola = async (providerOrSigner: Provider | JsonRpcSigner): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getDbrPriceInDola();
+};
+
+export const getDbrPriceInInv = async (providerOrSigner: Provider | JsonRpcSigner): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getDbrPriceInInv();
+};
+
+export const getDepletionTimestamp = async (providerOrSigner: Provider | JsonRpcSigner, account: string): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getDepletionTimestamp(account);
+};
+
+export const getDolaPrice = async (providerOrSigner: Provider | JsonRpcSigner): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getDolaPrice();
+};
+
+export const getInvOraclePrice = async (providerOrSigner: Provider | JsonRpcSigner): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getInvOraclePrice();
+};
+
+export const getInvPrice = async (providerOrSigner: Provider | JsonRpcSigner): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getInvPrice();
+};
+
+export const getInverseTokensPrices = async (providerOrSigner: Provider | JsonRpcSigner): Promise<PriceData> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getInverseTokensPrices();
+};
+
+export const getTotalSInvAssets = async (providerOrSigner: Provider | JsonRpcSigner): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getTotalSInvAssets();
+};
+
+export const getTotalSInvSupply = async (providerOrSigner: Provider | JsonRpcSigner): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getTotalSInvSupply();
+};
+
+export const getTotalInvStaked = async (providerOrSigner: Provider | JsonRpcSigner): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.getTotalInvStaked();
+};
+
+export const xinvExchangeRate = async (providerOrSigner: Provider | JsonRpcSigner): Promise<number> => {
+    const contract = getViewerContract(providerOrSigner);
+    return contract.xinvExchangeRate();
 };
