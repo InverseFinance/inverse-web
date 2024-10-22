b61d59f9f3570efd90c164ee989ba14ef35d84b7
webmass <webmass@inverse.finance>
2024-10-22 20:58:31 +0400

    Liquidity: aggregated histo as chunks

--- pages/api/transparency/liquidity-histo.ts
+++ pages/api/transparency/liquidity-histo.ts
@@ -3,18 +3,18 @@ import { getCacheFromRedis, isInvalidGenericParam, redisSetWithTimestamp } from 
 import { getAggregatedDataFromPools } from '@app/util/pools';
 
 export default async function handler(req, res) {
+    const cacheDuration = 120;
     const { cacheFirst, excludeCurrent, chainId } = req.query;
     const isExlcudeCurrent = excludeCurrent === 'true';
     if (isInvalidGenericParam(excludeCurrent) || (!!chainId && !/^[0-9]+$/.test(chainId))) {
         res.status(400).json({ msg: 'invalid request' });
         return;
     }
-    const cacheKey = `liquidity-history-aggregated${chainId ? '-c'+chainId+'-' : ''}-${isExlcudeCurrent ? '-exclude-current' : ''}v1.0.1`
+    const cacheKey = `liquidity-history-aggregated${chainId ? '-c'+chainId+'-' : ''}-${isExlcudeCurrent ? '-exclude-current' : ''}v1.0.2`
 
     try {
-        const cacheDuration = 60;
         res.setHeader('Cache-Control', `public, max-age=${cacheDuration}`);
-        const validCache = await getCacheFromRedis(cacheKey, cacheFirst !== 'true', cacheDuration);
+        const validCache = await getCacheFromRedis(cacheKey, cacheFirst !== 'true', cacheDuration, true);
         if (validCache) {
             res.status(200).json(validCache);
             return
@@ -35,14 +35,14 @@ export default async function handler(req, res) {
             timestamp: +(new Date()),
             aggregatedHistory,
         }
-        await redisSetWithTimestamp(cacheKey, resultData);
+        await redisSetWithTimestamp(cacheKey, resultData, true);
 
         res.status(200).json(resultData)
     } catch (err) {
         console.error(err);
         // if an error occured, try to return last cached results
         try {
-            const cache = await getCacheFromRedis(cacheKey, false);
+            const cache = await getCacheFromRedis(cacheKey, false, cacheDuration, true);
             if (cache) {
                 console.log('Api call failed, returning last cache found');
                 res.status(200).json(cache);
