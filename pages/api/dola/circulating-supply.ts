b06627152ad33c247956317ecb7010824c2a1edc
webmass <webmass@inverse.finance>
2024-10-22 16:07:50 +0400

    opti

--- pages/api/dola/circulating-supply.ts
+++ pages/api/dola/circulating-supply.ts
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
   const cacheKey = `dola-circ-supply-v1.0.1`;
 
   try {
-    const cacheDuration = 60;
+    const cacheDuration = 120;
     res.setHeader('Cache-Control', `public, max-age=${cacheDuration}`);
     const validCache = await getCacheFromRedis(cacheKey, true, cacheDuration);
     const isSaveCircSupply = req.method === 'POST' || req.query.saveCircSupply === 'true';
