e65528f92723eb615de704c707b4fa88943bd125
webmass <webmass@inverse.finance>
2024-11-25 15:09:02 +0400

    fixing

--- components/common/Animation/index.tsx
+++ components/common/Animation/index.tsx
@@ -7,7 +7,8 @@ import warningLottie from '@app/public/assets/lotties/warning.json';
 import inverseLottie from '@app/public/assets/lotties/inverse.json';
 import launchLottie from '@app/public/assets/lotties/launch.json';
 import confettiLottie from '@app/public/assets/lotties/confetti.json';
-// import Lottie from 'lottie-react';
+import dynamic from 'next/dynamic';
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
 import { useEffect, useState } from 'react';
 
 export type AnimProps = {
@@ -27,7 +28,7 @@ export const Animation = ({ animData, height = 30, width = 30, loop = false, aut
     }, []);
     return (
         <Box height={`${height}px`} width={`${width}px`}>
-            {/* {
+            {
                 inited && <Lottie
                     loop={loop}
                     autoplay={autoplay}
@@ -36,7 +37,7 @@ export const Animation = ({ animData, height = 30, width = 30, loop = false, aut
                         preserveAspectRatio: 'xMidYMid slice'
                     }}
                 />
-            } */}
+            }
         </Box>
     )
 }
