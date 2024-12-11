63ed435b60f0686318b5091c3d1119e8d23b8a47
webmass <webmass@inverse.finance>
2024-12-11 18:28:25 +0400

    Firm logo comp

---
+++ components/common/Logo/FirmLogo.tsx
@@ -0,0 +1,18 @@
+import { useAppTheme } from '@app/hooks/useAppTheme';
+import { Image, ImageProps } from '@chakra-ui/react'
+
+const firmImages = {
+  'dark': 'firm-final-logo-white.png',
+  'light': 'firm-final-logo.png',
+}
+
+export const FirmLogo = ({  
+  ...props
+}: {
+ 
+} & ImageProps) => {
+  const { themeName } = useAppTheme();
+  return <Image transform="translateY(6px)" src={`/assets/firm/${firmImages[themeName]}`} w='110px' h="auto"  {...props} />;
+}
+
+export default FirmLogo
