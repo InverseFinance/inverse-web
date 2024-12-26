626d54fa4e910cb51fd0241c497143d917dad991
webmass <webmass@inverse.finance>
2024-12-26 20:24:36 +0400

    Ale: sdola case in transformer

--- components/F2/forms/F2CombinedForm.tsx
+++ components/F2/forms/F2CombinedForm.tsx
@@ -238,6 +238,7 @@ export const F2CombinedForm = ({
                     dolaPrice,
                     leverageMinAmountUp,
                     underlyingExRate,
+                    sDolaExRate,
                 );
             }
             else if (isAutoDBR || isUseNativeCoin) {
