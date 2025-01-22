463e2cb2857bdf93e68c32db281b12c6446239a6
webmass <webmass@inverse.finance>
2025-01-22 09:28:09 +0400

    Blast: add msg

--- pages/blast/index.tsx
+++ pages/blast/index.tsx
@@ -31,6 +31,7 @@ export const BlastPage = () => {
               Open the <b>blast.gfxlabs.io</b> bridge app (fork of Superbridge)
             </Link>
             <Text>DOLA address on Blast: <b>0x8e38179D361402f6a94767757e807146609E9B3d</b></Text>
+            <Text>If DOLA shows as invalid on blast.gfxlabs.io, then you can initialize the withdrawal with the UI below, and do the prove and finalize steps on blast.gfxlabs.io in the "action needed" section.</Text>
           </VStack>
         } />
       <VStack
