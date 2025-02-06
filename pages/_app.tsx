c2327f5833d613e4482b427aefab4d17f339782e
webmass <webmass@inverse.finance>
2025-02-06 15:44:10 +0400

    bump: env variables updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -62,7 +62,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 
           <meta name="og:title" content="Inverse Finance - Fixed-Rate DeFi borrowing" />
           <meta name="og:description" content="Inverse Finance is an Open Source Protocol for Fixed-Rate borrowing. Earn high yields with the DOLA stablecoin. Stake INV to earn real yield through DBR streaming." />
-          {/* <meta name="og:image" content="https://images.ctfassets.net/kfs9y9ojngfc/1Ma9aOfVoZhPqBSfWypXPO/51c94241f900cd3f3252e4628916250e/inv_medium_article_image_1.png?w=3840&q=75" /> */}
+          
           <meta name="twitter:site" content="@InverseFinance" />
           <meta name="twitter:image:alt" content="inverse finance" />
           <meta property="twitter:card" content="summary_large_image" />
