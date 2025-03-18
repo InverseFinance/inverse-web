4e4dcb30e04f1211358eceaaf230a0b1f308daea
webmass <webmass@inverse.finance>
2025-03-18 10:50:53 +0400

    Comparator: rework historical rates

--- pages/api/dola/sdola-comparator.ts
+++ pages/api/dola/sdola-comparator.ts
@@ -2,13 +2,55 @@ import 'source-map-support'
 import { getProvider } from '@app/util/providers';
 import { getCacheFromRedis, getCacheFromRedisAsObj, redisSetWithTimestamp } from '@app/util/redis'
 import { NetworkIds } from '@app/types';
-import { getDefiLlamaApy, getSavingsCrvUsdData, getSavingsUSDData, getSavingsUSDzData, getSFraxData, getSUSDEData } from '@app/util/markets';
+import { getBnToNumber, getDefiLlamaApy, getSavingsCrvUsdData, getSavingsUSDData, getSavingsUSDzData, getSFraxData, getSUSDEData } from '@app/util/markets';
 import { getDSRData } from '@app/util/markets';
 import { TOKEN_IMAGES } from '@app/variables/images';
 import { timestampToUTC } from '@app/util/misc';
+import { Contract } from 'ethers';
+import { SDOLA_ABI } from '@app/config/abis';
+import { getMulticallOutput } from '@app/util/multicall';
+import { DAILY_UTC_CACHE_KEY } from '../cron-daily-block-timestamp';
+import { ARCHIVED_UTC_DATES_BLOCKS } from '@app/fixtures/utc-dates-blocks';
+
+const getHistoricalRates = async (addresses: string[]) => {
+  const { data: utcKeyBlockValues, isValid } = await getCacheFromRedisAsObj(DAILY_UTC_CACHE_KEY, false) || { data: ARCHIVED_UTC_DATES_BLOCKS, isValid: false };
+  const provider = getProvider(NetworkIds.mainnet);
+
+  const now = Date.now();
+  const dayMs = 24 * 60 * 60 * 1000;
+
+  const thirtyDaysAgoBlock = utcKeyBlockValues[NetworkIds.mainnet][timestampToUTC(now - 30 * dayMs)];
+  const sixtyDaysAgoBlock = utcKeyBlockValues[NetworkIds.mainnet][timestampToUTC(now - 60 * dayMs)];
+  const ninetyDaysAgoBlock = utcKeyBlockValues[NetworkIds.mainnet][timestampToUTC(now - 90 * dayMs)];
+  const blocks = [undefined, thirtyDaysAgoBlock, sixtyDaysAgoBlock, ninetyDaysAgoBlock];
+
+  const [todayRates, thirtyDayRates, sixtyDayRates, ninetyDayRates] = await Promise.all(
+    blocks.map(block => getMulticallOutput(
+      addresses.map(address => ({
+        contract: new Contract(address, SDOLA_ABI, provider),
+        functionName: 'convertToAssets',
+        params: ['1000000000000000000'],
+      })),
+      1,
+      block,
+    ))
+  );
+
+  return addresses.map((address, index) => {
+    const todayExRate = getBnToNumber(todayRates[index]);
+    const apy30d = (getBnToNumber(thirtyDayRates[index]) / todayExRate - 1) * 365 / 30 * 100;
+    const apy60d = (getBnToNumber(sixtyDayRates[index]) / todayExRate - 1) * 365 / 60 * 100;
+    const apy90d = (getBnToNumber(ninetyDayRates[index]) / todayExRate - 1) * 365 / 90 * 100;
+    return {
+      apy30d,
+      apy60d,
+      apy90d,
+    }
+  });
+}
 
 export default async function handler(req, res) {
-  const cacheKey = `sdola-rates-compare-v1.0.4`;
+  const cacheKey = `sdola-rates-compare-v1.0.5`;
 
   try {
     const cacheDuration = 60;
@@ -24,10 +66,22 @@ export default async function handler(req, res) {
 
     const symbols = [
       // 'USDC', 'USDT',
-      'sDAI', 'sfrxUSD', 'sUSDe', 'sDOLA', 'scrvUSD', 'sUSDS', 'sUSDz'];
+      'sDAI', 'sfrxUSD', 'sUSDe', 'sDOLA', 'scrvUSD', 'sUSDS'
+      // , 'sUSDz'
+    ];
+    const addresses = [
+      '0x83F20F44975D03b1b09e64809B757c47f942BEeA',
+      '0xcf62F905562626CfcDD2261162a51fd02Fc9c5b6',
+      '0x9D39A5DE30e57443BfF2A8307A4256c8797A3497',
+      '0xb45ad160634c528Cc3D2926d9807104FA3157305',
+      '0x0655977FEb2f289A4aB78af67BAB0d17aAb84367',
+      '0xa3931d71877C0E7a3148CB7Eb4463524FEc27fbD',
+    ]
     const projects = [
       // 'Aave-V3', 'Aave-V3', 
-      'Spark', 'Frax', 'Ethena', 'FiRM', 'Curve', 'Sky', 'Anzen'];
+      'Spark', 'Frax', 'Ethena', 'FiRM', 'Curve', 'Sky'
+      // , 'Anzen'
+    ];
     const links = [
       // 'https://app.aave.com/reserve-overview/?underlyingAsset=0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48&marketName=proto_mainnet_v3',
       // 'https://app.aave.com/reserve-overview/?underlyingAsset=0xdac17f958d2ee523a2206206994597c13d831ec7&marketName=proto_mainnet_v3',
@@ -53,6 +107,8 @@ export default async function handler(req, res) {
       getSavingsUSDData(),
       // getSavingsUSDzData(),
     ]);
+
+    const historicalRates = await getHistoricalRates(addresses);
 
     const now = Date.now();
     const nowDayUTC = timestampToUTC(now);
@@ -80,9 +136,9 @@ export default async function handler(req, res) {
           apy: (rate.supplyRate || rate.apy),
           apy30d: (rate.apyMean30d || rate.apy30d),
           avg7: last7.length >= 7 ? last7.reduce((prev, curr) => prev + (curr[symbol] || 0), 0) / last7.length : 0,
-          avg30: last30.length >= 30 ? last30.reduce((prev, curr) => prev + (curr[symbol] || 0), 0) / last30.length : 0,
-          avg60: last60.length >= 60 ? last60.reduce((prev, curr) => prev + (curr[symbol] || 0), 0) / last60.length : 0,
-          avg90: last90.length >= 90 ? last90.reduce((prev, curr) => prev + (curr[symbol] || 0), 0) / last90.length : 0,
+          avg30: historicalRates[index].apy30d || (last30.length >= 30 ? last30.reduce((prev, curr) => prev + (curr[symbol] || 0), 0) / last30.length : 0),
+          avg60: historicalRates[index].apy60d || (last60.length >= 60 ? last60.reduce((prev, curr) => prev + (curr[symbol] || 0), 0) / last60.length : 0),
+          avg90: historicalRates[index].apy90d || (last90.length >= 90 ? last90.reduce((prev, curr) => prev + (curr[symbol] || 0), 0) / last90.length : 0),
           symbol,
           image: TOKEN_IMAGES[symbol],
           project: projects[index],
@@ -99,6 +155,7 @@ export default async function handler(req, res) {
 
     const result = {
       timestamp: Date.now(),
+      historicalRates,
       pastRates,
       utcSnapshots,
       rates: sortedRates,
