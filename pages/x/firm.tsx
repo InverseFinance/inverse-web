024bbbcde220568587ec8f57cf35732215a3067d
webmass <webmass@inverse.finance>
2025-04-03 11:49:13 +0400

    Firm: x page ssr

--- pages/x/firm.tsx
+++ pages/x/firm.tsx
@@ -1,7 +1,49 @@
+import { SERVER_BASE_URL } from '@app/config/constants';
 import F2PAGE from '../firm'
+import { F2Market } from '@app/types';
 
-const FirmAlertPage = () => {
-    return F2PAGE({ isTwitterAlert: true });
+export async function getServerSideProps(context) {
+    context.res.setHeader('Cache-Control', 'public, s-maxage=120, stale-while-revalidate=120');
+    const [
+        marketsData,
+        firmTvlData,
+        currentCirculatingSupply,
+        dbrData,
+        dolaPriceData,
+    ] = await Promise.all([
+        fetch(`${SERVER_BASE_URL}/api/f2/fixed-markets?cacheFirst=true`).then(res => res.json()),
+        fetch(`${SERVER_BASE_URL}/api/f2/tvl?cacheFirst=true`).then(res => res.json()),
+        fetch(`${SERVER_BASE_URL}/api/dola/circulating-supply?cacheFirst=true`).then(res => res.text()),
+        fetch(`${SERVER_BASE_URL}/api/dbr?cacheFirst=true`).then(res => res.json()),
+        fetch(`${SERVER_BASE_URL}/api/dola-price?cacheFirst=true`).then(res => res.json()),
+    ]);
+    const dbrPriceUsd = dbrData.priceUsd;
+    const dolaPriceUsd = dolaPriceData['dola-usd'] || 1;
+    return {
+        props: {
+            marketsData: marketsData,
+            firmTvlData,
+            currentCirculatingSupply: parseFloat(currentCirculatingSupply),
+            dbrPriceUsd,
+            dolaPriceUsd,
+         },
+    };
+}
+
+export const FirmAlertPage = ({
+    marketsData,
+    firmTvlData,
+    currentCirculatingSupply,
+    dbrPriceUsd,
+    dolaPriceUsd,
+}: {
+    marketsData: { markets: F2Market[] },
+    firmTvlData: any,
+    currentCirculatingSupply: number,
+    dbrPriceUsd: number,
+    dolaPriceUsd: number,
+}) => {
+    return F2PAGE({ isTwitterAlert: true, marketsData, firmTvlData, currentCirculatingSupply, dbrPriceUsd, dolaPriceUsd });
 }
 
 export default FirmAlertPage
