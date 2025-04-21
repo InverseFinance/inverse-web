6d9e406ee620babeb129e3d28efdc3ad52361525
webmass <webmass@inverse.finance>
2025-04-21 19:36:27 +0400

    DBR: add pending burn detail version api

--- pages/api/dbr/pending-burn.ts
+++ pages/api/dbr/pending-burn.ts
@@ -13,7 +13,7 @@ const { DBR } = getNetworkConfigConstants();
 
 export const dbrPendingBurnCacheKey = `dbr-pending-burn-v1.0.0`;
 
-export const getPendingDbrBurn = async (_block?: number, _now?: number) => {
+export const getPendingDbrBurn = async (_block?: number, _now?: number, isDetails = false) => {
   try {
     const dbrSpenders = await getCacheFromRedis(DBR_SPENDERS_CACHE_KEY, false) || { activeDbrHolders: [] };
     const actualSpenders = dbrSpenders.activeDbrHolders.filter(s => _block ? true : s.debt >= 1);
@@ -38,13 +38,19 @@ export const getPendingDbrBurn = async (_block?: number, _now?: number) => {
     const pendingBurns = actualSpenders.map((s, i) => {
       const lastUpdateTimestamp = lastUpdatesInSeconds[i] * 1000;
       const msSinceLastUpdate = (now - lastUpdateTimestamp);
-      const dailyBurn = s.debt * msSinceLastUpdate / (365 * ONE_DAY_MS);
+      const pendingBurn = s.debt * msSinceLastUpdate / (365 * ONE_DAY_MS);
       return {
         user: s.user,
-        pendingBurn: dailyBurn,
+        pendingBurn,
+        lastUpdateTimestamp,
+        debt: s.debt,
+        dailyBurn: s.debt / 365,
+        refTimeMs: now,
+        timeSinceLastUpdateMs: msSinceLastUpdate,
+        timeSinceLastUpdateInDays: msSinceLastUpdate / ONE_DAY_MS,
       }
     });
-    return pendingBurns.reduce((acc, s) => acc + s.pendingBurn, 0);
+    return isDetails ? pendingBurns : pendingBurns.reduce((acc, s) => acc + s.pendingBurn, 0);
   } catch (err) {
     console.error(err);
     return 0;
@@ -52,27 +58,29 @@ export const getPendingDbrBurn = async (_block?: number, _now?: number) => {
 }
 
 export default async function handler(req, res) {
-
+  const { showDetails } = req.query;
+  const isDetails = showDetails === 'true';
+  const cacheKey = isDetails ? `${dbrPendingBurnCacheKey}-${showDetails}` : dbrPendingBurnCacheKey;
   try {
     const cacheDuration = 60;
     res.setHeader('Cache-Control', `public, max-age=${cacheDuration}`);
-    const validCache = await getCacheFromRedis(dbrPendingBurnCacheKey, true, cacheDuration);
+    const validCache = await getCacheFromRedis(cacheKey, true, cacheDuration);
 
     if (validCache) {
       res.status(200).send(validCache);
       return
     }
 
-    const totalPendingBurn = await getPendingDbrBurn();
+    const result = await getPendingDbrBurn(undefined, undefined, isDetails);
 
-    await redisSetWithTimestamp(dbrPendingBurnCacheKey, totalPendingBurn);
+    await redisSetWithTimestamp(cacheKey, result);
 
-    res.status(200).send(totalPendingBurn);
+    res.status(200).send(result);
   } catch (err) {
     console.error(err);
     // if an error occured, try to return last cached results
     try {
-      const cache = await getCacheFromRedis(dbrPendingBurnCacheKey, false);
+      const cache = await getCacheFromRedis(cacheKey, false);
       if (cache) {
         console.log('Api call failed, returning last cache found');
         res.status(200).send(cache);
