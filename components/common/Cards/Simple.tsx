d62672de81b583fa3da1846624b1f9fefd9ef3f1
webmass <webmass@inverse.finance>
2025-05-21 17:07:20 +0400

    Landing: add comparator section

--- components/common/Cards/Simple.tsx
+++ components/common/Cards/Simple.tsx
@@ -1,5 +1,5 @@
-import { StackProps, VStack } from "@chakra-ui/react"
+import { StackProps, Stack } from "@chakra-ui/react"
 
 export const SimpleCard = (props: StackProps) => {
-    return <VStack bgColor="white" p="8" alignItems="flex-start" boxShadow="0 4px 5px 5px #33333322" {...props} />
+    return <Stack direction="column" bgColor="white" p="8" alignItems="flex-start" boxShadow="0 4px 5px 5px #33333322" {...props} />
 }
