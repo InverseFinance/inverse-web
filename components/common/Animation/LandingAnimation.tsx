b9ec577138c4788e479840a55da40e235a6635f4
webmass <webmass@inverse.finance>
2025-06-05 15:36:25 +0400

    test

--- components/common/Animation/LandingAnimation.tsx
+++ components/common/Animation/LandingAnimation.tsx
@@ -1,4 +1,5 @@
 import { AnimIcon, AnimIconProps } from '.';
-import landingLottie from '@app/public/assets/landing/animation.json';
+// import landingLottie from '@app/public/assets/landing/animation.json';
+import landingLottie from '@app/public/assets/lotties/plus.json';
 
 export const LandingAnimation = (props: AnimIconProps) => <AnimIcon {...props} renderer="svg" animData={landingLottie} />
