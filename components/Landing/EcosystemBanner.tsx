f931d6958ebe22351f8c731fa2964d012b8e8118
webmass <webmass@inverse.finance>
2025-06-05 13:11:02 +0400

    Landing: ecosystem updates

--- components/Landing/EcosystemBanner.tsx
+++ components/Landing/EcosystemBanner.tsx
@@ -27,173 +27,231 @@ export const ecosystemData = [
     {
         image: 'https://assets.coingecko.com/coins/images/12124/large/Curve.png',
         label: 'Curve',
-        href: '',
+
         category: 'DEX',
         categories: ['DEX', 'LENDING', 'LIQUIDITY'],
+        "href": "https://curve.finance",
+        "description": "Curve is a decentralized exchange liquidity pool on Ethereum designed for extremely efficient stablecoin trading",
     },
     {
         image: 'https://assets.coingecko.com/coins/images/15585/large/convex.png?1621256328',
         label: 'Convex',
-        href: '',
+
         category: 'YIELD',
+        "href": "https://www.convexfinance.com/",
+        "description": "Convex simplifies your Curve-boosting experience to maximize your yields.",
     },
     {
         image: 'https://assets.coingecko.com/coins/images/11849/large/yfi-192x192.png?1598325330',
         label: 'Yearn',
-        href: '',
+
         category: 'YIELD',
+        "href": "https://yearn.finance/",
+        "description": "Yearn.finance is an aggregator service for decentralized finance (DeFi) investors, using automation to allow them to maximize profits from yield farming.",
     },
     {
         image: '/assets/projects/coinbase.svg',
         label: 'Coinbase',
-        href: '',
+
         category: 'CEX',
+        "href": "https://www.coinbase.com/",
+        description: "Coinbase is a secure online platform for buying, selling, transferring, and storing cryptocurrency.",
     },
     {
         image: TOKEN_IMAGES.VELO,
         label: 'Velodrome',
-        href: '',
         category: 'DEX',
         categories: ['DEX', 'YIELD'],
+        "href": "https://velodrome.finance",
+        "description": "Velodrome is a DeFi platform that focuses on improving the efficiency of asset exchange and liquidity in the Optimism ecosystem.",
     },
     {
         image: TOKEN_IMAGES.AERO,
         label: 'Aerodrome',
-        href: '',
         category: 'DEX',
         categories: ['DEX', 'YIELD'],
+        "href": "https://aerodrome.finance/",
+        "description": "Velodrome is a DeFi platform that focuses on improving the efficiency of asset exchange and liquidity in the Base ecosystem.",
     },
     {
         image: 'https://assets.coingecko.com/coins/images/11683/large/Balancer.png?1592792958',
         label: 'Balancer',
-        href: '',
+
         category: 'DEX',
+        "href": "https://balancer.finance/",
+        "description": "Balancer is a protocol for programmable liquidity.",
     },
     {
         image: 'https://assets.coingecko.com/coins/images/25942/large/logo.png?1654784187',
         label: 'Aura',
-        href: '',
+
         category: 'YIELD',
+        "href": "https://aura.finance",
+        "description": "Aura Finance is a protocol built on top of the Balancer system to provide maximum incentives to Balancer liquidity providers and BAL stakers (into veBAL) through social aggregation of BAL deposits and Aura's native token.",
     },
     {
         image: 'https://assets.coingecko.com/coins/images/15069/large/Pendle_Logo_Normal-03.png?1696514728',
         label: 'Pendle',
-        href: '',
+
         category: 'YIELD',
+        "href": "https://pendle.finance/",
+        "description": "Pendle Finance is a protocol that enables the trading of tokenized future yield on an AMM system.",
     },
     {
         image: 'https://assets.coingecko.com/coins/images/52854/large/spectra.jpg?1734517434',
         label: 'Spectra',
-        href: '',
+
         category: 'YIELD',
+        "href": "https://app.spectra.finance/fixed-rate",
+        "description": "Secure your future yield at a fixed rate.",
     },
     {
         image: TOKEN_IMAGES.THENA,
         label: 'Thena',
-        href: '',
+
         category: 'DEX',
         categories: ['DEX', 'YIELD'],
+        "href": "https://www.thena.fi",
+        "description": "THENA FUSION is the concentrated liquidity protocol on BSC chain, powered by Algebra Protocol",
     },
     {
         image: 'https://assets.coingecko.com/coins/images/54833/standard/RSUP-icon.png?1741965338',
         label: 'Resupply',
-        href: '',
+
         category: 'LENDING',
         categories: ['LENDING', 'YIELD'],
+        "href": "https://resupply.fi/",
+        "description": "A decentralized stablecoin protocol, leveraging the liquidity and stability of lending markets",
     },
     {
         image: 'https://assets.coingecko.com/coins/images/2822/large/huobi-token-logo.png?1547036992',
-        label: 'Huobi',
-        href: '',
+        label: 'HTX',
+
         category: 'CEX',
+        "href": "https://www.htx.com/",
+        "description": "HTX is one of the world's biggest Bitcoin exchanges and altcoin crypto exchanges.",
     },
     {
         image: 'https://assets.coingecko.com/markets/images/409/large/WeChat_Image_20210622160936.png?1624349423',
         label: 'MEXC',
-        href: '',
+
         category: 'CEX',
+        "href": "https://www.mexc.com/",
+        "description": "MEXC is your easiest way to crypto. Explore the world's leading cryptocurrency exchange for buying, trading, and earning crypto.",
     },
     {
         image: TOKEN_IMAGES.FRAX,
         label: 'Frax',
-        href: '',
+
         category: 'LENDING',
         categories: ['LENDING', 'YIELD'],
+        "href": "https://frax.finance/",
+        "description": "Frax: Decentralized stablecoin protocol with multiple tokens and supporting subprotocols",
     },
     {
         image: 'https://assets.coingecko.com/coins/images/12704/large/token.png?1601876182',
         label: 'Beefy',
-        href: '',
+
         category: 'YIELD',
+        "href": "https://beefy.com",
+        "description": "Beefy automates yield farming to make DeFi easy, safe and efficient for all. By autocompounding your tokens, Beefy unlocks higher returns so you earn more of what you love.",
     },
     {
         image: '/assets/partners/immunefi.svg',
         label: 'Immunefi',
-        href: '',
+
         category: 'SECURITY',
+        "href": "https://immunefi.com/",
+        "description": "Immunefi offers large bug bounties for securing web3 projects",
     },
     {
         image: '/assets/v2/landing/nomoi.png',
         label: 'Nomoi',
-        href: '',
         category: 'SECURITY',
+        "href": "https://nomoi.xyz/",
+        "description": "Nomoi is a boutique Web3 hacker collective that provides comprehensive security audits for DeFi protocols, with roots in Open Zeppelin and Consensys",
     },
     {
         image: '/assets/v2/landing/code4arena.png',
         label: 'Code4Arena',
-        href: '',
+
         category: 'SECURITY',
+        "href": "https://code4arena.com/",
+        "description": "Code4rena security auditors (Wardens) compete to find high-severity vulnerabilities",
     },
     {
         image: '/assets/landing/yAudit.jpg',
         label: 'yAudit',
-        href: '',
+
         category: 'SECURITY',
+        "href": "https://electisec.com/",
+        "description": "yAudit is a security auditing firm that spun out of Yearn Finance, focusing on providing comprehensive smart contract audits and security practices for DeFi protocols",
     },
     {
         image: '/assets/v2/landing/defimoon.png',
         label: 'Defimoon',
-        href: '',
+
         category: 'SECURITY',
+        "href": "https://defimoon.xyz/",
+        "description": "DeFi Moon: Boutique auditing firm specializing in smart contract security reviews",
     },
     {
         image: 'https://assets.coingecko.com/coins/images/877/standard/chainlink-new-logo.png?1696502009',
         label: 'Chainlink',
-        href: '',
+
         category: 'INFRASTRUCTURE',
+        "href": "https://chain.link/",
+        "description": "Chainlink is a decentralized oracle network that provides secure and reliable data feeds to smart contracts.",
     },
     {
         image: '/assets/landing/enso.jpeg',
         label: 'Enso',
-        href: '',
+
         category: 'INFRASTRUCTURE',
+        "href": "https://www.enso.build/",
+        "description": "Enso: Universal DeFi dashboard for bundled, optimized multi-protocol interactions",
     },
     {
         image: '/assets/landing/debank.jpg',
         label: 'Debank',
-        href: '',
+
         category: 'INFRASTRUCTURE',
+        "href": "https://debank.com/",
+        "description": "DeBank is a comprehensive DeFi portfolio tracking and management platform",
     },
     {
         image: TOKEN_IMAGES.USR,
         label: 'USR',
-        href: '',
+
         category: 'LIQUIDITY',
         categories: ['LIQUIDITY', 'YIELD'],
+        "href": "https://usual.money/",
+        "description": "A secure, decentralized issuer that takes back the value captured by crypto giants—and redistributes it to the community through $USUAL.",
     },
     {
         image: TOKEN_IMAGES.CRVUSD,
         label: 'CrvUSD',
-        href: '',
+
         category: 'LIQUIDITY',
         categories: ['LIQUIDITY', 'YIELD'],
+        "href": "https://crvusd.curve.finance/#/ethereum/markets",
+        "description": "crvUSD is a collateralized-debt-position (CDP) stablecoin pegged to the US Dollar",
     },
     {
         image: TOKEN_IMAGES.PYUSD,
         label: 'PayPal',
-        href: '',
+
         category: 'LIQUIDITY',
         categories: ['LIQUIDITY', 'YIELD'],
+        "href": "https://paypal.com",
+        "description": "PayPal USD (PYUSD): Ethereum-based stablecoin backed 1:1 by US dollars",
+    },
+    {
+        image: "https://assets.coingecko.com/coins/images/13724/large/stakedao_logo.jpg?1696513468",
+        label: 'StakeDAO',
+        category: 'YIELD',
+        "href": "https://stakedao.org/",
+        "description": "Stake DAO is a non-custodial decentralized protocol enabling users to access optimized yield opportunities in DeFi",
     },
 ];
 
@@ -203,7 +261,7 @@ const projectImages = ecosystemData.reduce((acc, el) => {
 }, {});
 
 const EcoCellItem = ({ project, width = 50 }: { project: string, width?: number }) => {
-    return <Image borderRadius='full' src={projectImages[project]!} width={{ base: `${parseInt(width/2)}px`, md: `${width}px` }} alt={project} />
+    return <Image borderRadius='full' src={projectImages[project]!} width={{ base: `${parseInt(width / 2)}px`, md: `${width}px` }} alt={project} />
 }
 
 const bannerItems = ecosystemData.slice(0, 10);
@@ -317,13 +375,13 @@ export const EcosystemGrid = () => {
             <HStack spacing="0" w='full' maxW="1300px" >
                 <CellBigGrid>
                     <CellItem>
-                        <NetworkImage chainId={10} size={{ base: '25px', md: '50px' }}  />
+                        <NetworkImage chainId={10} size={{ base: '25px', md: '50px' }} />
                     </CellItem>
                     <CellItem>
-                        <NetworkImage chainId={8453} size={{ base: '25px', md: '50px' }}  />
+                        <NetworkImage chainId={8453} size={{ base: '25px', md: '50px' }} />
                     </CellItem>
                     <CellItem>
-                        <NetworkImage chainId={42161} size={{ base: '25px', md: '50px' }}  />
+                        <NetworkImage chainId={42161} size={{ base: '25px', md: '50px' }} />
                     </CellItem>
                     <CellItem>
                         <CellText>
@@ -334,7 +392,7 @@ export const EcosystemGrid = () => {
                         <NetworkImage chainId={43114} size={{ base: '25px', md: '50px' }} />
                     </CellItem>
                     <CellItem>
-                        <NetworkImage chainId={1} size={{ base: '25px', md: '50px' }}  />
+                        <NetworkImage chainId={1} size={{ base: '25px', md: '50px' }} />
                     </CellItem>
                 </CellBigGrid>
                 <CellBig>
--- pages/ecosystem.tsx
+++ pages/ecosystem.tsx
@@ -3,7 +3,7 @@ import Layout from '@app/components/common/Layout'
 import Head from 'next/head'
 import { shortenNumber } from '@app/util/markets'
 import { getLandingProps } from '@app/blog/lib/utils'
-import { ArrowForwardIcon, CheckCircleIcon, SearchIcon, SmallCloseIcon } from '@chakra-ui/icons'
+import { ArrowForwardIcon, CheckCircleIcon, ChevronDownIcon, ChevronRightIcon, ExternalLinkIcon, SearchIcon, SmallCloseIcon } from '@chakra-ui/icons'
 import { LandingAnimation } from '@app/components/common/Animation/LandingAnimation'
 import FloatingNav from '@app/components/common/Navbar/FloatingNav'
 import { EcosystemBanner, ecosystemData, EcosystemGrid } from '@app/components/Landing/EcosystemBanner'
@@ -38,11 +38,14 @@ const uniqueCategories = ecosystemData.reduce((acc, item) => {
 export const EcosystemPage = ({
 }: {
   }) => {
+  const isSmallerThan = useMediaQuery('(max-width: 768px)');
   const [windowSize, setWindowSize] = useState(0);
   const [isAnimNeedStretch, setIsAnimNeedStretch] = useState(true);
   const [animStrechFactor, setAnimStrechFactor] = useState(1);
   const [searchTerm, setSearchTerm] = useState('');
   const [categories, setCategories] = useState<string[]>([]);
+  // for mobile only
+  const [isFiltersOpen, setIsFiltersOpen] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -90,9 +93,11 @@ export const EcosystemPage = ({
               <GeistText fontSize={{ base: '16px', 'md': '20px' }}>
                 Backed by the biggest protocols and organizations.  A credit framework for all your blockchain activities.
               </GeistText>
-              <LandingNoisedBtn mt="10">
-                Become Our Partner
-              </LandingNoisedBtn>
+              <Link href="mailto:hello@inverse.finance?subject=Partner%20Inquiry&body=Hello%2C%0A%0AI%20am%20interested%20in%20becoming%20a%20partner%20with%20Inverse%20Finance." isExternal>
+                <LandingNoisedBtn mt="10">
+                  Become Our Partner
+                </LandingNoisedBtn>
+              </Link>
             </VStack>
             <VStack maxH={{ base: '200px', md: 'unset' }} borderRadius="6px" overflow="hidden" w={{ base: 'full', md: '50%' }}>
               <VStack bgImage="/assets/landing/anim_bg.png" bgSize="cover" bgRepeat="no-repeat" bgPosition="center" height="80vh" width="100%">
@@ -101,20 +106,41 @@ export const EcosystemPage = ({
             </VStack>
           </ResponsiveStack>
         </VStack>
-        <ResponsiveStack spacing="1px" w='full' alignItems="flex-start">
-          <VStack spacing="8" alignItems="center" w={{ base: 'full', md: '25%' }} bgColor="white" p="4" borderRadius="0">
-            <InputGroup alignItems="center" maxH="48px" w='full'>
-              <Input py="0" maxH="48px" fontSize="16px" bgColor="transparent" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search" textAlign="left" type="search" borderRadius="2px" border={`1px solid ${landingLightBorderColor}`} />
-              <InputRightElement
-                pointerEvents='none'
-                children={<SearchIcon color='gray.300' />}
-              />
-            </InputGroup>
+        <ResponsiveStack bgColor="white" spacing="1px" w='full' alignItems="flex-start">
+          <VStack spacing="8" alignItems="center" w={{ base: 'full', md: '25%' }} bgColor="white" py="4" px="6" borderRadius="0">
+            <VStack spacing="4" w='full' alignItems="flex-end">
+              <HStack justifyContent="space-between" w='full'>
+                <GeistText fontSize="14px" color={landingMutedColor}>
+                  Filters
+                </GeistText>
+                <VStack w="50%" h="1px" bgColor={landingLightBorderColor} ></VStack>
+                <GeistText opacity={categories.length > 0 || !!searchTerm ? 1 : 0.8} fontSize="14px" color={landingPurpleText} cursor="pointer" onClick={() => {
+                  setCategories([])
+                  setSearchTerm('')
+                }}>
+                  Clear all
+                </GeistText>
+              </HStack>
+              <InputGroup alignItems="center" maxH="48px" w='full'>
+                <Input py="0" maxH="48px" fontSize="16px" bgColor="transparent" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search a project" textAlign="left" type="text" borderRadius="2px" border={`1px solid ${landingLightBorderColor}`} />
+                <InputRightElement
+                  pointerEvents='none'
+                  children={<SearchIcon color='gray.300' />}
+                />
+              </InputGroup>
+            </VStack>
             <VStack alignItems="flex-start" w='full'>
-              <GeistText fontSize="14px" color={landingMutedColor}>
-                Filter by Partner Category:
-              </GeistText>
-              <VStack alignItems="flex-start" w='full'>
+              <HStack spacing="4" w='full' alignItems="center" justifyContent="space-between">
+                <GeistText fontSize="14px" color={landingMutedColor}>
+                  Partner Category
+                </GeistText>
+                {
+                  isSmallerThan ? <HStack cursor="pointer" onClick={() => setIsFiltersOpen(!isFiltersOpen)}>
+                    {isFiltersOpen ? <ChevronDownIcon /> : <ChevronRightIcon />}
+                  </HStack> : <VStack w="50%" h="1px" bgColor={landingLightBorderColor} ></VStack>
+                }
+              </HStack>
+              <VStack alignItems="flex-start" w='full' display={isFiltersOpen ? 'flex' : 'none'}>
                 <CheckboxGroup colorScheme='blue' defaultValue={[]} value={categories} onChange={setCategories}>
                   <VStack alignItems="flex-start" w='full' spacing="0" justifyContent="center">
                     {
@@ -131,18 +157,25 @@ export const EcosystemPage = ({
               </VStack>
             </VStack>
           </VStack>
-          <VStack w={{ base: 'full', md: '75%' }}>
+          <VStack w={{ base: 'full', md: '75%' }} borderLeft={{ base: 'none', md: `1px solid ${landingLightBorderColor}` }}>
             <SimpleGrid columns={{ base: 1, md: 3, '2xl': 4 }} spacing="1px" bgColor={landingLightBorderColor}>
               {
                 filteredEcosystemData.map((item, i) => {
-                  return <VStack cursor="pointer" _hover={{ bgColor: landingLightBorderColor }} spacing="4" alignItems="flex-start" bgColor={"white"} p="4" borderRadius="0">
-                    <MiniCard p="4" bgColor="white">
-                      <Image w="40px" h="40px" src={item.image} alt={item.label} />
-                    </MiniCard>
-                    <LandingHeading fontSize="16px" fontWeight="bold">{item.label}</LandingHeading>
-                    <GeistText fontSize="14px" color={landingMutedColor}>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.
-                    </GeistText>
+                  return <Link flexDirection="column" display="flex" isExternal href={item.href} target="_blank" cursor="pointer" className="ecosystem-partner-card" position="relative" _hover={{ bgColor: landingLightBorderColor }} spacing="4" alignItems="flex-start" justifyContent="space-between" bgColor={"white"} p="4" borderRadius="0">
+                    <VStack alignItems="flex-start" justifyContent="space-between" w='full'>
+                      <VStack spacing="0">
+                        <MiniCard p="4" bgColor="white">
+                          <Image w="auto" h="40px" src={item.image} alt={item.label} />
+                        </MiniCard>
+                        <VStack display="none" className="ecosystem-partner-card-link" zIndex="1" cursor="pointer" position="absolute" top="4" right="4" alignItems="center" justifyContent="center" isExternal href={item.url} target="_blank" borderRadius="2px" w='30px' h="30px" bgColor={landingPurpleText}>
+                          <ArrowForwardIcon transform="rotate(-45deg)" color={'white'} />
+                        </VStack>
+                      </VStack>
+                      <LandingHeading fontSize="16px" fontWeight="bold">{item.label}</LandingHeading>
+                      <GeistText fontSize="14px" color={landingMutedColor}>
+                        {item.description}
+                      </GeistText>
+                    </VStack>
                     <HStack spacing="4">
                       {
                         item.categories?.map((category) => {
@@ -154,7 +187,7 @@ export const EcosystemPage = ({
                         })
                       }
                     </HStack>
-                  </VStack>
+                  </Link>
                 })
               }
             </SimpleGrid>
