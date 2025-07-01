b11a578ea6862f730074afa103b9018831138ad4
webmass <webmass@inverse.finance>
2025-07-01 10:42:00 +0400

    csv: add usr & deusd prices

--- pages/api/misc/csv-feeds.ts
+++ pages/api/misc/csv-feeds.ts
@@ -26,8 +26,10 @@ export default async function handler(req, res) {
     const usdsFeedContract = new Contract('0x070287A072cf7Ead994F5b91d75FBdf92A5eAFB7', ["function latestAnswer() external view returns (int256)"], provider);
     const crvusdFeedContract = new Contract('0x4DEcE678ceceb27446b35C672dC7d61F30bAD69E', ["function price_oracle() external view returns (uint256)"], provider);
     const usdcFeedContract = new Contract('0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6', ["function latestAnswer() external view returns (int256)"], provider);
+    const usrFeedContract = new Contract('0x182Af82E3619D2182b3669BbFA8C72bC57614aDf', ["function latestAnswer() external view returns (int256)"], provider);
+    const deusdFeedContract = new Contract('0x15f7744C393CD07bEac9322cE531bd0cB363536b', ["function latestAnswer() external view returns (int256)"], provider);
 
-    const decimals = [18, 18, 18, 8];
+    const decimals = [18, 18, 18, 8, 18, 18];
 
     const pricesBn = await getMulticallOutput([
       {
@@ -50,12 +52,22 @@ export default async function handler(req, res) {
         functionName: 'latestAnswer',
         params: [],
       },
+      {
+        contract: usrFeedContract,
+        functionName: 'latestAnswer',
+        params: [],
+      },
+      {
+        contract: deusdFeedContract,
+        functionName: 'latestAnswer',
+        params: [],
+      },
     ], 1);
 
     const prices = pricesBn.map((price, index) => getBnToNumber(price, decimals[index]));
 
-    let csvData = `Timestamp,USDe,USDS,crvUsd\n`;
-    csvData += `${new Date().toISOString()},${prices[0]},${prices[1]},${prices[2]*prices[3]}\n`;
+    let csvData = `Timestamp,USDe,USDS,crvUsd,USR,deUSD\n`;
+    csvData += `${new Date().toISOString()},${prices[0]},${prices[1]},${prices[2]*prices[3]},${prices[4]},${prices[5]}\n`;
 
     await redisSetWithTimestamp(cacheKey, { csvData });
 
