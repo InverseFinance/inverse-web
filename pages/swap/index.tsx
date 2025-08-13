1108099a85ad7960a151a1f5469c17fea5d5c5f8
webmass <webmass@inverse.finance>
2025-08-13 20:24:23 +0400

    swap: init replace by zap

--- pages/swap/index.tsx
+++ pages/swap/index.tsx
@@ -3,23 +3,20 @@ import Layout from '@app/components/common/Layout'
 import { AppNav } from '@app/components/common/Navbar'
 import Head from 'next/head';
 import { InfoMessage } from '@app/components/common/Messages';
-import { SwapViewSocket } from '@app/components/ThirdParties/SwapViewSocket';
 import Link from '@app/components/common/Link';
-import { useRouter } from 'next/router';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
+import EnsoZap from '@app/components/ThirdParties/enso/EnsoZap';
+import { ZAP_TOKENS_ARRAY } from '@app/components/ThirdParties/tokenlist';
 
 export const Swap = () => {
-  const { query } = useRouter();
-  const { fromToken, toToken, fromChain, toChain } = (query || {});
-
   return (
     <Layout>
       <Head>
-        <title>Inverse Finance - Swap / Bridge</title>
-        <meta name="og:title" content="Inverse Finance - Swap / Bridge" />
-        <meta name="og:description" content="Swap and Bridge DOLA and other assets" />
-        <meta name="description" content="Swap and Bridge DOLA and other assets" />
-        <meta name="keywords" content="Inverse Finance, swap, bridge, stablecoin, DOLA, DAI, USDT, USDC, INV, DBR" />
+        <title>Inverse Finance - Swap</title>
+        <meta name="og:title" content="Inverse Finance - Swap" />
+        <meta name="og:description" content="Swap between popular assets within the Inverse ecosystem" />
+        <meta name="description" content="Swap between popular assets within the Inverse ecosystem" />
+        <meta name="keywords" content="Inverse Finance, swap, stablecoin, DOLA, DAI, USDT, USDC, INV, DBR" />
       </Head>
       <AppNav active="More" activeSubmenu="Swap" />
       <Stack
@@ -41,12 +38,25 @@ export const Swap = () => {
                 <Text>Crypto assets are high-risk investments and you should not expect to be protected if something goes wrong. Take time to learn more before investing in a crypto asset.</Text>
               </VStack>
             } />
-          <SwapViewSocket fromToken={fromToken} toToken={toToken} fromChain={fromChain} toChain={toChain} />
-          <InfoMessage alertProps={{ w: 'full' }} description={
+            <EnsoZap
+              // defaultTokenIn={fromToken as string}
+              // defaultTokenOut={toToken as string}
+              defaultTargetChainId={'1'}
+              title="Swap between popular assets"
+              containerProps={{ w: 'full' }}
+              isSingleChoice={true}
+              ensoPools={
+                ZAP_TOKENS_ARRAY.filter(token => token.chainId === 1).map(token => {
+                  return { poolAddress: token.address, ...token, chainId: token.chainId }
+                })
+              }
+            />
+          {/* <SwapViewSocket fromToken={fromToken} toToken={toToken} fromChain={fromChain} toChain={toChain} /> */}
+          {/* <InfoMessage alertProps={{ w: 'full' }} description={
             <VStack w='full' alignItems='flex-start'>
               <Text>Socket is a third-party service</Text>
             </VStack>
-          } />
+          } /> */}
         </VStack>
         <Stack w={{ base: 'full', lg: '45%' }} direction="column" justifyContent="space-between">
           <InfoMessage
@@ -54,13 +64,10 @@ export const Swap = () => {
             alertProps={{ fontSize: '12px', mb: '8' }}
             description={
               <Stack>
-                <Text fontSize="14px" fontWeight="bold">About the swap / bridge routing</Text>
+                <Text fontSize="14px" fontWeight="bold">About the swap routing</Text>
                 <Text>
-                  Our swap / bridge component is powered by Socket.tech which works as an aggregator.
+                  Our swap component is powered by Socket.tech which works as an aggregator.
                 </Text>
-                <Link isExternal target="_blank" href="https://www.socket.tech/" textDecoration="underline">
-                  More infos on Socket.tech <ExternalLinkIcon />
-                </Link>
                 <Link isExternal target="_blank" href="https://docs.inverse.finance/inverse-finance/inverse-finance/product-guide/tokens/dola" textDecoration="underline">
                   Learn more about bridges & risk in the bridge tab in our docs <ExternalLinkIcon />
                 </Link>
