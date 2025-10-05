d0e627e185d19aff49ecbff26bd6b31ed423b298
webmass <webmass@inverse.finance>
2025-10-05 09:24:14 +0400

    tvls: debug

--- pages/api/f2/tvl.ts
+++ pages/api/f2/tvl.ts
@@ -14,7 +14,7 @@ import { getMulticallOutput } from '@app/util/multicall';
 
 const { F2_MARKETS } = getNetworkConfigConstants();
 
-export const firmTvlCacheKey = 'f2-tvl-v1.0.3'
+export const firmTvlCacheKey = 'f2-tvl-v1.0.4'
 
 export default async function handler(req, res) {
     const { cacheFirst } = req.query;
@@ -73,9 +73,10 @@ export default async function handler(req, res) {
                 const market = marketsCache.markets[marketIndex];
                 return {
                     tvl,
-                    market: { name: market.name, address: market.address, underlying: market.underlying }
+                    market: { name: market.name, address: market.address, price: market.price, underlying: market.underlying }
                 }
             }),
+            deposits,
         }
 
         await redisSetWithTimestamp(firmTvlCacheKey, resultData);
