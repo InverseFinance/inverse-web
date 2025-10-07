dcd4c33c76b55c884e1a27d548484ef1e8e55355
webmass <webmass@inverse.finance>
2025-10-07 09:39:39 +0400

    bump

--- pages/api/f2/fixed-markets.ts
+++ pages/api/f2/fixed-markets.ts
@@ -35,10 +35,10 @@ export default async function handler(req, res) {
 
   try {
     const { data: cachedData, isValid } = await getCacheFromRedisAsObj(cacheKey, cacheFirst !== 'true', cacheDuration);
-    // if (cachedData && isValid) {
-    //   res.status(200).json(cachedData);
-    //   return
-    // }
+    if (cachedData && isValid) {
+      res.status(200).json(cachedData);
+      return
+    }
 
     let provider;
     if (vnetPublicId) {
