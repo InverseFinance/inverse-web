84071828b3fa72a7b945477bfe9ffb38f4cac843
webmass <webmass@inverse.finance>
2025-10-28 15:28:28 +0400

    comment migration file

--- pages/api/transparency/redis-migration.ts
+++ pages/api/transparency/redis-migration.ts
@@ -1,25 +1,25 @@
 import 'source-map-support'
-import { migrateOtherKeys, migratePattern, migratePureKeys } from '@app/util/redis'
+// import { migrateOtherKeys, migratePattern, migratePureKeys } from '@app/util/redis'
 //firm-oracle-prices-*
 //poa-sign-*
 //proposal-reviews-*
 //user-dbr-balance-histo-*
 export default async function handler(req, res) {
-    const { k, type, p } = req.query;
-    if(process.env.API_SECRET_KEY !== k) {
-        return res.status(401).json({ success: false, msg: 'Unauthorized' });
-    }
-    try {
-        if(type === 'pure') {
-            await migratePureKeys();
-        } else if(type === 'other') {
-            await migrateOtherKeys();
-        } else if(type === 'pattern') {
-            await migratePattern(p);
-        }
-        return res.status(200).json({ success: true, p, type });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ success: err });
-    }
+    // const { k, type, p } = req.query;
+    // if(process.env.API_SECRET_KEY !== k) {
+    //     return res.status(401).json({ success: false, msg: 'Unauthorized' });
+    // }
+    // try {
+    //     if(type === 'pure') {
+    //         await migratePureKeys();
+    //     } else if(type === 'other') {
+    //         await migrateOtherKeys();
+    //     } else if(type === 'pattern') {
+    //         await migratePattern(p);
+    //     }
+    //     return res.status(200).json({ success: true, p, type });
+    // } catch (err) {
+    //     console.error(err);
+    //     res.status(500).json({ success: err });
+    // }
 }
