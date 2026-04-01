73c12312f6a5ebfe1e154a0409e5170e14c9f3b6
webmass <webmass@inverse.finance>
2026-04-01 13:52:51 +0400

    debug

--- pages/api/transparency/repayments-v2.ts
+++ pages/api/transparency/repayments-v2.ts
@@ -194,6 +194,8 @@ export default async function handler(req, res) {
             });
 
             return res.status(200).json({
+                postArchiveV5Block,
+                currentBlock,
                 wbtcRepayEvents,
                 wbtcRepaidByDAO,
             })
