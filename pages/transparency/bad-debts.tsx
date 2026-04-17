2e9e2f51fc8369888c6d770a244deec7de44e377
webmass <webmass@inverse.finance>
2026-04-17 16:05:50 +0400

    repayments: fix breakdown pie chart

--- pages/transparency/bad-debts.tsx
+++ pages/transparency/bad-debts.tsx
@@ -517,11 +517,11 @@ export const BadDebtPage = () => {
                   barProps={{ eventName: 'Repayment' }}
                   areaProps={{ id: 'bad-debt-chart', showRangeBtns: true, defaultRange: '1Y', rangesToInclude: ['All', 'YTD', '1Y', '6M', '3M', '7D'], yLabel: 'DOLA bad debt', useRecharts: true, fillInByDayInterval: 1, simplifyData: false, showEvents: true, showEventsLabels: true, domainYpadding: 1000000, showMaxY: false, showTooltips: true, autoMinY: true, mainColor: 'info', allowZoom: true }}
                 />
-                <Stack direction={{ base:'column', lg: 'row' }}>
+                <Stack w='full' justify="space-between" direction={{ base:'column', lg: 'row' }}>
                   <NumberAndPieCard
                     title="Repayments per protocol"
                     isLoading={isLoading}
-                    data={data?.DOLA?.badDebts?.breakdown?.repaid}
+                    data={data?.DOLA?.badDebts?.breakdown?.repaid || []}
                     dataKey="amount"
                     nameKey="protocol"
                     precision={0}
@@ -529,7 +529,7 @@ export const BadDebtPage = () => {
                   <NumberAndPieCard
                     title="Remaining per protocol"
                     isLoading={isLoading}
-                    data={data?.DOLA?.badDebts?.breakdown?.remaining}
+                    data={data?.DOLA?.badDebts?.breakdown?.remaining || []}
                     dataKey="amount"
                     nameKey="protocol"
                     precision={0}
