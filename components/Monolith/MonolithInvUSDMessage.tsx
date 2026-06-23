27ab2f7a21babebf7b2af697ed236d43d9749c17
webmass <webmass@inverse.finance>
2026-06-23 11:05:31 +0400

    inv: migration msg

--- components/Monolith/MonolithInvUSDMessage.tsx
+++ components/Monolith/MonolithInvUSDMessage.tsx
@@ -1,30 +1,54 @@
-import { Divider, VStack, Text, SimpleGrid, HStack, Image, Box, Stack } from "@chakra-ui/react";
+import { Divider, VStack, Text, HStack, Image, Stack } from "@chakra-ui/react";
 import { Link } from "../common/Link";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
+import useEtherSWR from "@app/hooks/useEtherSWR";
+import { useAccount } from "@app/hooks/misc";
+import { FIRM_VIEWER } from "@app/config/constants";
+import { getBnToNumber } from "@app/util/markets";
 
 export const MonolithInvUSDMessage = () => {
+    const account = useAccount();
+    const { data: invMarketBalBn } = useEtherSWR([
+        FIRM_VIEWER, 'getPositionBalance', '0xb516247596Ca36bf32876199FBdCaD6B3322330B', account,
+    ]);
+    const invMarketBal = invMarketBalBn ? getBnToNumber(invMarketBalBn) : 0;
+    const needMigration = invMarketBal >= 1;
     return <VStack maxW="1200px" w='full' alignItems="center" spacing="0" px="6" mb="4">
         <VStack w='full' alignItems="center" spacing="3" position="relative" zIndex={1}>
             <HStack spacing="2" flexWrap="wrap" justify="center">
-                <Text fontSize={{ base: '16px', md: '22px' }} fontWeight="bold" textAlign="center">
-                    Borrow against your INV with
-                </Text>
-                <HStack spacing="1">
-                    <Image
-                        src="https://app.monolith.market/invUSD.png"
-                        borderRadius="full"
-                        w={{ base: '22px', md: '26px' }}
-                        h={{ base: '22px', md: '26px' }}
-                    />
-                    <Text fontSize={{ base: '16px', md: '22px' }} fontWeight="extrabold" color="accentTextColor">
-                        invUSD
-                    </Text>
-                </HStack>
+                {
+                    needMigration ? <VStack>
+                        <Text fontSize={{ base: '16px', md: '22px' }} fontWeight="bold" textAlign="center">
+                            The INV market is being paused and will be phased out progressively by reducing CF with multiple governance proposals
+                        </Text>
+                        <Text fontSize={{ base: '16px', md: '22px' }} fontWeight="bold" textAlign="center">
+                            Please exit your position in the INV market and migrate it to Monolith where you can borrow up to 65% of your collateral value.
+                        </Text>
+                    </VStack>
+                        :
+                        <>
+                            <Text fontSize={{ base: '16px', md: '22px' }} fontWeight="bold" textAlign="center">
+                                Borrow against your INV with
+                            </Text>
+                            <HStack spacing="1">
+                                <Image
+                                    src="https://app.monolith.market/invUSD.png"
+                                    borderRadius="full"
+                                    w={{ base: '22px', md: '26px' }}
+                                    h={{ base: '22px', md: '26px' }}
+                                />
+                                <Text fontSize={{ base: '16px', md: '22px' }} fontWeight="extrabold" color="accentTextColor">
+                                    invUSD
+                                </Text>
+                            </HStack>
+                        </>
+                }
+
             </HStack>
-            <Stack justify="center" spacing={{base: 1, md:0}} direction={{ base: 'column' }} w='full'>
+            <Stack justify="center" spacing={{ base: 1, md: 0 }} direction={{ base: 'column' }} w='full'>
                 <HStack justify={{ base: 'center' }} spacing="1">
                     <Text fontSize={{ base: '13px', md: '15px' }} color="secondaryTextColor">
-                        Powered by <b>Monolith</b>
+                        { needMigration ? <b>Monolith</b> :  <>Powered by <b>Monolith</b></> }
                     </Text>
                     <Image
                         src="https://app.monolith.market/square-logo.png"
