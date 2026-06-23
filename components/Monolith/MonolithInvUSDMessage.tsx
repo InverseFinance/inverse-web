c7047242f58cc6a57b4ac9799bf5db96e23a6f49
webmass <webmass@inverse.finance>
2026-06-23 11:32:45 +0400

    monolith msg

--- components/Monolith/MonolithInvUSDMessage.tsx
+++ components/Monolith/MonolithInvUSDMessage.tsx
@@ -5,6 +5,7 @@ import useEtherSWR from "@app/hooks/useEtherSWR";
 import { useAccount } from "@app/hooks/misc";
 import { FIRM_VIEWER } from "@app/config/constants";
 import { getBnToNumber } from "@app/util/markets";
+import { InfoMessage } from "../common/Messages";
 
 export const MonolithInvUSDMessage = () => {
     const account = useAccount();
@@ -17,14 +18,25 @@ export const MonolithInvUSDMessage = () => {
         <VStack w='full' alignItems="center" spacing="3" position="relative" zIndex={1}>
             <HStack spacing="2" flexWrap="wrap" justify="center">
                 {
-                    needMigration ? <VStack>
-                        <Text fontSize={{ base: '16px', md: '22px' }} fontWeight="bold" textAlign="center">
-                            The INV market is being paused and will be phased out progressively by reducing CF with multiple governance proposals
-                        </Text>
-                        <Text fontSize={{ base: '16px', md: '22px' }} fontWeight="bold" textAlign="center">
-                            Please exit your position in the INV market and migrate it to Monolith where you can borrow up to 65% of your collateral value.
-                        </Text>
-                    </VStack>
+                    needMigration ? <InfoMessage
+                        alertProps={{ w: 'full' }}
+                        description={
+                            <Stack direction={{ base: 'column', lg: 'row-reverse' }} alignItems="center" spacing="5">
+                                <Image borderRadius="20px" maxWidth="100%" src="/assets/migrate-inv-to-monolith.png" width="300px" />
+                                <VStack spacing="0" alignItems="center">
+                                    <Text fontSize={{ base: '12px', md: '14px' }} fontWeight="bold" textAlign="left">
+                                        Please exit your position in the INV market and migrate it to Monolith.
+                                    </Text>
+                                    <Text fontSize={{ base: '12px', md: '14px' }} textAlign="left">
+                                        The INV market is being paused and will be phased out progressively.
+                                    </Text>
+                                    <Text fontSize={{ base: '12px', md: '14px' }} textAlign="left">
+                                        CF on Monolith is 65% while CF on FiRM is 35% (and reducing soon).
+                                    </Text>
+                                </VStack>
+                            </Stack>
+                        }
+                    />
                         :
                         <>
                             <Text fontSize={{ base: '16px', md: '22px' }} fontWeight="bold" textAlign="center">
@@ -48,7 +60,7 @@ export const MonolithInvUSDMessage = () => {
             <Stack justify="center" spacing={{ base: 1, md: 0 }} direction={{ base: 'column' }} w='full'>
                 <HStack justify={{ base: 'center' }} spacing="1">
                     <Text fontSize={{ base: '13px', md: '15px' }} color="secondaryTextColor">
-                        { needMigration ? <b>Monolith</b> :  <>Powered by <b>Monolith</b></> }
+                        {needMigration ? <b>Monolith</b> : <>Powered by <b>Monolith</b></>}
                     </Text>
                     <Image
                         src="https://app.monolith.market/square-logo.png"
